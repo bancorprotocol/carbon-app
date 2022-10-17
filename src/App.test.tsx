ac12895094236b47fd65edc465bebe16bea60494
Jan Langheimer <langheimer@gmail.com>
2022-10-17 11:33:34 +0200

    init tailwind cleanup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from 'App';
+import { App } from 'App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hello/i);
   expect(linkElement).toBeInTheDocument();
 });
