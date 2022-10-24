4457315424ad668486ef2606fd6ef1428cd18911
Jan Langheimer <langheimer@gmail.com>
2022-10-24 16:45:02 +0200

    feature/localStorage: add warning

--- src/services/localeStorage/index.ts
+++ src/services/localeStorage/index.ts
@@ -1,7 +1,11 @@
 import { ManagedLocalStorage } from 'utils/managedLocalStorage';
 
-const appId = 'bancor';
-const appVersion = 'v0';
+const APP_ID = 'bancor';
+const APP_VERSION = 'v0';
+
+// ************************** /
+// BEWARE!! Keys are not to be removed or changed without setting a proper clean-up and migration logic in place!! Same for changing the app version!
+// ************************** /
 
 interface LocalStorageSchema {
   tenderlyRpc: string;
@@ -13,5 +17,5 @@ interface LocalStorageSchema {
 }
 
 export const lsService = new ManagedLocalStorage<LocalStorageSchema>((key) =>
-  [appId, appVersion, key].join('-')
+  [APP_ID, APP_VERSION, key].join('-')
 );
