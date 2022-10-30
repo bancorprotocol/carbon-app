4f162846804df33cb3fa2c3a86501de54c052245
Ran Cohen <ran@bancor.network>
2022-10-30 17:02:06 +0200

    added token query

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { MainMenu } from 'elements/menu';
 import { Router } from 'routing/router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export const App = () => {
+  const queryClient = new QueryClient();
+
   return (
-    <Router>
-      <MainMenu />
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <MainMenu />
+      </Router>
+    </QueryClientProvider>
   );
 };
--- src/pages/trade/index.tsx
+++ src/pages/trade/index.tsx
@@ -1,5 +1,12 @@
-export const TradePage = () => (
-  <div>
-    <span>Here be trades!'</span>
-  </div>
-);
+import { useQuery } from '@tanstack/react-query';
+import { tokenList } from 'services/tokens';
+
+export const TradePage = () => {
+  const query = useQuery(['tokens'], tokenList);
+
+  return (
+    <div>
+      <span>Here be trades!'</span>
+    </div>
+  );
+};
