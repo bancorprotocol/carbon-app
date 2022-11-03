f1ac32aa27cf285e1e6d085b2c2455e8491979bb
Jan Langheimer <langheimer@gmail.com>
2022-11-03 12:53:40 +0100

    feature/strategyBlock: fix ui mobile styles

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -5,7 +5,7 @@ export const Page: FC<{ children: ReactNode; title?: string }> = ({
   title,
 }) => {
   return (
-    <div className={'px-10 md:px-20'}>
+    <div className={'px-10 pb-30 md:px-20'}>
       {title && <h1 className={'mb-30'}>{title}</h1>}
       {children}
     </div>
--- src/pages/strategies/index.tsx
+++ src/pages/strategies/index.tsx
@@ -4,13 +4,14 @@ import { StrategyBlockCreate } from 'components/StrategyBlock/create';
 
 export const StrategiesPage = () => (
   <Page title={'Strategies'}>
-    <div className={'grid grid-cols-4 gap-30'}>
-      <StrategyBlock />
-      <StrategyBlock />
-      <StrategyBlock />
-      <StrategyBlock />
-      <StrategyBlock />
-      <StrategyBlock />
+    <div
+      className={
+        'grid grid-cols-1 gap-30 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3'
+      }
+    >
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((i) => (
+        <StrategyBlock key={i} />
+      ))}
 
       <StrategyBlockCreate />
     </div>
