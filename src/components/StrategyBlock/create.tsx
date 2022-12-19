d6863858c07f3b5288dfeedf0affda3958359ba8
Ran Cohen <51334627+RanCohenn@users.noreply.github.com>
2022-12-19 13:04:42 +0200

    cosmetics

--- src/components/StrategyBlock/create.tsx
+++ src/components/StrategyBlock/create.tsx
@@ -11,8 +11,12 @@ export const StrategyBlockCreate: FC<{
       to={PathNames.createStrategy}
       className="bg-content flex h-full items-center justify-center rounded-10 border-dashed"
     >
-      <div className={`${className} flex flex-col items-center gap-24 text-20`}>
-        <IconPlus />
+      <div
+        className={`${className}  flex flex-col items-center gap-24  text-20`}
+      >
+        <div className="w-40-h-40 rounded-full bg-white">
+          <IconPlus className="text-black" />
+        </div>
         {title}
       </div>
     </Link>
--- src/pages/strategies/index.tsx
+++ src/pages/strategies/index.tsx
@@ -14,12 +14,17 @@ import { WalletConnect } from 'components/WalletConnect';
 import { DropdownMenu } from 'components/DropdownMenu';
 
 export const StrategiesPage = () => {
-  const strategies = useGetUserStrategies().data;
-  const [search, setSearch] = useState('');
   const { user } = useWeb3();
 
+  return user ? <StrategyContent /> : <WalletConnect />;
+};
+
+const StrategyContent = () => {
+  const strategies = useGetUserStrategies();
+  const [search, setSearch] = useState('');
+
   const filteredStrategies = useMemo(() => {
-    return strategies?.filter(
+    return strategies.data?.filter(
       (strategy) =>
         strategy.order0.token.symbol
           .toLowerCase()
@@ -28,34 +33,47 @@ export const StrategiesPage = () => {
           .toLowerCase()
           .includes(search.toLowerCase())
     );
-  }, [search, strategies]);
+  }, [search, strategies.data]);
 
-  return user ? (
-    filteredStrategies && strategies && strategies.length > 0 ? (
-      <Page
-        title={'Strategies'}
-        widget={
-          <StrategyPageTitleWidget search={search} setSearch={setSearch} />
-        }
+  return strategies.isLoading ? (
+    <StrategyLoadingPage />
+  ) : filteredStrategies && strategies.data && strategies.data.length > 0 ? (
+    <Page
+      title={'Strategies'}
+      widget={<StrategyPageTitleWidget search={search} setSearch={setSearch} />}
+    >
+      <m.div
+        className={'grid grid-cols-1 gap-25 md:grid-cols-3'}
+        variants={mListVariant}
+        initial={'hidden'}
+        animate={'visible'}
       >
-        <m.div
-          className={'grid grid-cols-1 gap-25 md:grid-cols-3'}
-          variants={mListVariant}
-          initial={'hidden'}
-          animate={'visible'}
-        >
-          {filteredStrategies.map((s) => (
-            <StrategyBlock key={s.id} strategy={s} />
-          ))}
+        {filteredStrategies.map((s) => (
+          <StrategyBlock key={s.id} strategy={s} />
+        ))}
 
-          <StrategyBlockCreate />
-        </m.div>
-      </Page>
-    ) : (
-      <CreateFirstStrategy />
-    )
+        <StrategyBlockCreate />
+      </m.div>
+    </Page>
   ) : (
-    <WalletConnect />
+    <CreateFirstStrategy />
+  );
+};
+
+const StrategyLoadingPage = () => {
+  return (
+    <Page title={'Strategies'}>
+      <m.div
+        className={'grid grid-cols-1 gap-25 md:grid-cols-3'}
+        variants={mListVariant}
+        initial={'hidden'}
+        animate={'visible'}
+      >
+        {[...Array(3)].map(() => (
+          <div className="loading-skeleton h-[665px] w-full" />
+        ))}
+      </m.div>
+    </Page>
   );
 };
 
@@ -70,51 +88,7 @@ const StrategyPageTitleWidget: FC<{
         setValue={setSearch}
         className="h-40 w-full"
       />
-      <DropdownMenu
-        button={
-          <Button variant="tertiary" className="flex items-center gap-10">
-            Filter & Sort <IconArrowDown className="w-14" />
-          </Button>
-        }
-      >
-        <div className="grid w-[300px] gap-20 p-10">
-          <div className="text-secondary text-20">Sort By</div>
-          <button className="flex items-center justify-between">
-            Recent Trade Activity
-            {false && <IconCheck />}
-          </button>
-          <button className="flex items-center justify-between">
-            Largest Balance (USD)
-            {false && <IconCheck />}
-          </button>
-          <button className="flex items-center justify-between">
-            Smallest Balance (USD)
-            {false && <IconCheck />}
-          </button>
-          <button className="flex items-center justify-between">
-            Recently Created
-            {false && <IconCheck />}
-          </button>
-          <button className="flex items-center justify-between">
-            Oldest Created
-            {false && <IconCheck />}
-          </button>
-          <hr className="border-2 border-silver dark:border-emphasis" />
-          <button className="text-secondary">View</button>
-          <button className="flex items-center justify-between">
-            All
-            {false && <IconCheck />}
-          </button>
-          <button className="flex items-center justify-between">
-            Active
-            {false && <IconCheck />}
-          </button>
-          <button className="flex items-center justify-between">
-            Off curve
-            {false && <IconCheck />}
-          </button>
-        </div>
-      </DropdownMenu>
+      <FilterSort />
       <Link to={PathNames.createStrategy}>
         <Button variant="secondary">Create Strategy</Button>
       </Link>
@@ -130,5 +104,55 @@ const CreateFirstStrategy = () => {
         className="w-[270px] gap-[32px] text-center text-36"
       />
     </div>
+  );
+};
+
+const FilterSort = () => {
+  return (
+    <DropdownMenu
+      button={
+        <Button variant="tertiary" className="flex items-center gap-10">
+          Filter & Sort <IconArrowDown className="w-14" />
+        </Button>
+      }
+    >
+      <div className="grid w-[300px] gap-20 p-10">
+        <div className="text-secondary text-20">Sort By</div>
+        <button className="flex items-center justify-between">
+          Recent Trade Activity
+          {false && <IconCheck />}
+        </button>
+        <button className="flex items-center justify-between">
+          Largest Balance (USD)
+          {false && <IconCheck />}
+        </button>
+        <button className="flex items-center justify-between">
+          Smallest Balance (USD)
+          {false && <IconCheck />}
+        </button>
+        <button className="flex items-center justify-between">
+          Recently Created
+          {false && <IconCheck />}
+        </button>
+        <button className="flex items-center justify-between">
+          Oldest Created
+          {false && <IconCheck />}
+        </button>
+        <hr className="border-2 border-silver dark:border-emphasis" />
+        <button className="text-secondary">View</button>
+        <button className="flex items-center justify-between">
+          All
+          {false && <IconCheck />}
+        </button>
+        <button className="flex items-center justify-between">
+          Active
+          {false && <IconCheck />}
+        </button>
+        <button className="flex items-center justify-between">
+          Off curve
+          {false && <IconCheck />}
+        </button>
+      </div>
+    </DropdownMenu>
   );
 };
