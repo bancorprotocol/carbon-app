5c08507a1154e908cebaca6f59c68760a9381e7e
Jan Langheimer <langheimer@gmail.com>
2022-12-22 13:23:37 +0100

    fix page scroll issue

--- src/pages/strategies/index.tsx
+++ src/pages/strategies/index.tsx
@@ -99,7 +99,7 @@ const StrategyPageTitleWidget: FC<{
 
 const CreateFirstStrategy = () => {
   return (
-    <div className="h-screen p-20">
+    <div className="h-[calc(100vh-128px)] p-20">
       <StrategyBlockCreate
         title="Create Your First Strategy"
         className="w-[270px] gap-[32px] text-center text-36"
