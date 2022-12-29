4f4fec0926255c6797de87ab511168b221b874f7
Ran Cohen <51334627+RanCohenn@users.noreply.github.com>
2022-12-29 13:02:38 +0200

    separated components

--- src/pages/strategies/index.tsx
+++ src/pages/strategies/index.tsx
@@ -1,16 +1,15 @@
-import { Button } from 'components/Button';
 import { Page } from 'components/Page';
-import { SearchInput } from 'components/SearchInput';
 import { StrategyBlock } from 'components/StrategyBlock';
 import { StrategyBlockCreate } from 'components/StrategyBlock/create';
 import { m, mListVariant } from 'motion';
 import { useGetUserStrategies } from 'queries';
-import { FC, useMemo, useState } from 'react';
-import { Link, PathNames } from 'routing';
+import { useMemo, useState } from 'react';
 import { useWeb3 } from 'web3';
 import { WalletConnect } from 'components/WalletConnect';
-import { FilterSort, StrategyFilter, StrategySort } from './FilterSort';
-import { ReactComponent as IconEllipse } from 'assets/icons/ellipse.svg';
+import { StrategyFilter, StrategySort } from './FilterSort';
+import { CreateFirstStrategy } from './CreateFirstStrategy';
+import { StrategyNotFound } from './StrategyNotFound';
+import { StrategyPageTitleWidget } from './StrategyPageTitleWidget';
 
 export const StrategiesPage = () => {
   const { user } = useWeb3();
@@ -80,62 +79,5 @@ const StrategyContent = () => {
         </m.div>
       )}
     </Page>
-  );
-};
-
-const StrategyPageTitleWidget: FC<{
-  search: string;
-  setSearch: (value: string) => void;
-  showFilter: boolean;
-  sort: StrategySort;
-  filter: StrategyFilter;
-  setSort: (sort: StrategySort) => void;
-  setFilter: (sort: StrategyFilter) => void;
-}> = ({ search, setSearch, showFilter, sort, filter, setSort, setFilter }) => {
-  return (
-    <div className="flex items-center gap-20">
-      {showFilter && (
-        <>
-          <SearchInput
-            value={search}
-            setValue={setSearch}
-            className="h-40 w-full"
-          />
-          <FilterSort
-            sort={sort}
-            filter={filter}
-            setSort={setSort}
-            setFilter={setFilter}
-          />
-        </>
-      )}
-      <Link to={PathNames.createStrategy}>
-        <Button variant="secondary">Create Strategy</Button>
-      </Link>
-    </div>
-  );
-};
-
-const CreateFirstStrategy = () => {
-  return (
-    <div className="h-full p-20">
-      <StrategyBlockCreate
-        title="Create Your First Strategy"
-        className="w-[270px] gap-[32px] text-center text-36"
-      />
-    </div>
-  );
-};
-
-const StrategyNotFound = () => {
-  return (
-    <div className="flex h-screen items-center justify-center p-20">
-      <div className=" w-[209px] text-center font-weight-500">
-        <div className="mx-auto mb-32 w-80 rounded-full bg-silver p-16">
-          <IconEllipse />
-        </div>
-        <div className="text-36">Strategy Not Found</div>
-      </div>
-    </div>
   );
 };
