778c656d39c843454d4ab4084d9e122eb0afcc1e
Jan Langheimer <langheimer@gmail.com>
2023-01-03 18:02:05 +0100

    fix unknown token bug

--- src/libs/queries/chain/pairs.ts
+++ src/libs/queries/chain/pairs.ts
@@ -7,17 +7,21 @@ import { useTokens } from 'libs/tokens';
 
 export const useGetTradePairsData = () => {
   const { PoolCollection, Token } = useContract();
-  const { tokens, getTokenById } = useTokens();
+  const { tokens, getTokenById, importToken } = useTokens();
+
+  const _getTknData = async (address: string) => {
+    const data = await fetchTokenData(Token, address);
+    importToken(data);
+    return data;
+  };
 
   return useQuery({
     queryKey: QueryKey.pairs(),
     queryFn: async () => {
       const pairs = await PoolCollection.read.pairs();
       const promises = pairs.map(async (pair) => ({
-        baseToken:
-          getTokenById(pair[0]) ?? (await fetchTokenData(Token, pair[0])),
-        quoteToken:
-          getTokenById(pair[1]) ?? (await fetchTokenData(Token, pair[1])),
+        baseToken: getTokenById(pair[0]) ?? (await _getTknData(pair[0])),
+        quoteToken: getTokenById(pair[1]) ?? (await _getTknData(pair[1])),
       }));
       return await Promise.all(promises);
     },
