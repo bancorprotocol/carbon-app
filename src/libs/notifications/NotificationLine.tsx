d82ace76cc17edf7b397284a3be63bfa6e803543
Ran Cohen <51334627+RanCohenn@users.noreply.github.com>
2023-01-04 21:40:03 +0200

    finished design

--- src/libs/notifications/NotificationLine.tsx
+++ src/libs/notifications/NotificationLine.tsx
@@ -4,9 +4,8 @@ import {
   NotificationStatus,
   useNotifications,
 } from './NotificationsProvider';
-import { ReactComponent as IconTimes } from 'assets/icons/times.svg';
 import { ReactComponent as IconLink } from 'assets/icons/link.svg';
-import { ReactComponent as IconCarbon } from 'assets/logos/carbon.svg';
+import { ReactComponent as IconTimes } from 'assets/icons/times.svg';
 import { ReactComponent as IconCheck } from 'assets/icons/check.svg';
 import { getExplorerLink } from 'utils/blockExplorer';
 import { unix } from 'nightjs';
@@ -16,15 +15,21 @@ const StatusIcon = (status: NotificationStatus) => {
     case NotificationStatus.Pending:
       return (
         <div className="relative flex items-center justify-center">
-          <IconCarbon className="text-primary absolute w-5" />
-          <div className="absolute h-14 w-14 rounded-full border border-fog" />
-          <div className="border-primary h-14 w-14 animate-spin rounded-full border-t border-r" />
+          <div className="h-38 w-38 animate-spin rounded-full border-t border-r border-white" />
         </div>
       );
     case NotificationStatus.Success:
-      return <IconCheck className="w-8 text-white" />;
+      return (
+        <div className="flex flex h-38 w-38 items-center justify-center rounded-full bg-success-500/10">
+          <IconCheck className="w-11 text-success-500" />
+        </div>
+      );
     case NotificationStatus.Failed:
-      return <IconTimes className="w-6 text-white" />;
+      return (
+        <div className="flex flex h-38 w-38 items-center justify-center rounded-full bg-error-500/10">
+          <IconTimes className="w-11 text-error-500" />
+        </div>
+      );
   }
 };
 
@@ -39,34 +44,36 @@ export const NotificationLine: FC<{ notification: Notification }> = ({
       onMouseEnter={() => setIsHovering(true)}
       onMouseLeave={() => setIsHovering(false)}
     >
-      <div className="mb-10 flex items-center gap-10">
-        {StatusIcon(notification.status)}
-        <div className="flex w-full items-center justify-between">
-          <div>
-            {notification.title}
-            <div className="text-14">
-              {notification.txHash && isHovering ? (
-                <a
-                  href={getExplorerLink('tx', notification.txHash)}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  View on Etherscan <IconLink className="ml-6 w-14" />
-                </a>
-              ) : (
-                <div>{notification.description}</div>
-              )}
-            </div>
+      <div className="flex gap-16">
+        <div className="self-center">{StatusIcon(notification.status)}</div>
+        <div className="w-full">
+          {notification.title}
+          <div className="text-14 text-charcoal/80 dark:text-white/80">
+            {notification.txHash && isHovering ? (
+              <a
+                href={getExplorerLink('tx', notification.txHash)}
+                target="_blank"
+                rel="noreferrer"
+              >
+                View on Etherscan <IconLink className="ml-6 w-14" />
+              </a>
+            ) : (
+              <div>{notification.description}</div>
+            )}
           </div>
-
-          <button onClick={() => removeNotification(notification.id)}>
-            <IconTimes className="w-8" />
-          </button>
         </div>
-      </div>
-
-      <div className="text-secondary text-center">
-        {unix(notification.timestamp).fromNow(true)}
+        {isHovering ? (
+          <button
+            className="text-12"
+            onClick={() => removeNotification(notification.id)}
+          >
+            Clear
+          </button>
+        ) : (
+          <div className="text-secondary w-full max-w-fit text-center !text-12">
+            {unix(notification.timestamp).fromNow(true)}
+          </div>
+        )}
       </div>
     </div>
   );
