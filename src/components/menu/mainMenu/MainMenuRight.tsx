f0beef3fb28549ac739922c49877d1f5633d9a43
Ran Cohen <51334627+RanCohenn@users.noreply.github.com>
2023-01-04 12:59:22 +0200

    added notification menu icon and removed dropdown

--- src/components/menu/mainMenu/MainMenuRight.tsx
+++ src/components/menu/mainMenu/MainMenuRight.tsx
@@ -1,9 +1,11 @@
 import { FC } from 'react';
 import { MainMenuRightWallet } from 'components/menu/mainMenu/MainMenuRightWallet';
+import { NotificationsMenu } from 'libs/notifications/NotificationsMenu';
 
 export const MainMenuRight: FC = () => {
   return (
     <div className={'flex items-center space-x-20'}>
+      <NotificationsMenu />
       <MainMenuRightWallet />
     </div>
   );
--- src/libs/notifications/NotificationsMenu.tsx
+++ src/libs/notifications/NotificationsMenu.tsx
@@ -1,19 +1,36 @@
 import { Button } from 'components/common/button';
-import { DropdownMenu } from 'components/common/dropdownMenu';
+import { m } from 'libs/motion';
 import { NotificationLine } from './NotificationLine';
 import { useNotifications } from './NotificationsProvider';
+import { ReactComponent as IconBell } from 'assets/icons/bell.svg';
+import { useRef, useState } from 'react';
+import { useOutsideClick } from 'hooks/useOutsideClick';
 
 export const NotificationsMenu = () => {
   const { notifications, createStrategyNtfc } = useNotifications();
+  const [open, setOpen] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+  useOutsideClick(ref, () => setOpen(false));
 
   return (
-    <DropdownMenu
-      button={(onClick) => <Button onClick={onClick}>Notifications</Button>}
-    >
-      {notifications.map((notification) => (
-        <NotificationLine key={notification.id} notification={notification} />
-      ))}
-      <Button onClick={() => createStrategyNtfc('')}>New</Button>
-    </DropdownMenu>
+    <m.div ref={ref} initial={false} animate={open ? 'open' : 'closed'}>
+      <button
+        onClick={() => setOpen(!open)}
+        className="rounded-full bg-emphasis p-12"
+      >
+        <IconBell />
+      </button>
+      {open && (
+        <div className="bg-body min-w-[500px]">
+          {notifications.map((notification) => (
+            <NotificationLine
+              key={notification.id}
+              notification={notification}
+            />
+          ))}
+          <Button onClick={() => createStrategyNtfc('')}>New</Button>
+        </div>
+      )}
+    </m.div>
   );
 };
