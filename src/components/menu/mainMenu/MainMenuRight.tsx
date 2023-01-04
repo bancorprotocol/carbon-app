a15246475a124a05d6cae51c474a47a082430648
Jan Langheimer <langheimer@gmail.com>
2023-01-04 12:22:54 +0100

    add slide over modal

--- src/components/menu/mainMenu/MainMenuRight.tsx
+++ src/components/menu/mainMenu/MainMenuRight.tsx
@@ -1,9 +1,11 @@
 import { FC } from 'react';
 import { MainMenuRightWallet } from 'components/menu/mainMenu/MainMenuRightWallet';
+import { MainMenuRightNotifications } from 'components/menu/mainMenu/MainMenuRightNotifications';
 
 export const MainMenuRight: FC = () => {
   return (
     <div className={'flex items-center space-x-20'}>
+      <MainMenuRightNotifications />
       <MainMenuRightWallet />
     </div>
   );
