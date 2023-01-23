175441dbb42e894b952228d3279d38afd58217fe
Oz Nasi <oznasi1@gmail.com>
2023-01-23 15:02:27 +0200

    Add popper.js to Tooltip component

--- src/components/common/tooltip/index.tsx
+++ src/components/common/tooltip/index.tsx
@@ -1,6 +1,7 @@
 import { m, Variants } from 'libs/motion';
 import { FC, ReactNode, useRef, useState } from 'react';
 import { ReactComponent as IconTooltip } from 'assets/icons/tooltip.svg';
+import { usePopper } from 'react-popper';
 
 type Props = {
   children: ReactNode;
@@ -12,8 +13,11 @@ let timeout: NodeJS.Timeout;
 let prevPopFunc: Function = () => {};
 
 export const Tooltip: FC<Props> = ({ children, element, delay = 300 }) => {
-  const ref = useRef<HTMLDivElement>(null);
+  const tooltipRef = useRef<HTMLDivElement>(null);
+  const itemRef = useRef<HTMLDivElement>(null);
+
   const [isOpen, setIsOpen] = useState(false);
+  const { styles } = usePopper(itemRef.current, tooltipRef.current);
 
   const handleOnMouseEnter = () => {
     prevPopFunc();
@@ -28,12 +32,12 @@ export const Tooltip: FC<Props> = ({ children, element, delay = 300 }) => {
 
   return (
     <m.div
-      ref={ref}
       initial={false}
       animate={isOpen ? 'open' : 'closed'}
       className={`relative ${isOpen ? 'z-10' : 'z-0'}`}
     >
       <m.div
+        ref={itemRef}
         onMouseEnter={() => handleOnMouseEnter()}
         onMouseLeave={() => handleOnMouseLeave()}
         onClick={() => setIsOpen(!isOpen)}
@@ -41,13 +45,17 @@ export const Tooltip: FC<Props> = ({ children, element, delay = 300 }) => {
         {element ? element : <IconTooltip />}
       </m.div>
       <m.div
+        ref={tooltipRef}
         className={
-          'absolute mt-10 -ml-80 min-w-[200px] rounded border border-b-lightGrey bg-primary-500/10 px-24 py-16 shadow-lg backdrop-blur-2xl dark:border-darkGrey dark:bg-darkGrey/30'
+          'absolute -ml-[125px] mt-10 min-w-[275px] rounded border border-b-lightGrey bg-primary-500/10 px-24 py-16 shadow-lg backdrop-blur-2xl dark:border-darkGrey dark:bg-darkGrey/30'
         }
         onMouseEnter={() => handleOnMouseEnter()}
         onMouseLeave={() => handleOnMouseLeave()}
         variants={menuVariants}
-        style={{ pointerEvents: isOpen ? 'auto' : 'none' }}
+        style={{
+          ...styles.popper,
+          pointerEvents: isOpen ? 'auto' : 'none',
+        }}
       >
         {children}
       </m.div>
