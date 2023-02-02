4c6a5aa06e685879492cd95b2c2e64895bcf4cda
Oz Nasi <oznasi1@gmail.com>
2023-02-02 15:35:25 +0200

    Fix font-weight

--- src/components/common/approval/ApproveToken.tsx
+++ src/components/common/approval/ApproveToken.tsx
@@ -99,8 +99,8 @@ export const ApproveToken: FC<Props> = ({ data, isLoading, error }) => {
           <div className={'flex h-82 flex-col items-end justify-center gap-10'}>
             <div className={'flex items-center space-x-8'}>
               <div
-                className={`!text-12 ${
-                  isLimited ? 'text-white/85 font-weight-500' : 'text-white/60'
+                className={`!text-12 font-weight-500 ${
+                  isLimited ? 'text-white/85' : 'text-white/60'
                 }`}
               >
                 Limited
