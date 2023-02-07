9d87571d86a9b4f1713122e46d1c9a6acc237b3e
Jan Langheimer <langheimer@gmail.com>
2023-02-07 12:36:41 +0100

    add imposter logo and remove top bar alerts

--- src/components/core/menu/mainMenu/MainMenu.tsx
+++ src/components/core/menu/mainMenu/MainMenu.tsx
@@ -1,68 +1,15 @@
-import { FC, ReactNode } from 'react';
+import { FC } from 'react';
 import { MainMenuLeft } from 'components/core/menu/mainMenu/MainMenuLeft';
 import { MainMenuRight } from 'components/core/menu/mainMenu/MainMenuRight';
-import { IS_TENDERLY_FORK } from 'libs/web3/web3.constants';
-import { useWeb3 } from 'libs/web3';
 import { MainMenuTokens } from 'components/core/menu/mainMenu/MainMenuTokens';
 import { MainMenuSettings } from 'components/core/menu/mainMenu/MainMenuSettings';
 import { useTrade } from 'components/trade/useTrade';
-
-enum AlertType {
-  ERROR,
-  WARNING,
-  INFO,
-  PINK,
-}
-
-const getAlertColorClass = (type: AlertType) => {
-  switch (type) {
-    case AlertType.ERROR:
-      return 'bg-red';
-    case AlertType.WARNING:
-      return 'bg-amber-500';
-    case AlertType.INFO:
-      return 'bg-sky-500';
-    case AlertType.PINK:
-      return 'bg-pink-500';
-    default:
-      return 'bg-green';
-  }
-};
-const Alert: FC<{ children: ReactNode; type: AlertType }> = ({
-  children,
-  type,
-}) => {
-  return (
-    <div
-      className={`flex h-24 items-center justify-center ${getAlertColorClass(
-        type
-      )} text-14 font-weight-700 text-white`}
-    >
-      {children}
-    </div>
-  );
-};
-
-const TenderlyForkAlert = () => {
-  return IS_TENDERLY_FORK ? (
-    <Alert type={AlertType.PINK}>Tenderly Fork - ACTIVE</Alert>
-  ) : null;
-};
-
-const ImposterAccountAlert = () => {
-  const { isImposter } = useWeb3();
-  return isImposter ? (
-    <Alert type={AlertType.INFO}>Imposter Account - ACTIVE</Alert>
-  ) : null;
-};
 
 export const MainMenu: FC = () => {
   const { baseToken, quoteToken, isTradePage } = useTrade();
 
   return (
     <div className={`sticky top-0 z-40`}>
-      <TenderlyForkAlert />
-      <ImposterAccountAlert />
       <div className={'main-menu'}>
         <MainMenuLeft />
         {!(!isTradePage || !baseToken || !quoteToken) && (
--- src/components/core/menu/mainMenu/MainMenuRight.tsx
+++ src/components/core/menu/mainMenu/MainMenuRight.tsx
@@ -1,10 +1,21 @@
 import { FC } from 'react';
 import { MainMenuRightWallet } from 'components/core/menu/mainMenu/MainMenuRightWallet';
 import { MainMenuRightNotifications } from 'components/core/menu/mainMenu/MainMenuRightNotifications';
+import { IS_TENDERLY_FORK } from 'libs/web3';
+import { Button } from 'components/common/button';
+
+const TenderlyForkAlert = () => {
+  return IS_TENDERLY_FORK ? (
+    <Button variant={'error'} size={'sm'}>
+      Tenderly Fork
+    </Button>
+  ) : null;
+};
 
 export const MainMenuRight: FC = () => {
   return (
     <div className={'flex items-center space-x-20'}>
+      <TenderlyForkAlert />
       <MainMenuRightNotifications />
       <MainMenuRightWallet />
     </div>
