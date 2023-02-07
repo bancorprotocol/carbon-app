bd025c156f960907f31ef9bdb6ae19a9f613253e
Oz Nasi <oznasi1@gmail.com>
2023-02-07 15:03:42 +0200

    Code clean up

--- src/components/trade/depthChartWidget/DepthChartWidget.tsx
+++ src/components/trade/depthChartWidget/DepthChartWidget.tsx
@@ -1,24 +1,19 @@
 import { Highcharts, HighchartsReact } from 'libs/charts';
 import { NoData } from 'components/common/noData';
-import { getOptions } from 'libs/charts/utils';
 import { useDepthChartWidget } from './useDepthChartWidget';
 import { useTradeTokens } from '../useTradeTokens';
 
 export const DepthChartWidget = () => {
   const { baseToken, quoteToken } = useTradeTokens();
-  const { buyOrders, sellOrders } = useDepthChartWidget(
+  const { buyOrders, sellOrders, getOptions } = useDepthChartWidget(
     baseToken?.address,
     quoteToken?.address
   );
+
   const options = getOptions(buyOrders, sellOrders);
 
-  const isError = !baseToken || !quoteToken || !buyOrders || !sellOrders;
-  console.log('BUY', buyOrders);
-  console.log('SELL', sellOrders);
-
-  if (isError || !options) {
-    return null;
-  }
+  const isError =
+    !baseToken || !quoteToken || !buyOrders || !sellOrders || !options;
 
   return (
     <div className="rounded-10 bg-silver p-20">
--- src/components/trade/depthChartWidget/useDepthChartWidget.ts
+++ src/components/trade/depthChartWidget/useDepthChartWidget.ts
@@ -1,4 +1,4 @@
-import { OrderRow, useGetOrderBook } from 'libs/queries/sdk/orderBook';
+import { OrderRow, useGetOrderBook } from 'libs/queries';
 
 export const useDepthChartWidget = (base?: string, quote?: string) => {
   const { data } = useGetOrderBook(base, quote);
@@ -9,8 +9,134 @@ export const useDepthChartWidget = (base?: string, quote?: string) => {
     });
   };
 
+  const getOptions = (
+    bidsData?: number[][],
+    asksData?: number[][]
+  ): Highcharts.Options => {
+    const left = bidsData?.[bidsData.length - 1]?.[0] || 0;
+    const right = asksData?.[asksData.length - 1]?.[0] || 0;
+    const xMiddle = (right + left) / 2;
+
+    return {
+      chart: {
+        type: 'area',
+        backgroundColor: '#000000',
+        borderColor: '#000000',
+      },
+      credits: {
+        enabled: false,
+      },
+      title: {
+        text: ' ',
+      },
+      xAxis: {
+        minPadding: 0,
+        maxPadding: 0,
+        plotLines: [
+          {
+            color: 'rgba(255, 255, 255, 0.25)',
+            value: xMiddle,
+            width: 1.5,
+            label: {
+              text: ' ',
+              rotation: 90,
+            },
+          },
+        ],
+        title: {},
+        tickWidth: 0,
+        lineWidth: 0,
+        labels: {
+          style: {
+            color: 'rgba(255, 255, 255, 0.6)',
+          },
+        },
+        crosshair: {
+          color: 'rgba(255, 255, 255, 0.25)',
+          width: 1,
+          dashStyle: 'Dash',
+        },
+      },
+      yAxis: [
+        {
+          lineWidth: 0,
+          gridLineWidth: 0,
+          title: {},
+          tickWidth: 0,
+          tickLength: 5,
+          tickPosition: 'inside',
+          labels: {
+            x: 50,
+            enabled: false,
+          },
+          crosshair: {
+            color: 'rgba(255, 255, 255, 0.25)',
+            width: 1,
+            dashStyle: 'Dash',
+          },
+        },
+        {
+          opposite: true,
+          linkedTo: 0,
+          lineWidth: 0,
+          gridLineWidth: 0,
+          title: {},
+          tickWidth: 0,
+          tickLength: 5,
+          tickPosition: 'inside',
+          labels: {
+            style: {
+              color: 'rgba(255, 255, 255, 0.6)',
+            },
+          },
+        },
+      ],
+      legend: {
+        enabled: false,
+      },
+      plotOptions: {
+        area: {
+          fillOpacity: 0.2,
+          lineWidth: 1,
+          step: 'center',
+        },
+      },
+      tooltip: {
+        pointFormat: '{series.name} <b>{point.y}</b><br/>',
+        valueDecimals: 2,
+        borderRadius: 12,
+        backgroundColor: '#212123',
+        borderWidth: 0,
+        style: {
+          color: 'white',
+        },
+      },
+      series: [
+        {
+          type: 'area',
+          name: 'Asks',
+          data: asksData,
+          color: 'rgba(216, 99, 113, 0.8)',
+          marker: {
+            enabled: false,
+          },
+        },
+        {
+          type: 'area',
+          name: 'Bids',
+          data: bidsData,
+          color: 'rgba(0, 181, 120, 0.8)',
+          marker: {
+            enabled: false,
+          },
+        },
+      ],
+    };
+  };
+
   return {
     buyOrders: getOrders(data?.buy),
     sellOrders: getOrders(data?.sell),
+    getOptions,
   };
 };
