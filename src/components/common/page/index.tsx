e8e48e6a5331aa284431858101b000e543ef1f61
Jan Langheimer <langheimer@gmail.com>
2023-02-08 14:15:17 +0100

    fix strategy overview mobile ui

--- src/components/common/page/index.tsx
+++ src/components/common/page/index.tsx
@@ -9,7 +9,7 @@ export const Page: FC<{
     <div
       className={'mx-auto mt-50 max-w-[1280px] px-10 pb-30 md:px-20 xl:px-50'}
     >
-      <div className="mb-30 flex items-center justify-between">
+      <div className="mb-30 flex flex-col justify-between space-y-10 md:flex-row md:items-center md:space-y-0">
         {title && <h1>{title}</h1>}
         {widget && widget}
       </div>
