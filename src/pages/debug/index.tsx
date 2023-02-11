dc4cecc813c07cd077a9fd7c0e07d4f2c71d0cd9
Jan Langheimer <langheimer@gmail.com>
2023-02-11 20:42:17 +0100

    add web worker thread function helpers

--- src/pages/debug/index.tsx
+++ src/pages/debug/index.tsx
@@ -8,6 +8,7 @@ import { DebugResetDefault } from 'components/debug/DebugResetDefault';
 import { DebugNotifications } from 'components/debug/DebugNotifications';
 import { DebugCreateStrategy } from 'components/debug/DebugCreateStrategy';
 import { DebugFiatCurrency } from 'components/debug/DebugFiatCurrency';
+import { DebugWebWorker } from 'components/debug/DebugWebWorker';
 
 export const DebugPage = () => {
   return (
@@ -22,6 +23,7 @@ export const DebugPage = () => {
         <DebugNotifications />
         <DebugCreateStrategy />
         <DebugFiatCurrency />
+        <DebugWebWorker />
       </div>
     </Page>
   );
