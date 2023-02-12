b22780e4d2e0dd385cb84f219a8abc56c022d730
Oz Nasi <oznasi1@gmail.com>
2023-02-12 13:59:28 +0200

    Handle no liquidity case, buy & sell side in order book

--- src/components/trade/orderWidget/OrderBookWidget.tsx
+++ src/components/trade/orderWidget/OrderBookWidget.tsx
@@ -1,3 +1,4 @@
+import { NoOrders } from 'components/common/noOrders';
 import { OrderBookWidgetHeader } from 'components/trade/orderWidget/OrderBookWidgetHeader';
 import { OrderBookWidgetRate } from 'components/trade/orderWidget/OrderBookWidgetRate';
 import { OrderBookWidgetRow } from 'components/trade/orderWidget/OrderBookWidgetRow';
@@ -6,12 +7,21 @@ import { orderBy } from 'lodash';
 import { TradePageProps } from 'pages/trade';
 
 export const OrderBookWidget = ({ base, quote }: TradePageProps) => {
-  const { data } = useOrderBookWidget(base.address, quote.address);
+  const {
+    data: { buy, sell, middleRate },
+  } = useOrderBookWidget(base.address, quote.address);
+
+  const wrappedNoOrders = () => {
+    return (
+      <div className="flex h-[300px] items-center justify-center rounded-10 bg-black text-center">
+        <NoOrders />
+      </div>
+    );
+  };
 
   return (
     <div className={'h-full rounded-10 bg-silver p-20'}>
       <h2>Order Book</h2>
-
       <div className={'mt-20 font-mono'}>
         <OrderBookWidgetHeader
           baseSymbol={base.symbol}
@@ -22,28 +32,34 @@ export const OrderBookWidget = ({ base, quote }: TradePageProps) => {
             'mt-2 rounded-t-4 rounded-b-10 bg-black px-20 py-8 text-14'
           }
         >
-          <div className={'grid grid-cols-3'}>
-            {orderBy(data.sell, ({ rate }) => Number(rate), 'desc').map(
-              (props) => (
-                <OrderBookWidgetRow
-                  key={`orderbooksell-${props.rate}`}
-                  {...props}
-                />
-              )
-            )}
-          </div>
-          <OrderBookWidgetRate buy rate={data.middleRate} />
-          <div className={'grid grid-cols-3'}>
-            {orderBy(data.buy, ({ rate }) => Number(rate), 'desc').map(
-              (props) => (
+          {sell?.length > 0 ? (
+            <div className={'grid grid-cols-3'}>
+              {orderBy(sell, ({ rate }) => Number(rate), 'desc').map(
+                (props) => (
+                  <OrderBookWidgetRow
+                    key={`orderbooksell-${props.rate}`}
+                    {...props}
+                  />
+                )
+              )}
+            </div>
+          ) : (
+            wrappedNoOrders()
+          )}
+          <OrderBookWidgetRate buy rate={middleRate} />
+          {buy?.length > 0 ? (
+            <div className={'grid grid-cols-3'}>
+              {orderBy(buy, ({ rate }) => Number(rate), 'desc').map((props) => (
                 <OrderBookWidgetRow
                   key={`orderbookbuy-${props.rate}`}
                   buy
                   {...props}
                 />
-              )
-            )}
-          </div>
+              ))}
+            </div>
+          ) : (
+            wrappedNoOrders()
+          )}
         </div>
       </div>
     </div>
