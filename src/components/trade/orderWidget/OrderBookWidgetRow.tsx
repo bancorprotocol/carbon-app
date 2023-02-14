d955a9a980da0934b3688292abb56e377587d2e9
Jan Langheimer <langheimer@gmail.com>
2023-02-14 15:05:00 +0100

    add oder book overflow

--- src/components/trade/orderWidget/OrderBookWidgetRow.tsx
+++ src/components/trade/orderWidget/OrderBookWidgetRow.tsx
@@ -10,19 +10,21 @@ type Props = OrderRow & {
 export const OrderBookWidgetRow: FC<Props> = ({ buy, rate, amount, total }) => {
   return (
     <>
-      <div className={`${buy ? 'text-green' : 'text-red'} py-4`}>
+      <div
+        className={`${buy ? 'text-green' : 'text-red'} overflow-x-hidden py-4`}
+      >
         <Tooltip element={rate}>
           <span>{prettifyNumber(rate, { highPrecision: true })}</span>
         </Tooltip>
       </div>
 
-      <div className={'py-4 text-right text-white/80'}>
+      <div className={'overflow-x-hidden py-4 text-right text-white/80'}>
         <Tooltip element={amount}>
           <span>{prettifyNumber(amount, { highPrecision: true })}</span>
         </Tooltip>
       </div>
 
-      <div className={'py-4 text-right text-white/80'}>
+      <div className={'overflow-x-hidden py-4 text-right text-white/80'}>
         <Tooltip element={total}>
           <span>{prettifyNumber(total, { highPrecision: true })}</span>
         </Tooltip>
