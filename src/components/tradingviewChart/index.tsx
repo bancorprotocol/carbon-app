baaee34ce3a9423ae733b441f3ec770bc412efd1
Oz Nasi <oznasi1@gmail.com>
2023-02-22 15:15:30 +0200

    Add mapping for pairs to relevant exchange

--- src/components/tradingviewChart/index.tsx
+++ src/components/tradingviewChart/index.tsx
@@ -2,6 +2,7 @@ import { FC, memo } from 'react';
 import { Copyright } from './copyrights';
 import { Widget } from './widget';
 import { Token } from 'libs/tokens';
+import { pairsToExchangeMapping } from './utils';
 
 export type TradingviewChartProps = {
   token0: Token | undefined;
@@ -10,7 +11,9 @@ export type TradingviewChartProps = {
 
 export const TradingviewChart: FC<TradingviewChartProps> = memo(
   ({ token0, token1 }) => {
-    const symbol = `BINANCE:${token0?.symbol}${token1?.symbol}`;
+    const symbol =
+      pairsToExchangeMapping[`${token0?.symbol}${token1?.symbol}`] ||
+      `BINANCE:${token0?.symbol}${token1?.symbol}`;
 
     return (
       <div>
