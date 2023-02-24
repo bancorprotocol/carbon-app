21398249f9368032f20dce3a57aa66a2f46b9824
Jan Langheimer <langheimer@gmail.com>
2023-02-24 15:36:31 +0100

    fix orderbook precision issue when sanitizing numbers

--- src/components/trade/orderWidget/useOrderBookWidget.ts
+++ src/components/trade/orderWidget/useOrderBookWidget.ts
@@ -1,73 +1,65 @@
 import {
   OrderBook,
   orderBookConfig,
+  OrderRow,
   useGetOrderBook,
 } from 'libs/queries/sdk/orderBook';
 import { useMemo } from 'react';
 import BigNumber from 'bignumber.js';
 import { orderBy } from 'lodash';
 import { useTokens } from 'hooks/useTokens';
-import { sanitizeNumberInput } from 'utils/helpers';
+
+const _subtractPrevAmount = (
+  data: OrderRow[],
+  amount: string,
+  rate: string,
+  i: number
+) => {
+  const prevAmount = data[i - 1]?.amount || '0';
+  const newAmount = new BigNumber(amount).minus(prevAmount);
+  const totalAmount = newAmount.times(rate);
+
+  return {
+    rate,
+    amount: newAmount.toString(),
+    total: totalAmount.toString(),
+  };
+};
+
+const buildOrders = (
+  data: OrderRow[],
+  baseDecimals: number,
+  quoteDecimals: number
+): OrderRow[] => {
+  return data
+    .map(({ amount, rate }, i) => _subtractPrevAmount(data, amount, rate, i))
+    .filter(({ amount }) => amount !== '0')
+    .splice(0, orderBookConfig.buckets.orderBook)
+    .map(({ amount, rate, total }) => ({
+      rate: new BigNumber(rate).toFixed(quoteDecimals, 1),
+      amount: new BigNumber(amount).toFixed(baseDecimals, 1),
+      total: new BigNumber(total).toFixed(quoteDecimals, 1),
+    }));
+};
 
 export const useOrderBookWidget = (base?: string, quote?: string) => {
   const orderBookQuery = useGetOrderBook(base, quote);
   const { getTokenById } = useTokens();
+  const { data } = orderBookQuery;
+
+  const baseDecimals = getTokenById(base!)?.decimals || 0;
+  const quoteDecimals = getTokenById(quote!)?.decimals || 0;
 
   const orders = useMemo<OrderBook>(() => {
-    const buy = [...(orderBookQuery.data?.buy || [])];
-    const sell = [...(orderBookQuery.data?.sell || [])];
-
-    const data = {
-      buy: orderBy(buy, ({ rate }) => Number(rate), 'desc'),
-      sell: orderBy(sell, ({ rate }) => Number(rate), 'asc'),
-    };
-
-    const _subtractPrevAmount = (
-      buy: boolean,
-      amount: string,
-      rate: string,
-      i: number
-    ) => {
-      const prevAmount = data[buy ? 'buy' : 'sell'][i - 1]?.amount || '0';
-      const newAmount = new BigNumber(amount).minus(prevAmount);
-      const totalAmount = newAmount.times(rate);
-
-      return {
-        rate: sanitizeNumberInput(rate, getTokenById(quote!)?.decimals),
-        amount: sanitizeNumberInput(
-          newAmount.toString(),
-          getTokenById(base!)?.decimals
-        ),
-        total: sanitizeNumberInput(
-          totalAmount.toString(),
-          getTokenById(quote!)?.decimals
-        ),
-      };
-    };
+    const buy = orderBy(data?.buy || [], ({ rate }) => +rate, 'desc');
+    const sell = orderBy(data?.sell || [], ({ rate }) => +rate, 'asc');
 
     return {
-      buy: data.buy
-        .map(({ amount, rate }, i) =>
-          _subtractPrevAmount(true, amount, rate, i)
-        )
-        .filter(({ amount }) => amount !== '0')
-        .splice(0, orderBookConfig.buckets.orderBook),
-      sell: data.sell
-        .map(({ amount, rate }, i) =>
-          _subtractPrevAmount(false, amount, rate, i)
-        )
-        .filter(({ amount }) => amount !== '0')
-        .splice(0, orderBookConfig.buckets.orderBook),
-      middleRate: orderBookQuery.data?.middleRate || '0',
+      buy: buildOrders(buy, baseDecimals, quoteDecimals),
+      sell: buildOrders(sell, baseDecimals, quoteDecimals),
+      middleRate: data?.middleRate || '0',
     };
-  }, [
-    base,
-    getTokenById,
-    orderBookQuery.data?.buy,
-    orderBookQuery.data?.middleRate,
-    orderBookQuery.data?.sell,
-    quote,
-  ]);
+  }, [baseDecimals, data?.buy, data?.middleRate, data?.sell, quoteDecimals]);
 
   return { ...orderBookQuery, data: orders };
 };
