5ed5632c6a8f629c5fe3dd92438b1e2f363778f5
Jan Langheimer <langheimer@gmail.com>
2023-03-17 15:40:34 +0100

    fix create strategy duplicate

--- src/components/strategies/create/CreateStrategyMain.tsx
+++ src/components/strategies/create/CreateStrategyMain.tsx
@@ -1,9 +1,13 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { m } from 'libs/motion';
 import { list } from './variants';
 import { useCreateStrategy } from './useCreateStrategy';
 import { CreateStrategyHeader } from './CreateStrategyHeader';
 import { CreateStrategyContent } from './CreateStrategyContent';
+import { useSearch } from '@tanstack/react-location';
+import { MyLocationGenerics } from 'components/trade/useTradeTokens';
+import { useTokens } from 'hooks/useTokens';
+import { pairsToExchangeMapping } from 'components/tradingviewChart/utils';
 
 export const CreateStrategyMain = () => {
   const [showGraph, setShowGraph] = useState(false);
@@ -22,6 +26,27 @@ export const CreateStrategyMain = () => {
     token0BalanceQuery,
     token1BalanceQuery,
   } = useCreateStrategy();
+
+  const search = useSearch<MyLocationGenerics>();
+  const { base: baseAddress, quote: quoteAddress } = search;
+  const { getTokenById } = useTokens();
+
+  useEffect(() => {
+    if (pairsToExchangeMapping[`${base?.symbol}${quote?.symbol}`]) {
+      setShowGraph(true);
+    }
+    if (!base || !quote) {
+      setShowGraph(false);
+    }
+  }, [base, quote, setShowGraph]);
+
+  useEffect(() => {
+    if (!baseAddress && !quoteAddress) {
+      return;
+    }
+    setBase(getTokenById(baseAddress || ''));
+    setQuote(getTokenById(quoteAddress || ''));
+  }, [getTokenById, baseAddress, quoteAddress, setBase, setQuote]);
 
   return (
     <m.div
