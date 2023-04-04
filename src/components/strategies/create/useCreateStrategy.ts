c9806c645adc4c0d6e9226a3533e4baedcea7cb4
ashachaf <40430285+ashachaf@users.noreply.github.com>
2023-04-04 09:16:38 +0300

    Update useCreateStrategy.ts

--- src/components/strategies/create/useCreateStrategy.ts
+++ src/components/strategies/create/useCreateStrategy.ts
@@ -199,7 +199,7 @@ export const useCreateStrategy = () => {
         );
         break;
       }
-      case 'reoccurring': {
+      case 'recurring': {
         order0.resetFields();
         order1.resetFields();
         handleStrategySettings(strategySettings, [
