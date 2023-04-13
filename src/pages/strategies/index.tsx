39c9da1bb20a7a2eeb2f87ac2af47d234a5eb106
Jan Langheimer <langheimer@gmail.com>
2023-04-13 20:04:42 +0200

    update wallet connect

--- src/pages/strategies/index.tsx
+++ src/pages/strategies/index.tsx
@@ -1,9 +1,48 @@
 import { useWeb3 } from 'libs/web3';
 import { WalletConnect } from 'components/common/walletConnect';
 import { StrategyContent } from 'components/strategies/overview';
+import { StrategyPageTitleWidget } from 'components/strategies/overview/StrategyPageTitleWidget';
+import { useGetUserStrategies } from 'libs/queries';
+import { Page } from 'components/common/page';
+import { useState } from 'react';
+import {
+  StrategyFilter,
+  StrategySort,
+} from 'components/strategies/overview/StrategyFilterSort';
 
 export const StrategiesPage = () => {
   const { user } = useWeb3();
+  const strategies = useGetUserStrategies();
+  const [search, setSearch] = useState('');
+  const [sort, setSort] = useState(StrategySort.Old);
+  const [filter, setFilter] = useState(StrategyFilter.All);
 
-  return user ? <StrategyContent /> : <WalletConnect />;
+  return (
+    <Page
+      title={`${strategies.data?.length || ''} Strategies`}
+      widget={
+        <StrategyPageTitleWidget
+          sort={sort}
+          filter={filter}
+          setSort={setSort}
+          setFilter={setFilter}
+          search={search}
+          setSearch={setSearch}
+          showFilter={!!(strategies.data && strategies.data.length > 2)}
+        />
+      }
+    >
+      {user ? (
+        <StrategyContent
+          strategies={strategies}
+          search={search}
+          filter={filter}
+          sort={sort}
+        />
+      ) : (
+        <WalletConnect />
+      )}
+    </Page>
+  );
 };
+// ? <StrategyContent /> : <WalletConnect />
