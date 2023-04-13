e4930b555722f59bb8b678837972e9c325a4852e
Oz Nasi <oznasi1@gmail.com>
2023-04-13 16:29:01 +0300

    Remove ts-ignore

--- src/libs/modals/modals/ModalTradeTokenList/ModalTradeTokenList.tsx
+++ src/libs/modals/modals/ModalTradeTokenList/ModalTradeTokenList.tsx
@@ -32,7 +32,6 @@ export const ModalTradeTokenList: ModalFC<ModalTradeTokenListData> = ({
     addFavoritePair,
     removeFavoritePair,
   } = useModalTradeTokenList({ id, data });
-  // @ts-ignore
 
   return (
     <Modal id={id} title="Select Token Pair">
