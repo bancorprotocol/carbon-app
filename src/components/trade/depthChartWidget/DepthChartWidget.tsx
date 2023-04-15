649f5e20a5debca3cb2fea9cb6cf10ff54d97564
Jan Langheimer <langheimer@gmail.com>
2023-04-15 16:21:47 +0200

    add trading view chart to trade depth chart

--- src/components/trade/depthChartWidget/DepthChartWidget.tsx
+++ src/components/trade/depthChartWidget/DepthChartWidget.tsx
@@ -3,12 +3,15 @@ import { NoOrders } from 'components/common/noOrder';
 import { useDepthChartWidget } from './useDepthChartWidget';
 import { TradePageProps } from 'pages/trade';
 import { CarbonLogoLoading } from 'components/common/CarbonLogoLoading';
+import { TradingviewChart } from 'components/tradingviewChart';
+import { useState } from 'react';
 
 export const DepthChartWidget = ({ base, quote }: TradePageProps) => {
   const { buyOrders, sellOrders, getOptions, isLoading } = useDepthChartWidget(
     base,
     quote
   );
+  const [showTVChart, setShowTVChart] = useState(false);
 
   const options = getOptions(buyOrders, sellOrders);
 
@@ -16,7 +19,21 @@ export const DepthChartWidget = ({ base, quote }: TradePageProps) => {
 
   return (
     <div className="rounded-10 bg-silver p-20">
-      <div className="mb-20 font-weight-500">Depth</div>
+      <div className="mb-20 font-weight-500">
+        <button
+          onClick={() => setShowTVChart(true)}
+          className={`${showTVChart ? '' : 'text-white/60'}`}
+        >
+          Depth
+        </button>
+        <span className={'text-secondary px-10'}> | </span>
+        <button
+          onClick={() => setShowTVChart(false)}
+          className={`${showTVChart ? 'text-white/60' : ''}`}
+        >
+          Price
+        </button>
+      </div>
       {isLoading ? (
         <div className="flex h-[420px] items-center justify-center rounded-10 bg-black">
           <div className={'h-80'}>
@@ -27,8 +44,12 @@ export const DepthChartWidget = ({ base, quote }: TradePageProps) => {
         <div className="flex items-center justify-center rounded-10 bg-black">
           <NoOrders />
         </div>
-      ) : (
+      ) : showTVChart ? (
         <HighchartsReact highcharts={Highcharts} options={options} />
+      ) : (
+        <div className={'h-[420px] pb-20'}>
+          <TradingviewChart base={base} quote={quote} />
+        </div>
       )}
     </div>
   );
