00d11a473a20d8d08b22721b05a69d29861024dd
Oz Nasi <oznasi1@gmail.com>
2023-04-16 15:13:22 +0300

    Fix imports of HighchartsReact

--- src/libs/charts/index.ts
+++ src/libs/charts/index.ts
@@ -1,5 +1,5 @@
 import Highcharts from 'highcharts';
-import HighchartsReact from 'highcharts-react-official';
+export { HighchartsReact } from 'highcharts-react-official';
 
-export { Highcharts, HighchartsReact };
+export { Highcharts };
 export type { Options } from 'highcharts/highcharts';
