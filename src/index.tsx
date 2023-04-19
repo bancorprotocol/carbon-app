9c15b414b3727f42a83025e9c8c0b0c2f4af23ae
Oz Nasi <oznasi1@gmail.com>
2023-04-19 18:17:46 +0300

    Add sentry in production mode

--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,25 @@ import { LazyMotion } from 'libs/motion';
 import { QueryProvider } from 'libs/queries';
 import { StoreProvider } from 'store';
 
+const SENTRY_DSN =
+  process.env.NODE_ENV === 'production'
+    ? 'https://3bd709257f3946c29dbdeb08181c6eae@o1335274.ingest.sentry.io/4505034453483520'
+    : undefined;
+
+if (SENTRY_DSN) {
+  import('@sentry/react').then((Sentry) => {
+    Sentry.init({
+      dsn: SENTRY_DSN,
+      integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
+      // Performance Monitoring
+      tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+      // Session Replay
+      replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+      replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+    });
+  });
+}
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
