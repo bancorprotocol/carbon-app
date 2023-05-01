36cbff8f887452ecef08f1017565d227622ed4a8
Doron Zavelevsky <zavelevsky@gmail.com>
2023-05-01 17:53:11 +0300

    debugging cloudflare

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,18 +1,21 @@
 const restrictPagesByIp = async ({ request, next, env }) => {
   const url = new URL(request.url);
-  if (url.includes('carbon-app-csq.pages.dev')) {
-    const allowedIpsStr = env.ALLOWED_IPS;
-    if (allowedIpsStr) {
-      const allowedIps = allowedIpsStr.split(',').filter((ip) => ip !== '');
-      const clientIP = request.headers.get('CF-Connecting-IP');
-      if (clientIP && allowedIps.includes(clientIP)) {
-        return next();
-      }
-      return new Response('You are not allowed to access this page', {
-        status: 403,
-      });
-    }
-  }
+  console.log('url', url);
+  console.log('ips', env?.ALLOWED_IPS);
+  console.log('client ip', request?.headers?.get('CF-Connecting-IP'));
+  // if (url.includes('carbon-app-csq.pages.dev')) {
+  //   const allowedIpsStr = env.ALLOWED_IPS;
+  //   if (allowedIpsStr) {
+  //     const allowedIps = allowedIpsStr.split(',').filter((ip) => ip !== '');
+  //     const clientIP = request.headers.get('CF-Connecting-IP');
+  //     if (clientIP && allowedIps.includes(clientIP)) {
+  //       return next();
+  //     }
+  //     return new Response('You are not allowed to access this page', {
+  //       status: 403,
+  //     });
+  //   }
+  // }
   next();
 };
 
