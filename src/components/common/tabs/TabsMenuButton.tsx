9a0098cf87cfbd134e3571fec87f961a1be0db8d
Oz Nasi <oznasi1@gmail.com>
2023-05-02 13:30:42 +0300

    Remove optional className

--- src/components/common/tabs/TabsMenuButton.tsx
+++ src/components/common/tabs/TabsMenuButton.tsx
@@ -3,7 +3,6 @@ import { Button, ButtonProps } from 'components/common/button';
 
 type Props = ButtonProps & {
   isActive?: boolean;
-  className?: string;
 };
 
 export const TabsMenuButton: FC<Props> = ({ children, isActive, ...props }) => {
