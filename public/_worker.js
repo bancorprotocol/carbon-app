2b9cfe54cf88422900d9174dd3a05a391873cf6c
Jan Langheimer <langheimer@gmail.com>
2023-05-04 12:03:49 +0200

    add api worker test

--- public/_worker.js
+++ public/_worker.js
@@ -1,23 +1,54 @@
 /* eslint-disable import/no-anonymous-default-export */
+import axios from 'axios';
+
+const isIpBlocked = (request, env) => {
+  const RESTRICTED_DOMAIN = '.pages.dev';
+  const { hostname } = new URL(request.url);
+  const ALLOWED_IPS = env.ALLOWED_IPS;
+  if (hostname.includes(RESTRICTED_DOMAIN) && ALLOWED_IPS) {
+    const allowedIps = ALLOWED_IPS.split(',').filter((ip) => ip !== '');
+    const clientIP = request.headers.get('CF-Connecting-IP');
+    if (!allowedIps.includes(clientIP)) {
+      return new Response(`IP ${clientIP} isn't allowed to access this page`, {
+        status: 403,
+      });
+    }
+  }
+};
+
+const getPriceByAddress = async (env, address) => {
+  let response;
+  try {
+    response = await axios.get(
+      'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+      {
+        headers: {
+          'X-CMC_PRO_API_KEY': env.CMC_API_KEY,
+        },
+      }
+    );
+    return new Response(response, { status: 200 });
+  } catch (ex) {
+    return new Response(ex.message, { status: 500 });
+  }
+};
+
 export default {
   async fetch(request, env) {
-    const RESTRICTED_DOMAIN = '.pages.dev';
-    const url = new URL(request.url);
-    const ALLOWED_IPS = env.ALLOWED_IPS;
-    if (url.hostname.includes(RESTRICTED_DOMAIN) && ALLOWED_IPS) {
-      const allowedIps = ALLOWED_IPS.split(',').filter((ip) => ip !== '');
-      const clientIP = request.headers.get('CF-Connecting-IP');
-      if (!allowedIps.includes(clientIP)) {
-        return new Response(
-          `IP ${clientIP} isn't allowed to access this page`,
-          { status: 403 }
-        );
-      }
+    const isIpBlockedResponse = isIpBlocked(request, env);
+    if (isIpBlockedResponse) {
+      return isIpBlockedResponse;
     }
 
-    if (url.pathname.startsWith('/api/')) {
-      // TODO: Add your custom /api/* logic here.
-      return new Response('Ok');
+    const { pathname } = new URL(request.url);
+
+    if (pathname.startsWith('/api/')) {
+      switch (pathname) {
+        case '/api/price':
+          return getPriceByAddress();
+        default:
+          break;
+      }
     }
 
     return env.ASSETS.fetch(request);
