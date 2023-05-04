6074572770a0d95db6ea6ba2660aec9049f2265e
Jan Langheimer <langheimer@gmail.com>
2023-05-04 17:00:25 +0200

    cloudflare functions ts cleanup

--- cfFunctions/index.ts
+++ cfFunctions/index.ts
@@ -2,42 +2,20 @@ import type { CFWorkerEnv } from './types';
 import { isIpBlocked } from './ipBlock';
 import { getPriceByAddress } from './api/price';
 
-const fetch = {
-  async fetch(request: Request, env: CFWorkerEnv) {
-    const isIpBlockedResponse = isIpBlocked(request, env);
-    if (isIpBlockedResponse) {
-      return isIpBlockedResponse;
+export async function fetch(request: Request, env: CFWorkerEnv) {
+  const isIpBlockedResponse = isIpBlocked(request, env);
+  if (isIpBlockedResponse) {
+    return isIpBlockedResponse;
+  }
+
+  const { pathname } = new URL(request.url);
+  if (pathname.startsWith('/api/')) {
+    if (pathname.startsWith('/api/price/0x')) {
+      return getPriceByAddress(request, env);
     }
 
-    const { pathname } = new URL(request.url);
-    if (pathname.startsWith('/api/')) {
-      if (pathname.startsWith('/api/price/0x')) {
-        return getPriceByAddress(request, env);
-      }
+    return new Response('api endpoint not found', { status: 404 });
+  }
 
-      return new Response('api endpoint not found', { status: 404 });
-    }
-
-    return env.ASSETS.fetch(request);
-  },
-};
-
-export default {
-  async fetch(request: Request, env: CFWorkerEnv) {
-    const isIpBlockedResponse = isIpBlocked(request, env);
-    if (isIpBlockedResponse) {
-      return isIpBlockedResponse;
-    }
-
-    const { pathname } = new URL(request.url);
-    if (pathname.startsWith('/api/')) {
-      if (pathname.startsWith('/api/price/0x')) {
-        return getPriceByAddress(request, env);
-      }
-
-      return new Response('not found', { status: 404 });
-    }
-
-    return env.ASSETS.fetch(request);
-  },
-};
+  return env.ASSETS.fetch(request);
+}
