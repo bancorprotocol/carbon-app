845fcd8a0bc2c8271f93e033f3929eca8e4fbc85
Jan Langheimer <langheimer@gmail.com>
2023-05-04 17:32:18 +0200

    functions non advanced test

--- functions/api/marketrate/[address].ts
+++ functions/api/marketrate/[address].ts
@@ -6,8 +6,12 @@ export const onRequest: PagesFunction<CFWorkerEnv> = async ({
   env,
   params: { address },
 }) => {
-  if (typeof address !== 'string') {
-    return new Response('address is not a string', { status: 400 });
+  if (
+    typeof address !== 'string' ||
+    !address.startsWith('0x') ||
+    address.length !== 42
+  ) {
+    return new Response('address is not valid', { status: 400 });
   }
 
   return getPriceByAddress(env, url, address);
