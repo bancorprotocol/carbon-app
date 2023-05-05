4c9d1f0a26f7461a942ab8620ba098f44152bf83
Jan Langheimer <langheimer@gmail.com>
2023-05-05 11:50:35 +0200

    add coingecko api

--- src/functions/api/price/getCoinGeckoPriceByAddress.ts
+++ src/functions/api/price/getCoinGeckoPriceByAddress.ts
@@ -11,35 +11,37 @@ export const getCoinGeckoPriceByAddress = async (
     `${baseUrl}token_price/ethereum?contract_addresses=${address}&vs_currencies=${convert}`
   );
 
-  const json = await res.json<{
-    [k in string]: {
-      [k in string]: number;
-    };
-  }>();
-  const firstKey = Object.keys(json)[0];
-  if (!firstKey || firstKey === 'error' || json[firstKey] === undefined) {
-    throw new Error(
-      (json.error || 'Unknown error') +
-        ' | getCoinGeckoPriceByAddress: ' +
-        address
-    );
-  }
+  return await res.json();
 
-  const prices: { [k in string]: { price: number; timestamp: number } } = {};
-  Object.keys(json[firstKey]).forEach((c) => {
-    prices[c.toUpperCase()] = {
-      price: json[firstKey][c],
-      timestamp: new Date().getTime(),
-    };
-  });
-
-  if (Object.keys(prices).length === 0) {
-    throw new Error(
-      'Internal error: failed to get prices' +
-        ' | getCoinGeckoPriceByAddress: ' +
-        address
-    );
-  }
-
-  return prices;
+  // const json = await res.json<{
+  //   [k in string]: {
+  //     [k in string]: number;
+  //   };
+  // }>();
+  // const firstKey = Object.keys(json)[0];
+  // if (!firstKey || firstKey === 'error' || json[firstKey] === undefined) {
+  //   throw new Error(
+  //     (json.error || 'Unknown error') +
+  //       ' | getCoinGeckoPriceByAddress: ' +
+  //       address
+  //   );
+  // }
+  //
+  // const prices: { [k in string]: { price: number; timestamp: number } } = {};
+  // Object.keys(json[firstKey]).forEach((c) => {
+  //   prices[c.toUpperCase()] = {
+  //     price: json[firstKey][c],
+  //     timestamp: new Date().getTime(),
+  //   };
+  // });
+  //
+  // if (Object.keys(prices).length === 0) {
+  //   throw new Error(
+  //     'Internal error: failed to get prices' +
+  //       ' | getCoinGeckoPriceByAddress: ' +
+  //       address
+  //   );
+  // }
+  //
+  // return prices;
 };
