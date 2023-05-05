d890423ba94e5dbd6596a4e5f14dbf2ba55632d6
Jan Langheimer <langheimer@gmail.com>
2023-05-05 12:29:25 +0200

    add coingecko api

--- src/functions/api/price/getCoinGeckoPriceByAddress.ts
+++ src/functions/api/price/getCoinGeckoPriceByAddress.ts
@@ -7,46 +7,49 @@ export const getCoinGeckoPriceByAddress = async (
   address: string,
   convert: string
 ) => {
-  const res = await fetch(
-    `${baseUrl}token_price/ethereum?contract_addresses=${address}&vs_currencies=${convert}`,
-    {
-      headers: {
-        'content-type': 'application/json',
-      },
+  try {
+    const res = await fetch(
+      `${baseUrl}token_price/ethereum?contract_addresses=${address}&vs_currencies=${convert}`,
+      {
+        headers: {
+          'content-type': 'application/json',
+        },
+      }
+    );
+    return res.statusText;
+
+    const json = await res.json<{
+      [k in string]: {
+        [k in string]: number;
+      };
+    }>();
+    const firstKey = Object.keys(json)[0];
+    if (!firstKey || firstKey === 'error' || json[firstKey] === undefined) {
+      throw new Error(
+        (json.error || 'Unknown error') +
+          ' | getCoinGeckoPriceByAddress: ' +
+          address
+      );
     }
-  );
 
-  return await res.text();
+    const prices: { [k in string]: { price: number; timestamp: number } } = {};
+    Object.keys(json[firstKey]).forEach((c) => {
+      prices[c.toUpperCase()] = {
+        price: json[firstKey][c],
+        timestamp: new Date().getTime(),
+      };
+    });
 
-  // const json = await res.json<{
-  //   [k in string]: {
-  //     [k in string]: number;
-  //   };
-  // }>();
-  // const firstKey = Object.keys(json)[0];
-  // if (!firstKey || firstKey === 'error' || json[firstKey] === undefined) {
-  //   throw new Error(
-  //     (json.error || 'Unknown error') +
-  //       ' | getCoinGeckoPriceByAddress: ' +
-  //       address
-  //   );
-  // }
-  //
-  // const prices: { [k in string]: { price: number; timestamp: number } } = {};
-  // Object.keys(json[firstKey]).forEach((c) => {
-  //   prices[c.toUpperCase()] = {
-  //     price: json[firstKey][c],
-  //     timestamp: new Date().getTime(),
-  //   };
-  // });
-  //
-  // if (Object.keys(prices).length === 0) {
-  //   throw new Error(
-  //     'Internal error: failed to get prices' +
-  //       ' | getCoinGeckoPriceByAddress: ' +
-  //       address
-  //   );
-  // }
-  //
-  // return prices;
+    if (Object.keys(prices).length === 0) {
+      throw new Error('Internal error: failed to get prices');
+    }
+
+    return prices;
+  } catch (error: any) {
+    throw new Error(
+      (error.message || 'Internal error: failed to get prices') +
+        ' | getCoinGeckoPriceByAddress: ' +
+        address
+    );
+  }
 };
