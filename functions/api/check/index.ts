abfc4f3a8bad60b13ef9685520c6da519da71f3a
Jan Langheimer <langheimer@gmail.com>
2023-05-09 15:48:02 +0200

    add api/check endpoint

--- functions/api/check/index.ts
+++ functions/api/check/index.ts
@@ -1,12 +1,22 @@
 import { CFWorkerEnv } from './../../../src/functions';
 
+const BLOCKED_COUNTRIES = ['US'];
+
 export const onRequestGet: PagesFunction<CFWorkerEnv> = async ({ request }) => {
   const clientCountry = request.headers.get('CF-IPCountry') || '';
+  if (BLOCKED_COUNTRIES.includes(clientCountry)) {
+    return new Response(JSON.stringify(true), {
+      status: 200,
+      headers: {
+        'content-type': 'application/json',
+      },
+    });
+  }
 
-  return new Response('CODE: ' + clientCountry, {
+  return new Response(JSON.stringify(false), {
     status: 200,
     headers: {
-      'content-type': 'text/plain',
+      'content-type': 'application/json',
     },
   });
 };
