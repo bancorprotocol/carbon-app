21bd36772e9617b8ccdc32510906eda5b44e012d
Jan Langheimer <langheimer@gmail.com>
2023-05-09 11:13:48 +0200

    fix ip block

--- src/functions/ipBlock.ts
+++ src/functions/ipBlock.ts
@@ -1,10 +1,10 @@
 import type { CFWorkerEnv } from 'functions/types';
 
 export const isIpBlocked = (request: Request, env: CFWorkerEnv) => {
-  const RESTRICTED_DOMAIN = '.pages.dev';
+  const RESTRICTED_DOMAIN = 'carbon-app-csq.pages.dev';
   const { hostname } = new URL(request.url);
   const ALLOWED_IPS = env.ALLOWED_IPS;
-  if (hostname.includes(RESTRICTED_DOMAIN) && ALLOWED_IPS) {
+  if (hostname === RESTRICTED_DOMAIN && ALLOWED_IPS) {
     const allowedIps = ALLOWED_IPS.split(',').filter((ip) => ip !== '');
     const clientIP = request.headers.get('CF-Connecting-IP') || '';
     if (!allowedIps.includes(clientIP)) {
