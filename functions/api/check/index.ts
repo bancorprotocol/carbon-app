d3f61c39dc46dc2148153c9396e621cd68e85605
Jan Langheimer <langheimer@gmail.com>
2023-05-09 14:34:58 +0200

    add api/check endpoint

--- functions/api/check/index.ts
+++ functions/api/check/index.ts
@@ -3,7 +3,7 @@ import { CFWorkerEnv } from './../../../src/functions';
 export const onRequestGet: PagesFunction<CFWorkerEnv> = async ({ request }) => {
   const clientCountry = request.headers.get('CF-IPCountry') || '';
 
-  return new Response(clientCountry, {
+  return new Response('CODE: ' + clientCountry, {
     status: 200,
     headers: {
       'content-type': 'text/plain',
