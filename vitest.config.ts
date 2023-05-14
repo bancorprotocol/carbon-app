25be6c4894b61a6c9ff72bd93c615e09b6bbc6d3
Oz Nasi <oznasi1@gmail.com>
2023-05-14 10:39:24 +0300

    Exclude e2e test from vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vitest/config';
+import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     environment: 'jsdom',
+    exclude: [...configDefaults.exclude, 'e2e/*'],
   },
 });
