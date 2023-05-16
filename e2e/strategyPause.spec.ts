4225d1bba389ada3809d0ecc55e735b051a0b35a
Oz Nasi <oznasi1@gmail.com>
2023-05-16 17:46:09 +0300

    Add strategy pause modal snapshot

--- e2e/strategyPause.spec.ts
+++ e2e/strategyPause.spec.ts
@@ -1,15 +1,15 @@
 import { test, expect } from '@playwright/test';
 
-test('Strategy delete modal snapshot', async ({ page }) => {
+test('Strategy pause modal snapshot', async ({ page }) => {
   await page.goto('http://localhost:3000', { waitUntil: 'networkidle' });
   await page.getByRole('button', { name: 'Accept All Cookies' }).click();
 
   await page.getByRole('button', { name: 'Manage' }).first().click();
-  await page.getByRole('button', { name: 'Delete Strategy' }).first().click();
+  await page.getByRole('button', { name: 'Pause Strategy' }).first().click();
   await page
     .getByRole('button', { name: 'Create Strategy' })
     .first()
     .scrollIntoViewIfNeeded();
 
-  await expect(page).toHaveScreenshot('strategy-delete.png');
+  await expect(page).toHaveScreenshot('strategy-pause.png');
 });
