2312cbabe21d40fe8de6d784ee1a1c90b315111e
Jan Langheimer <langheimer@gmail.com>
2023-05-18 09:06:00 +0200

    add dev mode

--- src/utils/carbonApi.ts
+++ src/utils/carbonApi.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { FiatPriceDict, FiatSymbol } from 'store/useFiatCurrencyStore';
 
 const BASE_URL = '/api/';
 
@@ -9,4 +10,28 @@ const carbonApiAxios = axios.create({
 carbonApiAxios.defaults.headers.common['x-carbon-auth-key'] =
   import.meta.env.VITE_CARBON_API_KEY;
 
-export { carbonApiAxios };
+const carbonApi = {
+  getCheck: async (): Promise<boolean> => {
+    if (import.meta.env.VITE_DEV_MODE) {
+      return false;
+    }
+    const { data } = await carbonApiAxios.get<boolean>('/check');
+    return data;
+  },
+  getMarketRate: async (
+    address: string,
+    convert: readonly FiatSymbol[]
+  ): Promise<FiatPriceDict> => {
+    const {
+      data: { data },
+    } = await carbonApiAxios.get<{ data: FiatPriceDict }>(
+      `marketrate/${address}`,
+      {
+        params: { convert: convert.join(',') },
+      }
+    );
+    return data;
+  },
+};
+
+export { carbonApiAxios, carbonApi };
