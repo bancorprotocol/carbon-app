6513a76ed06e9a2b7513b36406d47c41e2e510b3
Oz Nasi <oznasi1@gmail.com>
2023-05-24 10:26:28 +0300

    Enable pause & delete snapshots tests

--- e2e/strategyDelete.spec.ts
+++ e2e/strategyDelete.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test.skip('Strategy delete modal snapshot', async ({ page }) => {
+test('Strategy delete modal snapshot', async ({ page }) => {
   await page.goto('http://localhost:3000', { waitUntil: 'networkidle' });
 
   await page.getByRole('button', { name: 'Manage' }).first().click();
--- e2e/strategyPause.spec.ts
+++ e2e/strategyPause.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test.skip('Strategy pause modal snapshot', async ({ page }) => {
+test('Strategy pause modal snapshot', async ({ page }) => {
   await page.goto('http://localhost:3000', { waitUntil: 'networkidle' });
   await page.getByRole('button', { name: 'Manage' }).first().click();
   await page.getByRole('button', { name: 'Pause Strategy' }).first().click();
