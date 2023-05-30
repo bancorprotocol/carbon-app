db39c821374a631a5abcf1cd2ba49b0d144163fd
Oz Nasi <oznasi1@gmail.com>
2023-05-30 12:28:57 +0300

    Added chromium

--- e2e/trade.spec.ts
+++ e2e/trade.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('Trade snapshot', async ({ page }) => {
   await page.goto('/trade', { waitUntil: 'networkidle' });
+  // await injectAxe(page);
   // await page.waitForSelector('div#trade-content');
   // await page.waitForLoadState('domcontentloaded');
   const error = await page.locator('#networkError');
