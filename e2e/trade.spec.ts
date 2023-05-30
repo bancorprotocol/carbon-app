1842efc8d9f42bee4c1ea24135de41145c4fbfe5
Oz Nasi <oznasi1@gmail.com>
2023-05-30 12:49:29 +0300

    Add static wait of 30 sec

--- e2e/trade.spec.ts
+++ e2e/trade.spec.ts
@@ -1,10 +1,14 @@
 import { test, expect } from '@playwright/test';
+import { wait } from '../src/utils/helpers';
 
 test('Trade snapshot', async ({ page }) => {
+  await wait(30000);
   await page.goto('/trade', { waitUntil: 'networkidle' });
+  await wait(30000);
   // await injectAxe(page);
   // await page.waitForSelector('div#trade-content');
   // await page.waitForLoadState('domcontentloaded');
   const error = await page.locator('#networkError');
+  await wait(30000);
   await expect(error).toBeVisible();
 });
