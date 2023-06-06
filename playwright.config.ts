eb942a0ae8750da944dee1e4bbba02e1b56aa5bc
Oz Nasi <oznasi1@gmail.com>
2023-06-06 13:44:17 +0300

    Update config

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,50 +5,64 @@ import { defineConfig, devices } from '@playwright/test';
  * https://github.com/motdotla/dotenv
  */
 // require('dotenv').config();
-
 const port = 3000;
 const baseURL = `http://localhost:${port}`;
-
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
 export default defineConfig({
   testDir: './e2e',
-  timeout: 30 * 1000,
+  /* Run tests in files in parallel */
   fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  // globalTeardown: require.resolve('./e2e/global-teardown'),
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL,
-    // storageState: 'e2e/user.json',
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    // baseURL: 'http://127.0.0.1:3000',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    viewport: { width: 1280, height: 720 },
   },
-  expect: {
-    timeout: 2 * 60 * 1000,
-    toHaveScreenshot: {
-      threshold: 0.2,
-      maxDiffPixelRatio: 0.01,
-    },
-  },
+
+  /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-      use: {
-        ...devices['Desktop Chrome'],
-      },
+      use: { ...devices['Desktop Chrome'] },
     },
+    /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
     //   use: { ...devices['Pixel 5'] },
-    //   dependencies: ['setup'],
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ..devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
+
+  /* Run your local dev server before starting the tests */
   webServer: {
     port,
-    command: `yarn build && yarn vite preview --port ${port}`,
+    command: `yarn build && yarn serve --port ${port}`,
     reuseExistingServer: !process.env.CI,
   },
 });
