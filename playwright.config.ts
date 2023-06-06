84002c94501e6ac74fb2a1c03ad8b93a957f37a8
Oz Nasi <oznasi1@gmail.com>
2023-06-06 13:20:29 +0300

    Fix tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,17 +36,6 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
