c86ec9b6713f9b59469071911e700f63f3f5db50
Oz Nasi <oznasi1@gmail.com>
2023-06-06 13:38:39 +0300

    Add another test - no network

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -6,3 +6,9 @@ test('has title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Carbon/);
 });
+
+test('Trade', async ({ page }) => {
+  await page.goto(`${page}/`, { waitUntil: 'networkidle' });
+  const element = await page.getByText('Network Error');
+  await expect(element !== undefined).toBeTruthy();
+});
