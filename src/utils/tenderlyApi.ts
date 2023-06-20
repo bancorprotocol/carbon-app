211a8633442634c3b9e831542c16fa6109a8a66f
Oz Nasi <oznasi1@gmail.com>
2023-06-20 17:53:02 +0300

    Update tenderly access key

--- src/utils/tenderlyApi.ts
+++ src/utils/tenderlyApi.ts
@@ -7,13 +7,13 @@ const projectUrl = `account/bancor/project/frontend-forks`;
 const e2eTestBaseForkId = '5ac3ffc6-abcd-4fbd-8430-069d927cbff9';
 const defaultNewForkName = 'e2e-disposable';
 console.log(
-  !!env.TENDERLY_ACCESS_KEY,
-  '-=-=-=-=-=- env.TENDERLY_ACCESS_KEY -=-=-=-=-=-'
+  !!process.env.VITE_TENDERLY_ACCESS_KEY,
+  '-=-=-=-=-=- env.VITE_TENDERLY_ACCESS_KEY -=-=-=-=-=-'
 );
 const axiosOnTenderly = axios.create({
   baseURL: 'https://api.tenderly.co/api/v1',
   headers: {
-    'X-Access-Key': env.TENDERLY_ACCESS_KEY || '',
+    'X-Access-Key': process.env.VITE_TENDERLY_ACCESS_KEY || '',
     'Content-Type': 'application/json',
   },
 });
