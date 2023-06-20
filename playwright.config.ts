b7f0593283bdf769c49e15cc0345f9f1118e1df0
Oz Nasi <oznasi1@gmail.com>
2023-06-20 13:51:22 +0300

    Update maxDiffPixelRatio

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ const baseURL = `http://localhost:${port}`;
 export default defineConfig({
   testDir: './e2e',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -34,7 +34,7 @@ export default defineConfig({
   expect: {
     toHaveScreenshot: {
       threshold: 0.2,
-      maxDiffPixelRatio: 0.2,
+      maxDiffPixelRatio: 0.02,
     },
   },
   /* Configure projects for major browsers */
