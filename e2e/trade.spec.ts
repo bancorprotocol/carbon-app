e65120966f2600c141d3e46c492955479e90fabb
Oz Nasi <oznasi1@gmail.com>
2023-06-20 09:33:28 +0300

    Update tests & check ci

--- e2e/trade.spec.ts
+++ e2e/trade.spec.ts
@@ -1,14 +1,37 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('/');
+test.describe('Navigate to page', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(`/debug`);
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Carbon/);
-});
+    // forkId = await duplicateFork();
+    const forkId = '5ac3ffc6-abcd-4fbd-8430-069d927cbff9';
+    console.log(forkId, '-=-=-=-=-=- Fork Created -=-=-=-=-=-');
+    // process.env.ForkId = forkId;
 
-test('Trade', async ({ page }) => {
-  await page.goto(`${page}/`, { waitUntil: 'networkidle' });
-  const element = await page.getByText('Network Error');
-  await expect(element !== undefined).toBeTruthy();
+    await page
+      .locator('#imposterAccount')
+      .fill('0x75e89d5979e4f6fba9f97c104c2f0afb3f1dcb88');
+    await page.locator('#saveImposter').click();
+
+    await page
+      .locator('#rpcUrl')
+      .fill(`https://rpc.tenderly.co/fork/${forkId}`);
+    await page
+      .locator('#controller')
+      .fill('0xC537e898CD774e2dCBa3B14Ea6f34C93d5eA45e1');
+    await page
+      .locator('#voucher')
+      .fill('0x3660F04B79751e31128f6378eAC70807e38f554E');
+    await page.locator('#checkbox').first().click();
+
+    await page.locator('#saveRpc').click();
+  });
+
+  test('Trade snapshot', async ({ page }) => {
+    await page.goto('/trade', { waitUntil: 'networkidle' });
+    await page.waitForSelector('div#trade-content');
+    await page.waitForLoadState('domcontentloaded');
+    await expect(page).toHaveScreenshot('trade.png');
+  });
 });
--- playwright.config.ts
+++ playwright.config.ts
@@ -31,7 +31,12 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
-
+  expect: {
+    toHaveScreenshot: {
+      threshold: 0.2,
+      maxDiffPixelRatio: 0.01,
+    },
+  },
   /* Configure projects for major browsers */
   projects: [
     {
