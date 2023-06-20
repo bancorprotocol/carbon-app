67fa1b36929f94b7d748a27d642b7ca45d6cdf32
Oz Nasi <oznasi1@gmail.com>
2023-06-20 15:12:17 +0300

    Enable strategyOverview tests

--- e2e/strategyOverview.spec.ts
+++ e2e/strategyOverview.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Overview strategy', () => {
-  test.skip('Strategy overview snapshot', async ({ page }) => {
+  test('Strategy overview snapshot', async ({ page }) => {
     await page.goto('/', { waitUntil: 'networkidle' });
     await page.locator('#strategies').isVisible();
     await page.waitForLoadState('domcontentloaded');
