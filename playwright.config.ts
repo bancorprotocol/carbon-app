2d49275dd9035be211d38d5a10c2de3354f72100
Oz Nasi <oznasi1@gmail.com>
2023-06-20 09:47:14 +0300

    Check ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,13 +23,13 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  globalSetup: require.resolve('./e2e/global-setup'),
+  globalTeardown: require.resolve('./e2e/global-teardown'),
   use: {
     baseURL,
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    storageState: 'e2e/user.json',
+    viewport: { width: 1280, height: 720 },
   },
   expect: {
     toHaveScreenshot: {
