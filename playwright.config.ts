edb0e831c0f053fbbefe42292cfaf2d759bff3b0
Oz Nasi <oznasi1@gmail.com>
2023-06-20 12:08:16 +0300

    Update snapshot & config

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ const baseURL = `http://localhost:${port}`;
 export default defineConfig({
   testDir: './e2e',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
