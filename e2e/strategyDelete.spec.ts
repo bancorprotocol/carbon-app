6ebc308e0b6c4362fa306e29b71100c6eb7505d1
Oz Nasi <oznasi1@gmail.com>
2023-06-20 11:56:02 +0300

    Add delete strategy tests

--- e2e/strategyDelete.spec.ts
+++ e2e/strategyDelete.spec.ts
@@ -1,29 +1,34 @@
 import { test, expect } from '@playwright/test';
 
-test.skip('Strategy delete modal snapshot', async ({ page }) => {
-  await page.goto('/', { waitUntil: 'networkidle' });
+test.describe('Delete strategy', () => {
+  test('Strategy delete modal snapshot', async ({ page }) => {
+    await page.goto('/', { waitUntil: 'networkidle' });
 
-  await page.getByRole('button', { name: 'Manage' }).first().click();
-  await page.getByRole('button', { name: 'Delete Strategy' }).first().click();
-  await page
-    .getByRole('button', { name: 'Create Strategy' })
-    .first()
-    .scrollIntoViewIfNeeded();
+    await page.getByRole('button', { name: 'Manage' }).first().click();
+    await page.getByRole('button', { name: 'Delete Strategy' }).first().click();
+    await page
+      .getByRole('button', { name: 'Create Strategy' })
+      .first()
+      .scrollIntoViewIfNeeded();
 
-  expect(await page.locator('#modal').screenshot()).toMatchSnapshot(
-    'strategy-delete.png'
-  );
-});
+    expect(await page.locator('#modal').screenshot()).toMatchSnapshot(
+      'strategy-delete.png'
+    );
+  });
 
-test.skip('Strategy deleted successfully', async ({ page }) => {
-  await page.goto('/', { waitUntil: 'networkidle' });
+  test.skip('Strategy deleted successfully', async ({ page }) => {
+    await page.goto('/', { waitUntil: 'networkidle' });
 
-  await page.getByRole('button', { name: 'Manage' }).first().click();
-  await page.getByRole('button', { name: 'Delete Strategy' }).first().click();
+    await page.getByRole('button', { name: 'Manage' }).first().click();
+    await page.getByRole('button', { name: 'Delete Strategy' }).first().click();
 
-  const modal = await page.locator('#modal');
-  await modal.getByRole('button', { name: 'Delete Strategy' }).first().click();
+    const modal = await page.locator('#modal');
+    await modal
+      .getByRole('button', { name: 'Delete Strategy' })
+      .first()
+      .click();
 
-  await modal.waitFor({ state: 'detached' });
-  await expect(page).toHaveScreenshot('strategy-delete-success.png');
+    await modal.waitFor({ state: 'detached' });
+    await expect(page).toHaveScreenshot('strategy-delete-success.png');
+  });
 });
--- e2e/trade.spec.ts
+++ e2e/trade.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('Navigate to page', () => {
+test.describe('Trade page', () => {
   test('Trade snapshot', async ({ page }) => {
     await page.goto('/trade', { waitUntil: 'networkidle' });
     await page.waitForSelector('div#trade-content');
