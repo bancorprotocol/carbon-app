99f033145b7c343a7acdb075d243826ee7f59a6c
Oz Nasi <oznasi1@gmail.com>
2023-06-20 12:46:57 +0300

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -45,6 +45,19 @@ export default defineConfig(({ mode }) => {
         env.SENTRY_PROJECT &&
         env.SENTRY_AUTH_TOKEN
       ),
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return id
+                .toString()
+                .split('node_modules/')[1]
+                .split('/')[0]
+                .toString();
+            }
+          },
+        },
+      },
     },
     resolve: {
       alias: {
