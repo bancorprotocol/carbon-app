0203c1e3a49397ffa7874572a4975046faf91460
Oz Nasi <oznasi1@gmail.com>
2023-06-25 18:04:02 +0300

    Return pause strategy tests

--- e2e/strategyPause.spec.ts
+++ e2e/strategyPause.spec.ts
@@ -1,29 +1,32 @@
-import { test, expect } from '@playwright/test';
+import { expect } from '@playwright/test';
+import { test } from './fixture';
 
 test.describe('Pause strategy', () => {
-  test.skip('Strategy pause modal snapshot', async ({ page }) => {
+  test('Strategy pause modal snapshot', async ({ page }) => {
     await page.goto('/', { waitUntil: 'networkidle' });
     await page.getByRole('button', { name: 'Manage' }).first().click();
     await page.getByRole('button', { name: 'Pause Strategy' }).first().click();
-    await page
-      .getByRole('button', { name: 'Create Strategy' })
-      .first()
-      .scrollIntoViewIfNeeded();
+    const pauseStrategyModal = await page.getByTestId('modal');
 
-    expect(await page.locator('#modal').screenshot()).toMatchSnapshot(
+    expect(await pauseStrategyModal.screenshot()).toMatchSnapshot(
       'strategy-pause.png'
     );
   });
 
-  test.skip('Strategy paused successfully', async ({ page }) => {
+  test.only('Strategy paused successfully', async ({ page }) => {
     await page.goto('/', { waitUntil: 'networkidle' });
     await page.getByRole('button', { name: 'Manage' }).first().click();
     await page.getByRole('button', { name: 'Pause Strategy' }).first().click();
 
-    const modal = await page.locator('#modal');
-    await modal.getByRole('button', { name: 'Pause Strategy' }).first().click();
-
-    await modal.waitFor({ state: 'detached' });
-    await expect(page).toHaveScreenshot('strategy-pause-success.png');
+    const pauseStrategyModal = await page.getByTestId('modal');
+    await pauseStrategyModal
+      .getByRole('button', { name: 'Pause Strategy' })
+      .first()
+      .click();
+    await pauseStrategyModal.waitFor({ state: 'hidden' });
+    const notification = await page.getByText(
+      'Your strategy was successfully paused.'
+    );
+    await expect(notification).toBeVisible();
   });
 });
