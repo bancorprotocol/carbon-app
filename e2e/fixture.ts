330b24590b84fe3674d1fe16e6bae39826877ce1
Oz Nasi <oznasi1@gmail.com>
2023-06-25 16:12:58 +0300

    Check ci

--- e2e/fixture.ts
+++ e2e/fixture.ts
@@ -1,9 +1,44 @@
-import { setupBeforeEach } from './test-setup';
 import { test as base } from '@playwright/test';
+import { deleteFork, duplicateFork } from '../src/utils/tenderlyApi';
 
 export const newTest = base.extend({
-  auth: async ({ page }, use) => {
-    await setupBeforeEach(page);
+  page: async ({ page }, use) => {
+    let forkId;
+
+    await page.goto(`/debug`);
+
+    try {
+      forkId = await duplicateFork();
+      console.log(forkId, '-=-=-=-=-=- Fork Created -=-=-=-=-=-');
+      process.env.ForkId = forkId;
+      await page.waitForLoadState('load');
+
+      await page
+        .locator('#imposterAccount')
+        .fill('0x75e89d5979e4f6fba9f97c104c2f0afb3f1dcb88');
+      await page.locator('#saveImposter').click();
+
+      await page
+        .locator('#rpcUrl')
+        .fill(`https://rpc.tenderly.co/fork/${forkId}`);
+      await page
+        .locator('#controller')
+        .fill('0xC537e898CD774e2dCBa3B14Ea6f34C93d5eA45e1');
+      await page
+        .locator('#voucher')
+        .fill('0x3660F04B79751e31128f6378eAC70807e38f554E');
+      await page.locator('#checkbox').first().click();
+
+      await page.locator('#saveRpc').click();
+
+      // await page.context().storageState({ path: 'user.json' as string });
+    } catch (error) {
+      console.log(
+        error,
+        `-=-=-=-=-=- global setup error - delete fork ${forkId}-=-=-=-=-=-`
+      );
+      await deleteFork(forkId);
+    }
     await use(page);
   },
 });
--- e2e/trade.spec.ts
+++ e2e/trade.spec.ts
@@ -1,15 +1,14 @@
 import { expect } from '@playwright/test';
-// import { setupAfterEach, setupBeforeEach } from './test-setup';
 import { newTest } from './fixture';
 
 newTest.describe('Trade page', () => {
-  newTest('Trade snapshot', async ({ auth }) => {
-    await auth.goto('/trade');
-    await auth
+  newTest('Trade snapshot', async ({ page }) => {
+    await page.goto('/trade');
+    await page
       .getByTestId('logo-animation')
       .last()
       .waitFor({ state: 'hidden' });
-
-    await expect(await auth.screenshot()).toMatchSnapshot('trade.png');
+    await page.waitForLoadState('load');
+    await expect(await page.screenshot()).toMatchSnapshot('trade.png');
   });
 });
