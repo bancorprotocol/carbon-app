1fb34526e173f43d0ceb2c0ac295cf507a6062cb
Oz Nasi <oznasi1@gmail.com>
2023-06-26 11:32:18 +0300

    Update tests

--- e2e/trade.spec.ts
+++ e2e/trade.spec.ts
@@ -1,15 +1,15 @@
 import { expect } from '@playwright/test';
 import { test } from './fixture';
 
-test.describe.only('Trade page', () => {
+test.describe('Trade page', () => {
   test('Trade snapshot', async ({ page }) => {
     await page.goto('/trade');
     await page.getByTestId('logoAnimation').last().waitFor({ state: 'hidden' });
+    await page.getByTestId('orderBookWidgetRate').waitFor({ state: 'visible' });
     await page
       .getByTestId('orderbookCell')
       .last()
-      .waitFor({ state: 'attached' });
-    await page.getByTestId('orderBookWidgetRate').waitFor({ state: 'visible' });
+      .waitFor({ state: 'visible' });
 
     await expect(
       await page.screenshot({
