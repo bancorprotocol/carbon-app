68b29b2628d46ffea7bf142da1ef01f9b2dca43e
Jan Langheimer <langheimer@gmail.com>
2023-08-25 12:05:29 +0200

    add no no countries to /check endpoint

--- functions/api/check/index.ts
+++ functions/api/check/index.ts
@@ -1,6 +1,35 @@
 import { CFWorkerEnv } from './../../../src/functions';
 
-const BLOCKED_COUNTRIES = [
+const BLOCKED_HOSTS = ['carbon-app-csq.pages.dev'];
+
+const NO_NO_COUNTRIES = [
+  'BY', // Belarus
+  'BI', // Burundi
+  'CF', // Central African Republic
+  'CG', // Congo
+  'CD', // Congo, Democratic Republic of the
+  'CU', // Cuba
+  'GN', // Guinea
+  'GW', // Guinea-Bissau
+  'IR', // Iran, Islamic Republic of
+  'IQ', // Iraq
+  'KP', // Korea, Democratic People's Republic of
+  'LB', // Lebanon
+  'LY', // Libyan Arab Jamahiriya
+  'ML', // Mali
+  'MM', // Myanmar
+  'RU', // Russian Federation
+  'SO', // Somalia
+  'SS', // South Sudan
+  'SD', // Sudan
+  'SY', // Syrian Arab Republic
+  'UA', // Ukraine
+  'VE', // Venezuela, Bolivarian Republic of
+  'YE', // Yemen
+  'ZW', // Zimbabwe
+];
+
+const LIMITED_COUNTRIES = [
   'US', // USA
   'PR', // Puerto Rico
   'AS', // American Samoa
@@ -10,8 +39,23 @@ const BLOCKED_COUNTRIES = [
 ];
 
 export const onRequestGet: PagesFunction<CFWorkerEnv> = async ({ request }) => {
+  const { hostname } = new URL(request.url);
+  const isBlockedHost = BLOCKED_HOSTS.includes(hostname);
+
+  if (isBlockedHost) {
+    return new Response(JSON.stringify(true), {
+      status: 200,
+      headers: {
+        'content-type': 'application/json',
+      },
+    });
+  }
+
   const clientCountry = request.headers.get('CF-IPCountry') || '';
-  if (BLOCKED_COUNTRIES.includes(clientCountry)) {
+  const isBlockedCountry =
+    NO_NO_COUNTRIES.concat(LIMITED_COUNTRIES).includes(clientCountry);
+
+  if (isBlockedCountry) {
     return new Response(JSON.stringify(true), {
       status: 200,
       headers: {
--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,4 +1,3 @@
 export { getPriceByAddress } from './api/price/getPriceByAddress';
-export { isIpBlocked } from './ipBlock';
 export { buildOptionsResponse, build403Response } from './responses';
 export type { CFWorkerEnv } from './types';
