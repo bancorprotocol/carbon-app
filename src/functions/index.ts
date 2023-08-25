4e9ad31c26dd9e0c788336471ce87f0666ee7f2f
Jan Langheimer <langheimer@gmail.com>
2023-08-25 15:57:10 +0200

    fix build error

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,3 +1,2 @@
-export { getPriceByAddress } from './api/price/getPriceByAddress';
 export { buildOptionsResponse, build403Response } from './responses';
 export type { CFWorkerEnv } from './types';
