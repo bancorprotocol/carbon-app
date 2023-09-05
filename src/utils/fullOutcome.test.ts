e28884caf3aa6366a322d27a1934b7129399d780
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2023-09-05 21:51:59 +0200

    setup unit test

---
+++ src/utils/fullOutcome.test.ts
@@ -0,0 +1,89 @@
+import { describe, it, expect } from 'vitest';
+import { AcquireAmountProps, geoMean, getAcquiredAmount } from './fullOutcome';
+import Decimal from 'decimal.js';
+
+describe('fullOutcome', () => {
+  describe('Geo Mean', () => {
+    it('should return undefined if min is greater or equal to max', () => {
+      expect(geoMean('100', '10')).toBeUndefined();
+    });
+
+    it('should return same price for limit price', () => {
+      expect(geoMean('100', '100')).toEqual(new Decimal('100'));
+    });
+
+    it('should return mean price for range price', () => {
+      const mean = new Decimal('158.1138830084189666');
+      expect(geoMean('100', '250')).toEqual(mean);
+    });
+  });
+
+  describe('Acquired Amount', () => {
+    const base: AcquireAmountProps = {
+      budget: '1',
+      min: '',
+      max: '',
+      price: '',
+      buy: true,
+    };
+    it('should return undefined if no budget', () => {
+      const input = {
+        buy: true,
+        price: '1',
+        min: '100',
+        max: '10',
+        budget: '',
+      };
+      expect(getAcquiredAmount(input)).toBeUndefined();
+    });
+    it('should return undefined if neither price nor min&max', () => {
+      const noPriceOrMinAndMax = { ...base, price: '', min: '', max: '' };
+      const noPriceAndMax = { ...base, price: '', min: '10', max: '' };
+      expect(getAcquiredAmount(noPriceOrMinAndMax)).toBeUndefined();
+      expect(getAcquiredAmount(noPriceAndMax)).toBeUndefined();
+    });
+    it('should return undefined if min is greater than max', () => {
+      const input = { ...base, price: '', min: '100', max: '10' };
+      expect(getAcquiredAmount(input)).toBeUndefined();
+    });
+    describe('Limit rate', () => {
+      it('[Buy] should return 3.1645569620253164557 with price 1580 & budget 5000', () => {
+        const input = { ...base, price: '1580', budget: '5000' };
+        expect(getAcquiredAmount(input)?.amount).toBe('3.1645569620253164557');
+      });
+      it('[Buy] should return 2 with price 1600 & budget 3200', () => {
+        const input = { ...base, price: '1600', budget: '3200' };
+        expect(getAcquiredAmount(input)?.amount).toBe('2');
+      });
+      it('[Buy] should return 3 with price 1500 & budget 4500 ', () => {
+        const input = { ...base, price: '1500', budget: '4500' };
+        expect(getAcquiredAmount(input)?.amount).toBe('3');
+      });
+      it('[Sell] should return 3260 with price 1630 & budget 2', () => {
+        const input = { ...base, buy: false, price: '1630', budget: '2' };
+        expect(getAcquiredAmount(input)?.amount).toBe('3260');
+      });
+    });
+    describe('Range rate', () => {
+      it('[Buy] should return 1.8353258709644941273 with min 1600, max 1900 & budget 3200', () => {
+        const input = {
+          ...base,
+          min: '1600',
+          max: '1900',
+          budget: '3200',
+        };
+        expect(getAcquiredAmount(input)?.amount).toBe('1.8353258709644941273');
+      });
+      it('[Sell] should return 4623.3105022267323378 with min 1800, max 1900 & budget 2.5', () => {
+        const input = {
+          ...base,
+          buy: false,
+          min: '1800',
+          max: '1900',
+          budget: '2.5',
+        };
+        expect(getAcquiredAmount(input)?.amount).toBe('4623.3105022267323378');
+      });
+    });
+  });
+});
