519a14e9e25b9a071e36d287d0f9f0b7dbb34d1e
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2023-09-08 17:43:44 +0200

    update vitest matcher

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'jsdom',
+    include: ['src/**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
   },
   plugins: [],
 });
