95167c66f97984b5b0fb6df74f9daba210d0ae89
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2023-09-11 10:29:49 +0200

    use binance address

--- e2e/pages/strategy.spec.ts
+++ e2e/pages/strategy.spec.ts
@@ -2,9 +2,9 @@ import { test } from '@playwright/test';
 import { screenshot } from '../utils/operators';
 
 test.describe('Strategies', () => {
-  test('Screenshot page', async ({ page }) => {
+  test('First Strategy Page', async ({ page }) => {
     await page.goto('/');
-    await page.getByTestId('strategies-list').waitFor({ state: 'visible' });
-    await screenshot(page, 'strategies');
+    await page.getByTestId('first-strategy').waitFor({ state: 'visible' });
+    await screenshot(page, 'first-strategy');
   });
 });
