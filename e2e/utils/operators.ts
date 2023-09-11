4abafd82c21e832f90406a4259371218670133d8
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2023-09-11 09:50:41 +0200

    add draft flag

--- e2e/utils/operators.ts
+++ e2e/utils/operators.ts
@@ -1,8 +1,11 @@
 import { Locator, Page } from 'playwright-core';
 
 export const isCI = !!process.env.CI && process.env.CI !== 'false';
+export const isDraft = !!process.env.DRAFT && process.env.DRAFT !== 'false';
 
+/** Utils to take screenshot on CI that are not draft */
 export const screenshot = (target: Page | Locator, name: string) => {
+  if (!isCI || isDraft) return;
   return target.screenshot({
     type: 'jpeg',
     path: `e2e/screenshots/${name}.jpg`,
