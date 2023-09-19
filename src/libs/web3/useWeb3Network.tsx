6a73eabaa216205a80f62baa68c754f82f21a62b
Tiago Silva <tfn.da.silva@gmail.com>
2023-09-19 16:50:27 +0100

    Remove GTM event when user is undefined

--- src/libs/web3/useWeb3Network.tsx
+++ src/libs/web3/useWeb3Network.tsx
@@ -9,7 +9,6 @@ import { useCallback, useEffect, useState } from 'react';
 import { lsService } from 'services/localeStorage';
 import { useStore } from 'store';
 import useAsyncEffect from 'use-async-effect';
-import { carbonEvents } from 'services/events';
 
 export const useWeb3Network = () => {
   const { isCountryBlocked, setSelectedWallet } = useStore();
@@ -57,12 +56,6 @@ export const useWeb3Network = () => {
         );
         if (success) {
           setSelectedWallet(isNativeAppBrowser.type);
-
-          carbonEvents.wallet.walletConnect({
-            address: undefined,
-            name: network?.name || '',
-          });
-
           return; // If successfully connected, stop further connection attempts
         }
       }
@@ -73,10 +66,6 @@ export const useWeb3Network = () => {
         const { success } = await attemptToConnectWallet(storedConnection);
         if (success) {
           setSelectedWallet(storedConnection);
-          carbonEvents.wallet.walletConnect({
-            address: undefined,
-            name: network?.name || '',
-          });
         }
       }
     }
