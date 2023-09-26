facbac6a9e61d814ce34062184cdba43ed1a6ffa
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2023-09-26 15:19:05 +0200

    fix empty element My strategies

--- src/pages/strategies/overview/index.tsx
+++ src/pages/strategies/overview/index.tsx
@@ -1,14 +1,16 @@
 import { StrategyContent } from 'components/strategies/overview';
 import { StrategyCreateFirst } from 'components/strategies/overview/StrategyCreateFirst';
+import { StrategyNotFound } from 'components/strategies/overview/StrategyNotFound';
 import { useStrategyCtx } from 'hooks/useStrategies';
 
 export const StrategiesOverviewPage = () => {
-  const { strategies, isLoading } = useStrategyCtx();
+  const { strategies, isLoading, search } = useStrategyCtx();
+  const emptyElement = search ? <StrategyNotFound /> : <StrategyCreateFirst />;
   return (
     <StrategyContent
       strategies={strategies}
       isLoading={isLoading}
-      emptyElement={<StrategyCreateFirst />}
+      emptyElement={emptyElement}
     />
   );
 };
