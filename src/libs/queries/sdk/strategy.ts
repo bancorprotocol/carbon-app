801ca22839fdc51635d557ee46591b7520dad0c5
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2023-10-04 10:44:07 +0200

    add marginalRate

--- src/libs/queries/sdk/strategy.ts
+++ src/libs/queries/sdk/strategy.ts
@@ -28,6 +28,7 @@ export interface Order {
   balance: string;
   startRate: string;
   endRate: string;
+  marginalRate: string;
 }
 
 export interface Strategy {
@@ -100,6 +101,7 @@ const buildStrategiesHelper = async ({
       balance: s.buyBudget,
       startRate: s.buyPriceLow,
       endRate: s.buyPriceHigh,
+      marginalRate: s.buyPriceMarginal,
     };
 
     // ATTENTION *****************************
@@ -109,6 +111,7 @@ const buildStrategiesHelper = async ({
       balance: s.sellBudget,
       startRate: s.sellPriceLow,
       endRate: s.sellPriceHigh,
+      marginalRate: s.sellPriceMarginal,
     };
 
     const roi = new BigNumber(roiData.find((r) => r.id === s.id)?.ROI || 0);
