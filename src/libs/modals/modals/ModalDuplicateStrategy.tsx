bc0c8bccd1c89d6066a13ade1108b2e74ce3e401
Tiago Silva <tfn.da.silva@gmail.com>
2023-10-20 14:47:36 +0100

    Change modal design, add undercut strategy logic

--- src/libs/modals/modals/ModalDuplicateStrategy.tsx
+++ src/libs/modals/modals/ModalDuplicateStrategy.tsx
@@ -3,11 +3,12 @@ import { useModal } from 'hooks/useModal';
 import { ModalOrMobileSheet } from 'libs/modals/ModalOrMobileSheet';
 import { useDuplicateStrategy } from 'components/strategies/create/useDuplicateStrategy';
 import { Strategy } from 'libs/queries';
-import { Link } from 'libs/routing';
 import { ReactComponent as IconCut } from 'assets/icons/cut.svg';
 import { ReactComponent as IconCopy } from 'assets/icons/copy.svg';
 import { buttonStyles } from 'components/common/button/buttonStyles';
 import { cn } from 'utils/helpers';
+import { Button } from 'components/common/button';
+import Decimal from 'decimal.js';
 
 export type ModalDuplicateStrategyData = {
   strategy: Strategy;
@@ -21,55 +22,77 @@ export const ModalDuplicateStrategy: ModalFC<ModalDuplicateStrategyData> = ({
   const { duplicate } = useDuplicateStrategy();
 
   const undercutStrategy = () => {
-    const strategySpread = strategy.order0;
-    console.log('Undercutting strategy');
+    const undercutDifference = 0.01;
+    const undercuttedStrategy = {
+      ...strategy,
+      order0: {
+        ...strategy.order0,
+        startRate: new Decimal(strategy.order0.startRate)
+          .times(1 + undercutDifference)
+          .toString(),
+        endRate: new Decimal(strategy.order0.endRate)
+          .times(1 + undercutDifference)
+          .toString(),
+      },
+      order1: {
+        ...strategy.order1,
+        startRate: new Decimal(strategy.order1.startRate)
+          .times(1 - undercutDifference)
+          .toString(),
+        endRate: new Decimal(strategy.order1.endRate)
+          .times(1 - undercutDifference)
+          .toString(),
+      },
+    };
+
+    closeModal(id);
+    duplicate(undercuttedStrategy);
+  };
+
+  const duplicateStrategy = () => {
+    closeModal(id);
+    duplicate(strategy);
   };
 
   return (
-    <ModalOrMobileSheet id={id}>
-      <h2 className={'my-3'}>Duplicate Strategy</h2>
-      <p className="text-secondary my-5 flex w-full items-center">
-        Select your option.
-      </p>
-      <article className="grid grid-cols-[auto] grid-rows-[auto_auto] gap-8 rounded bg-emphasis p-16">
-        <section className="grid grid-cols-[32px_1fr_auto] grid-rows-[auto_auto] gap-8 rounded bg-emphasis p-16">
-          <IconCopy className="row-span-2 self-center" />
-          <h3 className="grid text-14 font-weight-500">Copy as Is</h3>
-          <Link
-            onClick={() => duplicate(strategy)}
-            // disabled={isAwaiting || isProcessing}
-            // to={}
-            search={{ type: 'price' }}
-            className={cn(
-              'row-span-2 self-center',
-              buttonStyles({ variant: 'white' })
-            )}
-          >
-            Select
-          </Link>
-          <p className="text-12 text-white/80">
-            Set prices at 0.1% tighter spread and try to get filled ahead
-          </p>
-        </section>
-        <section className="grid grid-cols-[32px_1fr_auto] grid-rows-[auto_auto] gap-8 rounded bg-emphasis p-16">
-          <IconCut className="row-span-2 self-center" />
-          <h3 className="text-14 font-weight-500">Undercut the Strategy</h3>
-          <Link
-            onClick={undercutStrategy}
-            // disabled={isAwaiting || isProcessing}
-            // to={}
-            search={{ type: 'price' }}
-            className={cn(
-              'row-span-2 self-center',
-              buttonStyles({ variant: 'white' })
-            )}
-          >
-            Select
-          </Link>
-          <p className="text-12 text-white/80">
-            Set prices at 0.1% tighter spread and try to get filled ahead
-          </p>
-        </section>
+    <ModalOrMobileSheet id={id} title="Duplicate Strategy">
+      <p className="text-secondary font-weight-400">Select your option.</p>
+
+      <article className="grid grid-cols-[32px_1fr_auto] grid-rows-[auto_auto] gap-8 rounded bg-black/90 p-16">
+        <div className="row-span-2 flex h-32 w-32 items-center justify-center self-center rounded-full bg-green/25">
+          <IconCopy className="h-16 w-16 text-green" />
+        </div>
+        <h3 className="grid text-14 font-weight-500">Copy as Is</h3>
+        <Button
+          onClick={duplicateStrategy}
+          className={cn(
+            'row-span-2 self-center',
+            buttonStyles({ variant: 'white' })
+          )}
+        >
+          Select
+        </Button>
+        <p className="text-12 font-weight-400 text-white/60">
+          Duplicate the strategy with the existing values (price, budget)
+        </p>
+      </article>
+      <article className="grid grid-cols-[32px_1fr_auto] grid-rows-[auto_auto] gap-8 rounded bg-black/90 p-16">
+        <div className="row-span-2 flex h-32 w-32 items-center justify-center self-center rounded-full bg-green/25">
+          <IconCut className="h-16 w-16 text-green" />
+        </div>
+        <h3 className="grid text-14 font-weight-500">Undercut the Strategy</h3>
+        <Button
+          onClick={undercutStrategy}
+          className={cn(
+            'row-span-2 self-center',
+            buttonStyles({ variant: 'white' })
+          )}
+        >
+          Select
+        </Button>
+        <p className="text-12 text-white/80">
+          Set prices at 0.1% tighter spread and try to get filled ahead
+        </p>
       </article>
     </ModalOrMobileSheet>
   );
