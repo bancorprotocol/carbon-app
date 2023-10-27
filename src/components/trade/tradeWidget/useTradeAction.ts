c587729079af4087eb8184544c886bf04d3a7ca3
Doron Zavelevsky <zavelevsky@gmail.com>
2023-10-27 03:31:19 +0100

    fixed maxInput and minReturn calculation to handle small numbers

--- src/components/trade/tradeWidget/useTradeAction.ts
+++ src/components/trade/tradeWidget/useTradeAction.ts
@@ -42,7 +42,7 @@ export const useTradeAction = ({
   const calcMinReturn = useCallback(
     (amount: string) => {
       const slippageBn = new BigNumber(slippage).div(100);
-      return new BigNumber(1).minus(slippageBn).times(amount).toString();
+      return new BigNumber(1).minus(slippageBn).times(amount).toFixed();
     },
     [slippage]
   );
@@ -50,7 +50,7 @@ export const useTradeAction = ({
   const calcMaxInput = useCallback(
     (amount: string) => {
       const slippageBn = new BigNumber(slippage).div(100);
-      return new BigNumber(1).plus(slippageBn).times(amount).toString();
+      return new BigNumber(1).plus(slippageBn).times(amount).toFixed();
     },
     [slippage]
   );
