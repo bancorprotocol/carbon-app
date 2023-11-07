b26939429736bed3359cbe7b2049ec325d9c0d6f
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2023-11-07 17:17:54 +0100

    conditional budget

---
+++ src/components/strategies/create/symmetric/CreateSymmerticStrategyBudget.tsx
@@ -0,0 +1,109 @@
+import { FC } from 'react';
+import { Token } from 'libs/tokens';
+import { OrderCreate } from '../useOrder';
+import { UseQueryResult } from '@tanstack/react-query';
+import { TokenInputField } from 'components/common/TokenInputField/TokenInputField';
+import { ReactComponent as IconWarning } from 'assets/icons/warning.svg';
+import { ReactComponent as IconLink } from 'assets/icons/link.svg';
+import BigNumber from 'bignumber.js';
+import { SymmetricStrategyProps } from './CreateSymmetricStrategy';
+import { useComparePrice } from 'hooks/useComparePrice';
+
+export const CreateSymmerticStrategyBudget: FC<SymmetricStrategyProps> = (
+  props
+) => {
+  const {
+    base,
+    quote,
+    order0,
+    order1,
+    token0BalanceQuery,
+    token1BalanceQuery,
+  } = props;
+  const { baseFiat, quoteFiat } = useComparePrice({ base, quote });
+  // Note: in the context of symmetric strategy order0 is same as order1
+  const minAboveMarket = baseFiat.lt(quoteFiat.times(order0.min));
+  const maxBelowMarket = baseFiat.gt(quoteFiat.times(order0.max));
+  if (minAboveMarket) {
+    return (
+      <>
+        <TokenBudget token={base} order={order0} query={token0BalanceQuery} />
+        <Explaination base={base} buy />
+      </>
+    );
+  } else if (maxBelowMarket) {
+    return (
+      <>
+        <TokenBudget token={quote} order={order1} query={token1BalanceQuery} />
+        <Explaination base={base} />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <TokenBudget token={quote} order={order1} query={token1BalanceQuery} />
+        <TokenBudget token={base} order={order0} query={token0BalanceQuery} />
+      </>
+    );
+  }
+};
+
+const Explaination: FC<{ base?: Token; buy?: boolean }> = ({ base, buy }) => {
+  return (
+    <p className="text-12 text-white/60">
+      The market price is outside the ranges you set for&nbsp;
+      {buy ? 'buying' : 'selling'}&nbsp;
+      {base?.symbol}. Budget for buying {base?.symbol} is not required.&nbsp;
+      {/* TODO: add url */}
+      <a
+        href="/"
+        target="_blank"
+        className="inline-flex items-center gap-4 text-12 font-weight-500 text-green"
+      >
+        Learn More
+        <IconLink className="h-12 w-12" />
+      </a>
+    </p>
+  );
+};
+
+interface TokenBudgetProps {
+  token?: Token;
+  order: OrderCreate;
+  query: UseQueryResult<string, any>;
+}
+
+const TokenBudget: FC<TokenBudgetProps> = (props) => {
+  const { token, order, query } = props;
+  if (!token) return <></>;
+
+  const insufficientBalance =
+    !query.isLoading && new BigNumber(query.data || 0).lt(order.budget);
+
+  return (
+    <>
+      <TokenInputField
+        id={`${token.symbol}-budget`}
+        className="rounded-16 bg-black p-16"
+        value={order.budget}
+        setValue={order.setBudget}
+        token={token}
+        isBalanceLoading={query.isLoading}
+        balance={query.data}
+        isError={insufficientBalance}
+        data-testid="input-budget"
+      />
+      {insufficientBalance && (
+        <output
+          htmlFor={`${token.symbol}-budget`}
+          role="alert"
+          aria-live="polite"
+          className="flex items-center gap-10 font-mono text-12 text-red"
+        >
+          <IconWarning className="h-12 w-12" />
+          <span className="flex-1">Insufficient balance</span>
+        </output>
+      )}
+    </>
+  );
+};
--- src/components/strategies/create/symmetric/CreateSymmetricStrategy.tsx
+++ src/components/strategies/create/symmetric/CreateSymmetricStrategy.tsx
@@ -2,17 +2,15 @@ import { Dispatch, FC, SetStateAction } from 'react';
 import { ReactComponent as IconLink } from 'assets/icons/link.svg';
 import { useMarketIndication } from 'components/strategies/marketPriceIndication';
 import { CreateSymmerticStrategySpread } from './CreateSymmerticStrategySpread';
-import { TokenInputField } from 'components/common/TokenInputField/TokenInputField';
-import { ReactComponent as IconWarning } from 'assets/icons/warning.svg';
 import { ReactComponent as IconTooltip } from 'assets/icons/tooltip.svg';
-import BigNumber from 'bignumber.js';
 import { InputRange } from '../BuySellBlock/InputRange';
 import { Tooltip } from 'components/common/tooltip/Tooltip';
 import { Token } from 'libs/tokens';
 import { OrderCreate } from '../useOrder';
 import { UseQueryResult } from '@tanstack/react-query';
+import { CreateSymmerticStrategyBudget } from './CreateSymmerticStrategyBudget';
 
-interface Props {
+export interface SymmetricStrategyProps {
   base?: Token;
   quote?: Token;
   order0: OrderCreate;
@@ -23,30 +21,14 @@ interface Props {
   setSpreadPPM: Dispatch<SetStateAction<number>>;
 }
 
-export const CreateSymmetricStrategy: FC<Props> = (props) => {
-  const {
-    base,
-    quote,
-    order0,
-    order1,
-    token0BalanceQuery,
-    token1BalanceQuery,
-    spreadPPM,
-    setSpreadPPM,
-  } = props;
+export const CreateSymmetricStrategy: FC<SymmetricStrategyProps> = (props) => {
+  const { base, quote, order0, spreadPPM, setSpreadPPM } = props;
   const { marketPricePercentage } = useMarketIndication({
     base,
     quote,
     order: order0,
-    buy: false,
+    buy: true,
   });
-
-  const insufficientBalance0 =
-    !token0BalanceQuery.isLoading &&
-    new BigNumber(token0BalanceQuery.data || 0).lt(order0.budget);
-  const insufficientBalance1 =
-    !token0BalanceQuery.isLoading &&
-    new BigNumber(token1BalanceQuery.data || 0).lt(order1.budget);
 
   return (
     <>
@@ -125,64 +107,16 @@ export const CreateSymmetricStrategy: FC<Props> = (props) => {
       </article>
       <article className="flex flex-col gap-20 rounded-10 bg-silver p-20">
         <header className="flex items-center gap-8 ">
+          <span className="flex h-16 w-16 items-center justify-center rounded-full bg-white/10 text-[10px] text-white/60">
+            3
+          </span>
           <h3 className="flex-1 text-18 font-weight-500">Set Budgets</h3>
           {/* TODO add tooltip text here */}
           <Tooltip element={''}>
             <IconTooltip className="h-14 w-14 text-white/60" />
           </Tooltip>
         </header>
-        {quote && (
-          <>
-            <TokenInputField
-              id={`${quote.symbol}-budget`}
-              className="rounded-16 bg-black p-16"
-              value={order1.budget}
-              setValue={order1.setBudget}
-              token={quote}
-              isBalanceLoading={token1BalanceQuery.isLoading}
-              balance={token1BalanceQuery.data}
-              isError={insufficientBalance1}
-              data-testid="input-budget"
-            />
-            {insufficientBalance1 && (
-              <output
-                htmlFor={`${quote.symbol}-budget`}
-                role="alert"
-                aria-live="polite"
-                className="flex items-center gap-10 font-mono text-12 text-red"
-              >
-                <IconWarning className="h-12 w-12" />
-                <span className="flex-1">Insufficient balance</span>
-              </output>
-            )}
-          </>
-        )}
-        {base && (
-          <>
-            <TokenInputField
-              id={`${base.symbol}-budget`}
-              className="rounded-16 bg-black p-16"
-              value={order0.budget}
-              setValue={order0.setBudget}
-              token={base}
-              isBalanceLoading={token0BalanceQuery.isLoading}
-              balance={token0BalanceQuery.data}
-              isError={insufficientBalance0}
-              data-testid="input-budget"
-            />
-            {insufficientBalance0 && (
-              <output
-                htmlFor={`${base.symbol}-budget`}
-                role="alert"
-                aria-live="polite"
-                className="flex items-center gap-10 font-mono text-12 text-red"
-              >
-                <IconWarning className="h-12 w-12" />
-                <span className="flex-1">Insufficient balance</span>
-              </output>
-            )}
-          </>
-        )}
+        <CreateSymmerticStrategyBudget {...props} />
       </article>
     </>
   );
