c45723e237a8b3eb348de42313613f3a5a7a3478
Jan Langheimer <langheimer@gmail.com>
2023-12-19 15:22:29 +0100

    change text

--- src/components/common/TokenInputField/TokenInputField.tsx
+++ src/components/common/TokenInputField/TokenInputField.tsx
@@ -129,7 +129,7 @@ export const TokenInputField: FC<Props> = ({
                       : 'text-green group-hover:text-white'
                   }
                 >
-                  TOP UP
+                  MAX
                 </b>
               </>
             )}
