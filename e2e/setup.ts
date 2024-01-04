df6bd3ab56ab3924f1530d1680d7f9dad05330a8
Jan Langheimer <langheimer@gmail.com>
2024-01-04 15:18:23 +0100

    e2e test with storage path

--- e2e/setup.ts
+++ e2e/setup.ts
@@ -12,23 +12,23 @@ async function globalSetup(config: FullConfig) {
   // process.env['TENDERLY_FORK_ID'] = fork.id;
   // const rpcUrl = forkRpcUrl(fork.id);
   // // On each browser, fill the Debug form and save localStorage in storageState
-  // const setupProjects = config.projects.map(async (project) => {
-  //   if (!(project.name in browsers)) return;
-  //   const { baseURL, storageState } = project.use;
-  //   if (!baseURL) return;
-  //
-  //   const browser = await browsers[project.name as BrowserName].launch();
-  //   const page = await browser.newPage();
-  //   await page.goto(`${baseURL}/debug`);
-  //   // SET RPC-URL
-  //   await page.getByLabel('RPC URL').fill(rpcUrl);
-  //   await page.getByTestId('unchecked-signer').click();
-  //   await page.getByTestId('save-rpc').click();
-  //   await page.waitForURL(`${baseURL}/debug`);
-  //   await page.context().storageState({ path: storageState as string });
-  //   await browser.close();
-  // });
-  // await Promise.all(setupProjects);
+  const setupProjects = config.projects.map(async (project) => {
+    if (!(project.name in browsers)) return;
+    const { baseURL, storageState } = project.use;
+    if (!baseURL) return;
+    //
+    const browser = await browsers[project.name as BrowserName].launch();
+    const page = await browser.newPage();
+    //   await page.goto(`${baseURL}/debug`);
+    //   // SET RPC-URL
+    //   await page.getByLabel('RPC URL').fill(rpcUrl);
+    //   await page.getByTestId('unchecked-signer').click();
+    //   await page.getByTestId('save-rpc').click();
+    //   await page.waitForURL(`${baseURL}/debug`);
+    await page.context().storageState({ path: storageState as string });
+    await browser.close();
+  });
+  await Promise.all(setupProjects);
   // console.timeEnd('Fork is setup');
   // console.log('RPC URL:', rpcUrl);
 }
