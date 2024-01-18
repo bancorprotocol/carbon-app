939949acd89ce56d49132b66f474242ad7857528
Tiago Silva <tfn.da.silva@gmail.com>
2024-01-18 19:47:36 +0000

    Update recurring create e2e test

--- e2e/tests/strategy/recurring/create.ts
+++ e2e/tests/strategy/recurring/create.ts
@@ -1,13 +1,15 @@
 import { expect, test } from '@playwright/test';
 import { checkApproval } from '../../../utils/modal';
 import { NotificationDriver } from '../../../utils/NotificationDriver';
-import { navigateTo, waitFor } from '../../../utils/operators';
+import { navigateTo, screenshot, waitFor } from '../../../utils/operators';
 import {
   CreateStrategyDriver,
   CreateStrategyTestCase,
   MyStrategyDriver,
   assertRecurringTestCase,
+  testDescription,
 } from '../../../utils/strategy';
+import { MainMenuDriver } from '../../../utils/MainMenuDriver';
 
 export const createRecurringStrategy = (testCase: CreateStrategyTestCase) => {
   assertRecurringTestCase(testCase);
@@ -34,6 +36,18 @@ export const createRecurringStrategy = (testCase: CreateStrategyTestCase) => {
     await expect(sellForm.outcomeValue()).toHaveText(output.sell.outcomeValue);
     await expect(sellForm.outcomeQuote()).toHaveText(output.sell.outcomeQuote);
 
+    const mainMenu = new MainMenuDriver(page);
+    await mainMenu.hide();
+    await screenshot(
+      buyForm.locator,
+      `[Create ${testDescription(testCase)}] Buy Form`
+    );
+    await screenshot(
+      sellForm.locator,
+      `[Create ${testDescription(testCase)}] Sell Form`
+    );
+    await mainMenu.show();
+
     await createForm.submit();
 
     await checkApproval(page, [base, quote]);
@@ -51,6 +65,7 @@ export const createRecurringStrategy = (testCase: CreateStrategyTestCase) => {
     const strategies = await myStrategies.getAllStrategies();
     await expect(strategies).toHaveCount(1);
     const strategy = await myStrategies.getStrategy(1);
+
     await expect(strategy.pair()).toHaveText(`${base}/${quote}`);
     await expect(strategy.status()).toHaveText('Active');
     await expect(strategy.totalBudget()).toHaveText(output.totalFiat);
@@ -58,5 +73,28 @@ export const createRecurringStrategy = (testCase: CreateStrategyTestCase) => {
     await expect(strategy.buyBudgetFiat()).toHaveText(output.buy.fiat);
     await expect(strategy.sellBudget()).toHaveText(output.sell.budget);
     await expect(strategy.sellBudgetFiat()).toHaveText(output.sell.fiat);
+
+    // Check range
+    const [buySetting, sellSetting] = testCase.input.setting.split('_');
+
+    const buyTooltip = await strategy.priceTooltip('buy');
+    if (buySetting === 'limit') {
+      await expect(buyTooltip.startPrice()).toHaveText(output.buy.min);
+      await expect(buyTooltip.startPrice()).toHaveText(output.buy.max);
+    } else {
+      await expect(buyTooltip.minPrice()).toHaveText(output.buy.min);
+      await expect(buyTooltip.maxPrice()).toHaveText(output.buy.max);
+    }
+    await buyTooltip.waitForDetached();
+
+    const sellTooltip = await strategy.priceTooltip('sell');
+    if (sellSetting === 'limit') {
+      await expect(sellTooltip.startPrice()).toHaveText(output.sell.min);
+      await expect(sellTooltip.startPrice()).toHaveText(output.sell.max);
+    } else {
+      await expect(sellTooltip.minPrice()).toHaveText(output.sell.min);
+      await expect(sellTooltip.maxPrice()).toHaveText(output.sell.max);
+    }
+    await sellTooltip.waitForDetached();
   });
 };
