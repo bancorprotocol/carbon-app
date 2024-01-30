7bde57faec43d6894db2df510be561480435ddec
Tiago Silva <tfn.da.silva@gmail.com>
2024-01-30 14:00:02 +0000

    Reformat Summary Gains

--- src/components/simulator/SimulatorSummaryGains.tsx
+++ src/components/simulator/SimulatorSummaryGains.tsx
@@ -1,6 +1,6 @@
 import { Tooltip } from 'components/common/tooltip/Tooltip';
 import { ReactComponent as IconTooltip } from 'assets/icons/tooltip.svg';
-import { cn, prettifySignedNumber } from 'utils/helpers';
+import { prettifySignedNumber } from 'utils/helpers';
 import { FC } from 'react';
 import { Token } from 'libs/tokens';
 import { useFiatCurrency } from 'hooks/useFiatCurrency';
@@ -26,14 +26,14 @@ export const SimulatorSummaryGains: FC<Props> = ({
   });
 
   return (
-    <article className={cn('flex flex-col rounded-8')}>
+    <article className="flex flex-col rounded-8">
       <Tooltip element={<TooltipContent />}>
         <h4 className="text-secondary flex items-center gap-4 font-mono !text-12">
           Estimated Gains
           <IconTooltip className="h-10 w-10" />
         </h4>
       </Tooltip>
-      <p className={`text-24 font-weight-500`}>{portfolioGainsFiatFormatted}</p>
+      <p className="text-24 font-weight-500">{portfolioGainsFiatFormatted}</p>
     </article>
   );
 };
