839eca8e0773eb533be5a9ed50f027ac0b22a503
Tiago Silva <tfn.da.silva@gmail.com>
2024-01-30 17:20:40 +0000

    Change ul>li>button components to article with link components

--- src/components/simulator/SimulatorStrategyType.tsx
+++ src/components/simulator/SimulatorStrategyType.tsx
@@ -4,7 +4,7 @@ import { ReactComponent as IconTwoRanges } from 'assets/icons/two-ranges.svg';
 import { ReactComponent as IconOverlappingStrategy } from 'assets/icons/overlapping-strategy.svg';
 import { cn } from 'utils/helpers';
 import { SimulatorType } from 'libs/routing/routes/sim';
-import { useNavigate } from '@tanstack/react-router';
+import { Link, useNavigate } from '@tanstack/react-router';
 
 interface Props {
   strategyType: SimulatorType;
@@ -56,52 +56,46 @@ export const SimulatorStrategyType: FC<Props> = ({ strategyType }) => {
       <header className="mb-16 flex items-center justify-between">
         <h2 className="m-0 text-18 font-weight-500">Strategy Type</h2>
       </header>
-      <ul
+      <article
         onKeyDown={handleKeyDown}
         ref={list}
         role="tablist"
         className={`grid grid-cols-2 gap-8`}
       >
         {items.map(({ label, svg, tooltipText }) => (
-          <li role="button" key={label} className="relative">
-            <button
-              id={'tab-' + label}
-              role="tab"
-              aria-controls={'panel-' + label}
-              aria-selected={strategyType === label}
-              onClick={() =>
-                navigate({
-                  to: '/simulator/$simulationType',
-                  search: {},
-                  params: { simulationType: label },
-                  replace: true,
-                  resetScroll: false,
-                })
-              }
-              className={cn(
-                'flex h-full w-full flex-row items-center justify-center gap-8 rounded-10 bg-black px-8 py-16 text-14 font-weight-500 outline-white/60',
-                'md:px-12',
-                'focus-visible:outline focus-visible:outline-1',
-                strategyType === label ? 'outline outline-1 outline-white' : ''
-              )}
+          <Link
+            role="tab"
+            id={'tab-' + label}
+            aria-controls={'panel-' + label}
+            aria-selected={strategyType === label}
+            key={label}
+            to="/simulator/$simulationType"
+            className={cn(
+              'flex h-full w-full flex-row items-center justify-center gap-8 rounded-10 bg-black px-8 py-16 text-14 font-weight-500 outline-white/60',
+              'md:px-12',
+              'focus-visible:outline focus-visible:outline-1',
+              strategyType === label ? 'outline outline-1 outline-white' : ''
+            )}
+            replace={true}
+            resetScroll={false}
+            params={{ simulationType: label }}
+          >
+            {svg}
+            <span
+              className={`capitalize ${
+                strategyType === label ? 'text-white' : 'text-white/40'
+              }`}
             >
-              {svg}
-              <span
-                className={`capitalize ${
-                  strategyType === label ? 'text-white' : 'text-white/40'
-                }`}
-              >
-                {label}
-              </span>
+              {label}
+            </span>
 
-              <Tooltip
-                element={<div>{tooltipText}</div>}
-                iconClassName="!h-12 !w-12 text-white/60"
-              />
-            </button>
-          </li>
+            <Tooltip
+              element={<div>{tooltipText}</div>}
+              iconClassName="!h-12 !w-12 text-white/60"
+            />
+          </Link>
         ))}
-      </ul>
+      </article>
     </section>
   );
 };
