bc5a934953171d69e4e34854e396e97268544ac8
Tiago Silva <tfn.da.silva@gmail.com>
2024-01-30 12:25:33 +0000

    Clean-up summary

--- src/components/simulator/SimulatorSummary.tsx
+++ src/components/simulator/SimulatorSummary.tsx
@@ -1,63 +1,61 @@
-import { Link, useSearch } from 'libs/routing';
+import { Link } from 'libs/routing';
 import { buttonStyles } from 'components/common/button/buttonStyles';
 import { SimulatorSummaryGains } from './SimulatorSummaryGains';
 import { SimulatorSummaryRoi } from './SimulatorSummaryRoi';
 import { SimulatorSummaryTokens } from './SimulatorTokens';
 import { SimulatorSummaryTable } from './SimulatorSummaryTable';
-import { useTokens } from 'hooks/useTokens';
+import { StrategyInput2 } from 'hooks/useStrategyInput';
 
 interface Props {
   roi: number;
   gains: number;
   isLoading: boolean;
+  state2: StrategyInput2;
 }
 
-export const SimulatorSummary = ({ roi, gains, isLoading }: Props) => {
-  const search = useSearch({ from: '/simulator/result' });
+export const SimulatorSummary = ({ roi, gains, isLoading, state2 }: Props) => {
+  const baseToken = state2.baseToken!;
+  const quoteToken = state2.quoteToken!;
 
   const portfolioGains = isLoading ? 0.0 : gains;
   const portfolioRoi = isLoading ? 0.0 : roi;
 
-  const { getTokenById } = useTokens();
-  const baseToken = getTokenById(search.baseToken);
-  const quoteToken = getTokenById(search.quoteToken);
-
   const summaryData = {
-    buyMin: search.buyMin,
-    buyMax: search.buyMax,
-    sellMin: search.sellMin,
-    sellMax: search.sellMax,
-    sellBudget: search.sellBudget,
-    buyBudget: search.buyBudget,
+    buyMin: state2.buy.min,
+    buyMax: state2.buy.max,
+    sellMin: state2.sell.min,
+    sellMax: state2.sell.max,
+    sellBudget: state2.sell.budget,
+    buyBudget: state2.buy.budget,
   };
 
-  const strategyType = 'recurring';
+  const strategyType = state2.simulationType;
 
   return (
     <header className="my-8 flex flex-wrap gap-8">
       <section className="flex flex-grow flex-wrap items-center justify-evenly gap-8 rounded-10 bg-black p-16 md:justify-between">
         <SimulatorSummaryTokens
-          baseToken={baseToken!}
-          quoteToken={quoteToken!}
+          baseToken={baseToken}
+          quoteToken={quoteToken}
           strategyType={strategyType}
         />
         <SimulatorSummaryTable
           summaryData={summaryData}
-          baseToken={baseToken!}
-          quoteToken={quoteToken!}
+          baseToken={baseToken}
+          quoteToken={quoteToken}
         />
       </section>
       <section className="flex flex-grow flex-wrap items-center justify-evenly gap-8 rounded-10 bg-black p-16 md:justify-between">
         <SimulatorSummaryGains
           portfolioGains={portfolioGains}
-          quoteToken={quoteToken!}
+          quoteToken={quoteToken}
         />
         <SimulatorSummaryRoi portfolioRoi={portfolioRoi} />
         <Link
           to="/strategies/create"
           search={{
-            base: baseToken!.address,
-            quote: quoteToken!.address,
+            base: baseToken?.address,
+            quote: quoteToken?.address,
             strategyType:
               strategyType === 'recurring' ? 'recurring' : undefined,
             strategySettings:
