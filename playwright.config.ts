8ad97e85a7ec7dedd32d2ef3878a79ee640e5ee0
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2024-02-01 09:57:12 +0100

    add video on failure

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,6 +27,9 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     storageState: 'e2e/storage.json',
+    video: {
+      mode: 'retain-on-failure',
+    },
   },
 
   /* Configure projects for major browsers */
