555ca1d26993705ab6a3ff4679333fbf5b1e494d
Tiago Silva <tfn.da.silva@gmail.com>
2024-02-17 01:15:25 +0000

    Update footer

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -1,11 +1,17 @@
 import { FC } from 'react';
+import { NewTabLink } from 'libs/routing';
 import { ReactComponent as LogoCarbonDeFi } from 'assets/logos/carbondefi.svg';
 
 export const Footer: FC = () => {
   return (
-    <footer className="bg-body fixed bottom-0 left-0 z-10 hidden h-65 w-full items-center justify-start gap-8 border-t border-darkGrey py-16 px-20 md:flex">
-      <span className="text-14 font-weight-500 text-white/60">Powered by </span>
-      <LogoCarbonDeFi className="h-16 w-[114px]" />
+    <footer className="bg-body hidden items-center border-t border-emphasis py-24 px-20 md:flex">
+      <NewTabLink
+        to="https://carbondefi.xyz"
+        className="flex h-16 items-center gap-8 whitespace-nowrap text-14 font-weight-500 text-white/60"
+      >
+        Powered by
+        <LogoCarbonDeFi className="text-white" />
+      </NewTabLink>
     </footer>
   );
 };
