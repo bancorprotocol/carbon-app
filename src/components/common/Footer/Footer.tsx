f933a5239381fc41c761ece1ef611e4fcfd3198a
Tiago Silva <tfn.da.silva@gmail.com>
2024-02-21 12:28:06 +0000

    Add aria-label to show link name as "Powered By CarbonDeFi" instead of default "Powered By"

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -6,6 +6,7 @@ export const Footer: FC = () => {
   return (
     <footer className="bg-body hidden items-center border-t border-emphasis py-24 px-20 md:flex">
       <NewTabLink
+        aria-label="Powered By CarbonDeFi"
         to={externalLinks.carbonHomepage}
         className="flex h-16 items-center gap-8 whitespace-nowrap text-14 font-weight-500 text-white/60"
       >
