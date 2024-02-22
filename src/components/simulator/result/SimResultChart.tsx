9a77ee13feb02e554ac8b3761e824c60929e1a94
Tiago Silva <tfn.da.silva@gmail.com>
2024-02-22 13:28:55 +0000

    Change error message

--- src/components/simulator/result/SimResultChart.tsx
+++ src/components/simulator/result/SimResultChart.tsx
@@ -6,6 +6,10 @@ import { useSimulator } from 'components/simulator/result/SimulatorProvider';
 import { StrategyInputValues } from 'hooks/useStrategyInput';
 import { SimulatorType } from 'libs/routing/routes/sim';
 import { useState } from 'react';
+import { ReactComponent as IconWarning } from 'assets/icons/warning.svg';
+import { Link, useSearch } from '@tanstack/react-router';
+import { IconTitleText } from 'components/common/iconTitleText/IconTitleText';
+import { buttonStyles } from 'components/common/button/buttonStyles';
 
 interface Props {
   state: StrategyInputValues;
@@ -15,20 +19,35 @@ interface Props {
 export const SimResultChart = ({ state, simulationType }: Props) => {
   const ctx = useSimulator();
   const [showSummary, setShowSummary] = useState(false);
+  const searchState = useSearch({ from: '/simulator/result' });
 
   if (ctx.isError) {
     return (
       <div className="flex h-[400px] items-center justify-center rounded-10 bg-black py-10">
-        <div className="max-w-1/3 flex flex-col items-center justify-center space-y-10 rounded-10 border border-red bg-red/30 p-20">
-          <div className="text-24 font-weight-500">Error</div>
-          <div className="text-center font-weight-500">
-            Something went wrong.
-            <br /> Please try again or contact support.
-          </div>
-          <div className="text-12 text-red/80">
-            {ctx.errorMsg ?? 'Unknown internal error'}
-          </div>
-        </div>
+        <section
+          role="alert"
+          aria-live="polite"
+          className="my-40 mx-auto flex w-[480px] flex-col items-center gap-20"
+        >
+          <IconTitleText
+            icon={<IconWarning />}
+            title="Missing Information"
+            text="
+          It appears that the simulation is missing essential information for
+          successful execution. Please attempt to re-enter the simulation data."
+            variant="error"
+          />
+          <Link
+            to={'/simulator/$simulationType'}
+            params={{ simulationType }}
+            search={searchState}
+            className={buttonStyles({
+              size: 'lg',
+            })}
+          >
+            Back
+          </Link>
+        </section>
       </div>
     );
   }
