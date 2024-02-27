3d4aa7b0de7c77572625db30c8d076f8758514ef
Tiago Silva <tfn.da.silva@gmail.com>
2024-02-27 16:38:27 +0000

    Add simulator to urlNames and add function to screenshot outside of viewport

--- e2e/utils/operators.ts
+++ e2e/utils/operators.ts
@@ -1,9 +1,73 @@
-import { Locator, Page } from 'playwright-core';
+import {
+  Locator,
+  Page,
+  LocatorScreenshotOptions,
+  PageScreenshotOptions,
+} from 'playwright-core';
 export { prettifyNumber as pn } from '../../src/utils/helpers/number';
 
 export const isCI = !!process.env.CI && process.env.CI !== 'false';
 export const isDraft = !!process.env.DRAFT && process.env.DRAFT !== 'false';
 export const shouldTakeScreenshot = isCI && !isDraft;
+
+function isPage(page: Page | Locator): page is Page {
+  return (page as Page).url !== undefined;
+}
+
+// Takes a screenshot of elements outside of the viewport
+export const screenshotFullScreen = async (
+  target: Page | Locator,
+  name: string
+) => {
+  if (!shouldTakeScreenshot) return;
+
+  const screenshotCommonParams:
+    | PageScreenshotOptions
+    | LocatorScreenshotOptions = {
+    type: 'jpeg',
+    path: `e2e/screenshots/${name}.jpg`,
+    maskColor: '#303030',
+  };
+
+  if (isPage(target)) {
+    return await target.screenshot({
+      ...screenshotCommonParams,
+      mask: [target.getByTestId('user-wallet')],
+      fullPage: true,
+    });
+  }
+  const page = target.page();
+  const targetBox = await target.boundingBox();
+
+  const fullPageSize = await page.evaluate(() => {
+    return {
+      width: Math.max(
+        document.documentElement.scrollWidth +
+          document.documentElement.scrollLeft
+      ),
+      height: Math.max(
+        document.documentElement.scrollHeight +
+          document.documentElement.scrollTop
+      ),
+    };
+  });
+
+  const newViewportSize = {
+    width: Math.max(fullPageSize.width, targetBox?.width || 0),
+    height: Math.max(fullPageSize.height, targetBox?.height || 0),
+  };
+
+  const originalSize = page.viewportSize();
+  if (!originalSize) throw Error('Could not get page viewportSize()');
+
+  await page.setViewportSize(newViewportSize);
+  const fullPageScreenshot = await target.screenshot({
+    ...screenshotCommonParams,
+    mask: [page.getByTestId('user-wallet')],
+  });
+  await page.setViewportSize(originalSize);
+  return fullPageScreenshot;
+};
 
 /** Utils to take screenshot on CI that are not draft */
 export const screenshot = (target: Page | Locator, name: string) => {
@@ -21,6 +85,7 @@ const urlNames = {
   '/trade?*': 'Trade',
   '/explorer': 'Explorer',
   '/debug': 'Debug',
+  '/simulator/recurring': 'Simulator',
 };
 
 /** Use soft navigation instead of reloading the page. */
