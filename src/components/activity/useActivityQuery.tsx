100ca5806fdbaa3b0b7902d0c3d5aca1a7657c0e
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2024-03-21 16:43:23 +0100

    remove filtering on tokens

--- src/components/activity/useActivityQuery.tsx
+++ src/components/activity/useActivityQuery.tsx
@@ -7,80 +7,44 @@ import {
   ServerActivity,
 } from 'libs/queries/extApi/activity';
 import { Token } from 'libs/tokens';
-import { fetchTokenData } from 'libs/tokens/tokenHelperFn';
 import { carbonApi } from 'utils/carbonApi';
 
 const toActivities = (
   data: ServerActivity[],
   tokenMap: Map<string, Token>
 ): Activity[] => {
-  return (
-    data
-      // TODO: Remove this filter once we have all tokens
-      .filter((activity) => {
-        const { strategy } = activity;
-        if (!tokenMap.has(strategy.base.toLowerCase())) return false;
-        if (!tokenMap.has(strategy.quote.toLowerCase())) return false;
-        return true;
-      })
-      .map((activity) => {
-        const { strategy } = activity;
-        const base = tokenMap.get(strategy.base.toLowerCase());
-        const quote = tokenMap.get(strategy.quote.toLowerCase());
-        if (!base) {
-          throw new Error(
-            `Base "${strategy.base}" not found for activity with txhash "${activity.txHash}"`
-          );
-        }
-        if (!quote) {
-          throw new Error(
-            `Quote "${strategy.quote}" not found for activity with txhash "${activity.txHash}"`
-          );
-        }
-        return {
-          ...activity,
-          date: new Date(activity.timestamp * 1000),
-          strategy: {
-            ...strategy,
-            base,
-            quote,
-          },
-        };
-      })
-  );
-};
-
-type GetTokenContract = Parameters<typeof fetchTokenData>[0];
-
-const getNewTokens = async (
-  tokenMap: Map<string, Token>,
-  tokenContract: GetTokenContract,
-  activities: ServerActivity[]
-) => {
-  const remainingAddresses = new Set<string>();
-  for (const activity of activities) {
-    const { base, quote } = activity.strategy;
-    if (!tokenMap.has(base)) remainingAddresses.add(base);
-    if (!tokenMap.has(quote)) remainingAddresses.add(quote);
-  }
-  const remainingList = Array.from(remainingAddresses);
-  const query = remainingList.map(async (address) => {
-    const token = await fetchTokenData(tokenContract, address);
-    tokenMap.set(address, token);
+  return data.map((activity) => {
+    const { strategy } = activity;
+    const base = tokenMap.get(strategy.base.toLowerCase());
+    const quote = tokenMap.get(strategy.quote.toLowerCase());
+    if (!base) {
+      throw new Error(
+        `Base "${strategy.base}" not found for activity with txhash "${activity.txHash}"`
+      );
+    }
+    if (!quote) {
+      throw new Error(
+        `Quote "${strategy.quote}" not found for activity with txhash "${activity.txHash}"`
+      );
+    }
+    return {
+      ...activity,
+      date: new Date(activity.timestamp * 1000),
+      strategy: {
+        ...strategy,
+        base,
+        quote,
+      },
+    };
   });
-  await Promise.all(query);
-  return remainingList.map((address) => tokenMap.get(address)!);
 };
 
-// TODO: query all remaining tokens
 export const useActivityQuery = (params: QueryActivityParams = {}) => {
-  // const { Token } = useContract();
   const { tokensMap, isLoading } = useTokens();
   return useQuery(
     QueryKey.activities(params),
     async () => {
       const activities = await carbonApi.getActivity(params);
-      // await getNewTokens(tokensMap, Token, activities);
       return toActivities(activities, tokensMap).sort((a, b) => {
         return b.date.getTime() - a.date.getTime();
       });
