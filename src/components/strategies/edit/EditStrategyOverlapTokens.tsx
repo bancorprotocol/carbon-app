946e043f394e2c5258e9e87fda1923ad0c08f46d
Tiago Silva <tfn.da.silva@gmail.com>
2024-04-10 16:09:27 +0100

    Fix edit strategy overlap tokens text size

--- src/components/strategies/edit/EditStrategyOverlapTokens.tsx
+++ src/components/strategies/edit/EditStrategyOverlapTokens.tsx
@@ -11,15 +11,17 @@ export const EditStrategyOverlapTokens = ({
   return (
     <header className="rounded-10 bg-background-900 flex w-full items-center space-x-10 p-20">
       <TokensOverlap tokens={[strategy.base, strategy.quote]} size={32} />
-      <div className="text-14">
-        <h2 className="flex gap-6">
+      <div>
+        <h2 className="text-14 flex gap-6">
           <span>{strategy.base.symbol}</span>
           <span role="separator" className="text-white/60">
             /
           </span>
           <span>{strategy.quote.symbol}</span>
         </h2>
-        <div className="flex text-white/60">ID: {strategy.idDisplay}</div>
+        <div className="text-14 flex text-white/60">
+          ID: {strategy.idDisplay}
+        </div>
       </div>
     </header>
   );
