046128baf533040f6ec78f0d7354f8b37603be45
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2024-05-07 11:06:25 +0200

    update text & tooltip

--- src/components/strategies/overlapping/OverlappingAnchor.tsx
+++ src/components/strategies/overlapping/OverlappingAnchor.tsx
@@ -4,6 +4,7 @@ import { TokenLogo } from 'components/common/imager/Imager';
 import { Token } from 'libs/tokens';
 import { WarningMessageWithIcon } from 'components/common/WarningMessageWithIcon';
 import style from './OverlappingBudget.module.css';
+import { Tooltip } from 'components/common/tooltip/Tooltip';
 
 interface Props {
   base: Token;
@@ -18,10 +19,14 @@ export const OverlappingAnchor: FC<Props> = (props) => {
   const { base, quote, anchor, setAnchor, anchorError } = props;
   return (
     <article className="rounded-10 bg-background-900 flex w-full flex-col gap-16 p-20">
-      <h2 className="text-18">Budget</h2>
-      <p className="text-14 text-white/80">
-        Please specify which token you'd prefer to use as the anchor.
-      </p>
+      <header className="flex items-center justify-between">
+        <h2 className="text-18">Budget</h2>
+        <Tooltip
+          iconClassName="size-14 text-white/60"
+          element="Indicate the token, action and amount for the strategy. Note that in order to maintain the overlapping behavior, the 2nd budget indication will be calculated using the prices, spread and budget values you use."
+        />
+      </header>
+      <p className="text-14 text-white/80">Please select a token to proceed.</p>
       <h3 className="text-16 font-weight-500 flex items-center gap-8">
         <span className="flex h-16 w-16 items-center justify-center rounded-full bg-black text-[10px] text-white/60">
           1
