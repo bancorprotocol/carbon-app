92f0c8ac0b7af33939187610a63e27eb3dd0cad5
Tiago Silva <tfn.da.silva@gmail.com>
2024-06-17 14:25:39 +0100

    Fix typo in comment

--- src/libs/wagmi/wagmi.utils.ts
+++ src/libs/wagmi/wagmi.utils.ts
@@ -1,4 +1,4 @@
-// Error messages for EIP-1993 Provider (https://eips.ethereum.org/EIPS/eip-1193#provider-errors)
+// Error messages for EIP-1193 Provider (https://eips.ethereum.org/EIPS/eip-1193#provider-errors)
 export const errorMessages: Record<string, string> = {
   4001: 'Connection rejected. Please approve dApp connection on your wallet.',
   4100: 'The request method/account is not authorized. Please approve dApp permissions on your wallet.',
