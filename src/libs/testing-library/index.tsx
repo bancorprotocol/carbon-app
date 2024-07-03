38690c2d6f9c604abe78ffff52022e0b02fca548
Tiago Silva <tfn.da.silva@gmail.com>
2024-07-03 12:50:23 +0100

    Add jsdoc to loadRouter and renderWithRouter

--- src/libs/testing-library/index.tsx
+++ src/libs/testing-library/index.tsx
@@ -26,6 +26,20 @@ const customRender = (
     ...options,
   });
 
+/**
+ * Asynchronously renders a component within a custom router and returns the rendered container and router instance.
+ *
+ * This function leverages `loadRouter` to asynchronously create and load a custom router based on the specified parameters.
+ * It then renders the component within this router using a `RouterProvider`. The function returns an object containing
+ * the rendered container and the custom router instance, allowing for further interaction or testing.
+ *
+ * @param params - The parameters for creating and loading the custom router.
+ * @param params.component - A function that returns the component to be rendered by the router.
+ * @param params.basePath - The base path for the router. Defaults to '/'.
+ * @param params.search - The optional search parameters to be included in the route. Object keys and values are encoded into a query string.
+ 
+ * @returns {Promise<{container: RenderResult, router: Router}>} A promise that resolves to an object containing the `container` with the rendered output and the `router` instance.
+ */
 export const renderWithRouter = async (params: RouterRenderParams) => {
   const customRouter = await loadRouter(params);
   return {
--- src/libs/testing-library/utils.ts
+++ src/libs/testing-library/utils.ts
@@ -21,6 +21,18 @@ export const tokens = [
   },
 ];
 
+/**
+ * Asynchronously created and loads a custom router with the specified component and optional base path and search parameters.
+ *
+ * This function initializes a custom router with a single route based on the provided parameters.
+ * It creates a memory history for the router with an initial path constructed from the basePath and
+ * encoded search parameters.
+ *
+ * @param component - A function that returns the component to be rendered by the router.
+ * @param basePath - The base path for the router. Defaults to '/'.
+ * @param search - The optional search parameters to be included in the route. Object keys and values are encoded into a query string.
+ * @returns A promise that resolves to the initialized and loaded router.
+ */
 export const loadRouter = async ({
   component,
   basePath = '/',
