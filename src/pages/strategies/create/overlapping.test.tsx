357e0731ecc8be727fa6d9ac651444a7b8b47ed1
Tiago Silva <tfn.da.silva@gmail.com>
2024-07-05 20:03:25 +0100

    Update overlapping test
    - Add endpoint mock instead of import mock
    - Use data-testid instead of text to get elements from screen
    - Add tests to cover user market price below min and above max

--- src/pages/strategies/create/overlapping.test.tsx
+++ src/pages/strategies/create/overlapping.test.tsx
@@ -1,70 +1,199 @@
-import { test, expect, afterEach, vitest } from 'vitest';
+import { describe, test, expect } from 'vitest';
+import { HttpResponse, http } from 'msw';
 import { renderWithRouter, screen } from 'libs/testing-library';
+import userEvent from '@testing-library/user-event';
 import { debugTokens } from '../../../../e2e/utils/types';
 import { CreateOverlappingStrategyPage } from './overlapping';
-import userEvent from '@testing-library/user-event';
-
-vitest.mock('hooks/useMarketPrice', () => {
-  return {
-    useMarketPrice: () => {
-      return { marketPrice: 2800, isPending: false };
-    },
-  };
-});
+import { MockServer } from 'libs/testing-library/utils';
 
 const basePath = '/strategies/create/overlapping';
-afterEach(async () => {
-  window.history.replaceState(null, 'root', '/');
-});
 
-test('run overlapping form with deeplink and user market price', async () => {
-  // Initialize user and set params
-  const user = userEvent.setup();
-  const search = {
-    base: debugTokens.ETH,
-    quote: debugTokens.USDC,
-    marketPrice: '2500',
-    min: '2000',
-    max: '3000',
-    spread: '0.01',
-    anchor: 'buy',
-    budget: '100',
-  };
+const marketRates: Record<string, Record<string, number>> = {
+  '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': { USD: 1 },
+  '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee': { USD: 2800 },
+};
 
-  // Render page
-  const { router } = await renderWithRouter({
-    component: () => <CreateOverlappingStrategyPage />,
-    basePath,
-    search,
+const marketRateHandler = (
+  marketRates: Record<string, Record<string, number>>
+) => {
+  return http.get('**/*/market-rate', ({ request }) => {
+    const queryParams = new URL(request.url).searchParams;
+    const address = queryParams.get('address')?.toLowerCase();
+    if (!address) return new HttpResponse(null, { status: 404 });
+    return HttpResponse.json({
+      data: marketRates[address],
+    });
+  });
+};
+
+const mockServer = new MockServer([marketRateHandler(marketRates)]);
+
+describe('overlapping page', () => {
+  mockServer.start();
+  test('run overlapping form with deeplink and user market price', async () => {
+    // Initialize user and set search params
+    const user = userEvent.setup();
+    const search = {
+      base: debugTokens.ETH,
+      quote: debugTokens.USDC,
+      marketPrice: '2500',
+      min: '2000',
+      max: '3000',
+      spread: '0.01',
+      anchor: 'buy',
+      budget: '100',
+    };
+
+    // Render page
+    const { router } = await renderWithRouter({
+      component: () => <CreateOverlappingStrategyPage />,
+      basePath,
+      search,
+    });
+
+    // Check search params
+    expect(router.state.location.pathname).toBe(basePath);
+    expect(router.state.location.search).toStrictEqual(search);
+
+    // Close price chart
+    await user.click(screen.getByTestId('close-chart'));
+    const priceChart = screen.queryByRole('heading', {
+      name: 'Price Chart',
+    });
+    expect(priceChart).not.toBeInTheDocument();
+
+    // Check price range input and market price indication
+    const priceRangeMin = await screen.findByTestId('input-min');
+    const priceRangeMax = await screen.findByTestId('input-max');
+    const marketPriceIndications = await screen.findAllByTestId(
+      'market-price-indication'
+    );
+    expect(priceRangeMin).toHaveValue(search.min);
+    expect(priceRangeMax).toHaveValue(search.max);
+    expect(marketPriceIndications[0]).toHaveTextContent('20.00% below');
+    expect(marketPriceIndications[1]).toHaveTextContent('20.00% above');
+
+    // Check budget
+    const inputBudget = screen.getByTestId('input-budget');
+    expect(inputBudget).toHaveValue(search.budget);
+
+    // Check warning to approve deposit exists
+    const depositWarning = screen.queryByTestId('approve-warnings');
+    expect(depositWarning).toBeInTheDocument();
   });
 
-  // Check search params
-  expect(router.state.location.pathname).toBe(basePath);
-  expect(router.state.location.search).toStrictEqual(search);
+  test('run overlapping form with external market price', async () => {
+    // Set search params
+    const search = {
+      base: debugTokens.ETH,
+      quote: debugTokens.USDC,
+      min: '2000',
+      max: '3000',
+      spread: '0.01',
+      anchor: 'buy',
+      budget: '100',
+    };
 
-  // Close price chart
-  await user.click(screen.getByText('Close Chart'));
-  const priceChart = screen.queryByRole('heading', {
-    name: 'Price Chart',
+    // Render page
+    const { router } = await renderWithRouter({
+      component: () => <CreateOverlappingStrategyPage />,
+      basePath,
+      search,
+    });
+
+    // Check search params
+    expect(router.state.location.pathname).toBe(basePath);
+    expect(router.state.location.search).toStrictEqual(search);
+
+    // Check price range input and market price indication
+    const priceRangeMin = await screen.findByTestId('input-min');
+    const priceRangeMax = await screen.findByTestId('input-max');
+    const marketPriceIndications = await screen.findAllByTestId(
+      'market-price-indication'
+    );
+    expect(priceRangeMin).toHaveValue(search.min);
+    expect(priceRangeMax).toHaveValue(search.max);
+    expect(marketPriceIndications[0]).toHaveTextContent('28.57% below');
+    expect(marketPriceIndications[1]).toHaveTextContent('7.14% above');
   });
-  expect(priceChart).not.toBeInTheDocument();
 
-  // Check price range input and market price indication
-  const priceRangeMin = await screen.findByTestId('input-min');
-  const priceRangeMax = await screen.findByTestId('input-max');
-  const marketPriceIndications = await screen.findAllByTestId(
-    'market-price-indication'
-  );
-  expect(priceRangeMin).toHaveValue(search.min);
-  expect(priceRangeMax).toHaveValue(search.max);
-  expect(marketPriceIndications[0]).toHaveTextContent('20.00% below');
-  expect(marketPriceIndications[1]).toHaveTextContent('20.00% above');
+  test('run overlapping form with user market price below min price', async () => {
+    // Set search params
+    const search = {
+      base: debugTokens.ETH,
+      quote: debugTokens.USDC,
+      min: '2000',
+      max: '3000',
+      marketPrice: '1500',
+      spread: '0.01',
+      anchor: 'buy',
+      budget: '100',
+    };
 
-  // Check budget
-  const inputBudget = screen.getByTestId('input-budget');
-  expect(inputBudget).toHaveValue(search.budget);
+    // Render page
+    const { router } = await renderWithRouter({
+      component: () => <CreateOverlappingStrategyPage />,
+      basePath,
+      search,
+    });
 
-  // Check warning to approve deposit exists
-  const depositWarning = screen.queryByTestId('approve-warnings');
-  expect(depositWarning).toBeInTheDocument();
+    // Check search params
+    const { budget, ...rest } = search;
+    expect(router.state.location.pathname).toBe(basePath);
+    expect(router.state.location.search).toStrictEqual({
+      ...rest,
+      anchor: 'sell',
+    });
+
+    // Check price range input and market price indication
+    const priceRangeMin = await screen.findByTestId('input-min');
+    const priceRangeMax = await screen.findByTestId('input-max');
+    const marketPriceIndications = await screen.findAllByTestId(
+      'market-price-indication'
+    );
+    expect(priceRangeMin).toHaveValue(search.min);
+    expect(priceRangeMax).toHaveValue(search.max);
+    expect(marketPriceIndications[0]).toHaveTextContent('33.33% above');
+    expect(marketPriceIndications[1]).toHaveTextContent('>99.99% above');
+  });
+
+  test('run overlapping form with user market price above max price', async () => {
+    // Set search params
+    const search = {
+      base: debugTokens.ETH,
+      quote: debugTokens.USDC,
+      min: '2000',
+      max: '3000',
+      marketPrice: '3200',
+      spread: '0.01',
+      anchor: 'sell',
+      budget: '100',
+    };
+
+    // Render page
+    const { router } = await renderWithRouter({
+      component: () => <CreateOverlappingStrategyPage />,
+      basePath,
+      search,
+    });
+
+    // Check search params
+    const { budget, ...rest } = search;
+    expect(router.state.location.pathname).toBe(basePath);
+    expect(router.state.location.search).toStrictEqual({
+      ...rest,
+      anchor: 'buy',
+    });
+
+    // Check price range input and market price indication
+    const priceRangeMin = await screen.findByTestId('input-min');
+    const priceRangeMax = await screen.findByTestId('input-max');
+    const marketPriceIndications = await screen.findAllByTestId(
+      'market-price-indication'
+    );
+    expect(priceRangeMin).toHaveValue(search.min);
+    expect(priceRangeMax).toHaveValue(search.max);
+    expect(marketPriceIndications[0]).toHaveTextContent('37.50% below');
+    expect(marketPriceIndications[1]).toHaveTextContent('6.25% below');
+  });
 });
