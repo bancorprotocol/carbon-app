5154333db1c43ab3e5dcb3808847d78795c0bef7
Tiago Silva <tfn.da.silva@gmail.com>
2024-07-12 19:06:14 +0100

    Update test utils
    - Add jsdoc to mock server class
    - Add mock tokenListHandlers

--- src/libs/testing-library/utils.ts
+++ src/libs/testing-library/utils.ts
@@ -10,26 +10,98 @@ import { parseSearchWith } from 'libs/routing/utils';
 import { isAddress } from 'ethers/lib/utils';
 import { RequestHandler } from 'msw';
 import { setupServer } from 'msw/node';
-import { afterAll, beforeAll } from 'vitest';
+import { HttpResponse, http } from 'msw';
+import tokenListsMock from '../../../e2e/mocks/tokenLists.json';
 
+/**
+ * Creates a handler for fetching market rates based on the provided market rates object.
+ * This handler intercepts HTTP GET requests to the 'market-rate' endpoint,
+ * extracts the 'address' query parameter, and returns the corresponding market rates
+ * from the provided marketRates object. If the address is not found, it returns a 404 response.
+ *
+ * @param {Record<string, Record<string, number>>} marketRates - An object mapping addresses
+ *        to their respective market rates. Each address key maps to another object where
+ *        each key is a market identifier and its value is the rate.
+ * @returns An HTTP handler function that can be passed to the MockServer handlers
+ */
+export const marketRateHandler = (
+  marketRates: Record<string, Record<string, number>>
+) => {
+  return http.get('**/*/market-rate', ({ request }) => {
+    const queryParams = new URL(request.url).searchParams;
+    const address = queryParams.get('address')?.toLowerCase();
+    if (!address || !marketRates[address])
+      return new HttpResponse(null, { status: 404 });
+
+    return HttpResponse.json({
+      data: marketRates[address],
+    });
+  });
+};
+
+/**
+ * Array of HTTP GET request handlers for each token list URL defined in `tokenLists.json`.
+ * Each handler intercepts requests to its corresponding URL and returns the associated token list data
+ * as a JSON response. If the requested token list does not exist in `tokenListsMock`, a 404 response is returned.
+ */
+export const tokenListHandlers = Object.keys(tokenListsMock).map(
+  (tokenList) => {
+    return http.get(tokenList, () => {
+      const json = tokenListsMock[tokenList as keyof typeof tokenListsMock];
+      if (!json) return new HttpResponse(null, { status: 404 });
+
+      return HttpResponse.json({
+        data: json,
+      });
+    });
+  }
+);
+
+/**
+ * MockServer is a utility class for managing a mock server for testing.
+ * It allows adding and resetting request handlers for the server.
+ */
 export class MockServer {
   private server;
+
+  /**
+   * Constructs a new instance of MockServer.
+   * @param {RequestHandler[]} handlers - An optional array of initial request handlers.
+   */
   constructor(private handlers: RequestHandler[] = []) {
     this.server = setupServer(...this.handlers);
   }
 
+  /**
+   * Adds a new request handler to the server.
+   * @param {RequestHandler} handler - The request handler to add.
+   */
   addHandler(handler: RequestHandler) {
     this.handlers.push(handler);
     this.server.resetHandlers(...this.handlers);
   }
 
-  reset() {
-    this.server.resetHandlers();
+  /**
+   * Resets the server's request handlers to their initial state.
+   * @param {RequestHandler[]} handlers - An optional array of initial request handlers.
+   */
+  reset(handlers?: RequestHandler[]) {
+    this.server.resetHandlers(...(handlers ?? []));
   }
 
+  /**
+   * Starts the mock server and sets up vitest lifecycle hooks to listen for requests
+   * and close the server before and after all tests, respectively.
+   */
   start() {
-    beforeAll(() => this.server.listen({ onUnhandledRequest: 'error' }));
-    afterAll(() => this.server.close());
+    this.server.listen({ onUnhandledRequest: 'error' });
+  }
+
+  /**
+   * Close the mock server.
+   */
+  close() {
+    this.server.close();
   }
 }
 
@@ -43,6 +115,16 @@ export const tokens = [
     address: debugTokens.USDC,
     decimals: 6,
     symbol: 'USDC',
+  },
+  {
+    address: debugTokens.WBTC,
+    decimals: 8,
+    symbol: 'WBTC',
+  },
+  {
+    address: debugTokens.USDT,
+    decimals: 6,
+    symbol: 'USDT',
   },
 ];
 
