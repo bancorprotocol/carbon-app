c7b431121cc3110a369f67c1d2ca7c118688f3e7
Tiago Silva <tfn.da.silva@gmail.com>
2024-07-15 14:25:10 +0100

    Remove quicknode from sei production config

--- src/config/sei/production.ts
+++ src/config/sei/production.ts
@@ -6,9 +6,6 @@ const config: AppConfig = {
   mode: 'production',
   network: {
     ...commonConfig.network,
-    rpc: {
-      url: 'https://holy-frequent-sky.sei-pacific.quiknode.pro/8d454102397f7c651636c3c832e6451a5a7f5928/',
-    },
   },
   sentryDSN:
     'https://1aaa1b99875949cdb08089743dcc1ec5@o1249488.ingest.us.sentry.io/4505074572197888',
