71dddebe2a2dc4f0a0beef1905368310f3987675
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2024-07-16 17:09:35 +0200

    use existing strategy as base for duplication

--- src/components/strategies/create/useDuplicateStrategy.ts
+++ src/components/strategies/create/useDuplicateStrategy.ts
@@ -1,58 +1,57 @@
-import { useNavigate } from 'libs/routing';
+import { StrategyType, useNavigate } from 'libs/routing';
 import { Strategy } from 'libs/queries';
 import { getRoundedSpread } from 'components/strategies/overlapping/utils';
-import {
-  isOverlappingStrategy,
-  isLimitOrder,
-} from 'components/strategies/common/utils';
+import { isLimitOrder } from 'components/strategies/common/utils';
 
-export const useDuplicate = () => {
+export const useDuplicate = (type: StrategyType) => {
   const navigate = useNavigate();
   return ({ base, quote, order0, order1 }: Strategy) => {
-    const isBuyEmpty = !+order0.endRate;
-    const isSellEmpty = !+order1.endRate;
-    const isDisposable = isBuyEmpty || isSellEmpty;
-    if (isDisposable) {
-      const order = isBuyEmpty ? order1 : order0;
-      navigate({
-        to: '/strategies/create/disposable',
-        search: {
-          base: base.address,
-          quote: quote.address,
-          min: order.startRate,
-          max: order.endRate,
-          budget: order.balance,
-          settings: isLimitOrder(order) ? 'limit' : 'range',
-          direction: isBuyEmpty ? 'sell' : 'buy',
-        },
-      });
-    } else if (isOverlappingStrategy({ order0, order1 })) {
-      navigate({
-        to: '/strategies/create/overlapping',
-        search: {
-          base: base.address,
-          quote: quote.address,
-          min: order0.startRate,
-          max: order1.endRate,
-          spread: getRoundedSpread({ order0, order1 }).toString(),
-        },
-      });
-    } else {
-      navigate({
-        to: '/strategies/create/recurring',
-        search: {
-          base: base.address,
-          quote: quote.address,
-          buyMin: order0.startRate,
-          buyMax: order0.endRate,
-          buyBudget: order0.balance,
-          buySettings: isLimitOrder(order0) ? 'limit' : 'range',
-          sellMin: order1.startRate,
-          sellMax: order1.endRate,
-          sellBudget: order1.balance,
-          sellSettings: isLimitOrder(order1) ? 'limit' : 'range',
-        },
-      });
+    switch (type) {
+      case 'disposable': {
+        const isBuyEmpty = !+order0.endRate;
+        const order = isBuyEmpty ? order1 : order0;
+        return navigate({
+          to: '/strategies/create/disposable',
+          search: {
+            base: base.address,
+            quote: quote.address,
+            min: order.startRate,
+            max: order.endRate,
+            budget: order.balance,
+            settings: isLimitOrder(order) ? 'limit' : 'range',
+            direction: isBuyEmpty ? 'sell' : 'buy',
+          },
+        });
+      }
+      case 'overlapping': {
+        return navigate({
+          to: '/strategies/create/overlapping',
+          search: {
+            base: base.address,
+            quote: quote.address,
+            min: order0.startRate,
+            max: order1.endRate,
+            spread: getRoundedSpread({ order0, order1 }).toString(),
+          },
+        });
+      }
+      case 'recurring': {
+        return navigate({
+          to: '/strategies/create/recurring',
+          search: {
+            base: base.address,
+            quote: quote.address,
+            buyMin: order0.startRate,
+            buyMax: order0.endRate,
+            buyBudget: order0.balance,
+            buySettings: isLimitOrder(order0) ? 'limit' : 'range',
+            sellMin: order1.startRate,
+            sellMax: order1.endRate,
+            sellBudget: order1.balance,
+            sellSettings: isLimitOrder(order1) ? 'limit' : 'range',
+          },
+        });
+      }
     }
   };
 };
--- src/libs/modals/modals/ModalDuplicateStrategy/ModalDuplicateStrategy.tsx
+++ src/libs/modals/modals/ModalDuplicateStrategy/ModalDuplicateStrategy.tsx
@@ -10,9 +10,18 @@ import { getUndercutStrategy } from './utils';
 import { isOverlappingStrategy } from 'components/strategies/common/utils';
 import { useNavigate } from '@tanstack/react-router';
 import { getRoundedSpread } from 'components/strategies/overlapping/utils';
+import { StrategyType } from 'libs/routing';
 
 export type ModalDuplicateStrategyData = {
   strategy: Strategy;
+};
+
+const getStrategyType = (strategy: Strategy): StrategyType => {
+  const isBuyEmpty = !+strategy.order0.endRate;
+  const isSellEmpty = !+strategy.order1.endRate;
+  if (isBuyEmpty || isSellEmpty) return 'disposable';
+  if (isOverlappingStrategy(strategy)) return 'overlapping';
+  return 'recurring';
 };
 
 export const ModalDuplicateStrategy: ModalFC<ModalDuplicateStrategyData> = ({
@@ -20,7 +29,7 @@ export const ModalDuplicateStrategy: ModalFC<ModalDuplicateStrategyData> = ({
   data: { strategy },
 }) => {
   const navigate = useNavigate();
-  const duplicate = useDuplicate();
+  const duplicate = useDuplicate(getStrategyType(strategy));
   const { closeModal } = useModal();
   const undercutDifference = 0.001;
 
