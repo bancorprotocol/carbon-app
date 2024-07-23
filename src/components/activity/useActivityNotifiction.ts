52e9a71a765f0eaa1c48aa7dbd89cbaf6d9617eb
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2024-07-23 11:31:32 +0200

    prevent notification when disconnecting

--- src/components/activity/useActivityNotifiction.ts
+++ src/components/activity/useActivityNotifiction.ts
@@ -5,7 +5,7 @@ import { useNotifications } from 'hooks/useNotifications';
 
 export const useActivityNotifications = () => {
   const { user } = useWagmi();
-  const [previousUser, setPreviousUser] = useState<string | null>(null);
+  const [previousUser, setPreviousUser] = useState<string>();
   const [previous, setPrevious] = useState<number | null>(null);
   const query = useActivityQuery({ ownerId: user });
   const allActivities = query.data || [];
@@ -15,9 +15,9 @@ export const useActivityNotifications = () => {
   const { dispatchNotification } = useNotifications();
 
   useEffect(() => {
-    if (query.isPending) return;
+    if (query.fetchStatus !== 'idle') return;
     // We need to keep this in the same useEffect to force re-evaluate previous in next render
-    if (user && user !== previousUser) {
+    if (user !== previousUser) {
       setPreviousUser(user);
       setPrevious(null);
       return;
@@ -27,7 +27,7 @@ export const useActivityNotifications = () => {
       // Sorted by date desc
       for (let i = 0; i < length - previous; i++) {
         const activity = buyOrSell[i];
-        dispatchNotification('activity', { activity });
+        if (user) dispatchNotification('activity', { activity });
       }
     }
     setPrevious(length);
@@ -35,7 +35,7 @@ export const useActivityNotifications = () => {
     buyOrSell,
     dispatchNotification,
     previous,
-    query.isPending,
+    query.fetchStatus,
     previousUser,
     user,
   ]);
