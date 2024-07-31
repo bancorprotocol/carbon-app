c420ded60ac289fbc4f481de31a015048c79701a
Tiago Silva <tfn.da.silva@gmail.com>
2024-07-31 16:05:54 +0100

    Add more popular pairs and popular tokens

--- src/config/celo/common.ts
+++ src/config/celo/common.ts
@@ -7,8 +7,11 @@ const addresses = {
   CELO_ALFAJORES: '0xF194afDf50B03e69Bd7D057c1Aa9e10c9954E4C9',
   ZERO: '0x0000000000000000000000000000000000000000',
   CUSD: '0x765de816845861e75a25fca122bb6898b8b1282a',
+  CEUR: '0xD8763CBa276a3738E6DE85b4b3bF5FDed6D6cA73',
+  CETH: '0x2DEf4285787d58a2f811AF24755A8150622f4361',
   USDC: '0xceba9300f2b948710d2653dd7b07f33a8b32118c',
   USDT: '0x48065fbBE25f71C9282ddf5e1cD6D6A887483D5e',
+  WETH: '0x66803FB87aBd4aaC3cbB3fAd7C3aa01f6F3FB207',
 };
 
 export const commonConfig: AppConfig = {
@@ -50,10 +53,30 @@ export const commonConfig: AppConfig = {
     [addresses.CELO, addresses.CUSD],
     [addresses.CELO, addresses.USDC],
     [addresses.CELO, addresses.USDT],
+    [addresses.CELO, addresses.CUSD],
+    [addresses.CELO, addresses.CEUR],
+    [addresses.CELO, addresses.CETH],
+    [addresses.CELO, addresses.WETH],
   ],
   popularTokens: {
-    base: [addresses.CELO, addresses.CUSD, addresses.USDT, addresses.USDC],
-    quote: [addresses.CELO, addresses.CUSD, addresses.USDT, addresses.USDC],
+    base: [
+      addresses.CELO,
+      addresses.CUSD,
+      addresses.CEUR,
+      addresses.CETH,
+      addresses.USDT,
+      addresses.USDC,
+      addresses.WETH,
+    ],
+    quote: [
+      addresses.CELO,
+      addresses.CUSD,
+      addresses.CEUR,
+      addresses.CETH,
+      addresses.USDT,
+      addresses.USDC,
+      addresses.WETH,
+    ],
   },
   addresses: {
     tokens: addresses,
