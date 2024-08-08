5aa52b52d34b3f13fba1fd332a08f5f6e29a021e
Tiago Silva <tfn.da.silva@gmail.com>
2024-08-08 09:12:38 +0100

    Remove force Number conversion for limit and offset

--- src/components/activity/ActivityProvider.tsx
+++ src/components/activity/ActivityProvider.tsx
@@ -155,10 +155,7 @@ export function useActivity(): ActivityContextType {
 
 export function useActivityPagination() {
   const { size = 0, searchParams, setSearchParams } = useActivity();
-  const { limit: rawLimit = 10, offset: rawOffset = 0 } = searchParams;
-
-  const limit = Number(rawLimit);
-  const offset = Number(rawOffset);
+  const { limit = 10, offset = 0 } = searchParams;
 
   const currentPage = Math.floor(offset / limit) + 1;
   const maxPage = Math.ceil(size / limit);
