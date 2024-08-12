9becc14da89f5bef53c18bc516aaf7a7c658cf8e
Tiago Silva <tfn.da.silva@gmail.com>
2024-08-12 14:23:06 +0100

    Remove link from Simulate Strategy text next to back button in simulator result page

--- src/pages/simulator/result/index.tsx
+++ src/pages/simulator/result/index.tsx
@@ -5,7 +5,6 @@ import { SimResultSummary } from 'components/simulator/result/SimResultSummary';
 import { useSimulator } from 'components/simulator/result/SimulatorProvider';
 import { useBreakpoints } from 'hooks/useBreakpoints';
 import { useCallback, useEffect } from 'react';
-import { ReactComponent as IconChevronLeft } from 'assets/icons/chevron-left.svg';
 import { wait } from 'utils/helpers';
 import { THREE_SECONDS_IN_MS } from 'utils/time';
 import { BackButton } from 'components/common/BackButton';
@@ -35,47 +34,47 @@ export const SimulatorResultPage = () => {
   return (
     <div className="p-20">
       {simulationType === 'recurring' && (
-        <Link
-          to="/simulate/recurring"
-          search={{
-            baseToken: ctx.search.baseToken,
-            quoteToken: ctx.search.quoteToken,
-            start: ctx.search.start,
-            end: ctx.search.end,
-            buyMin: ctx.search.buyMin,
-            buyMax: ctx.search.buyMax,
-            buyBudget: ctx.search.buyBudget,
-            sellMin: ctx.search.sellMin,
-            sellMax: ctx.search.sellMax,
-            sellBudget: ctx.search.sellBudget,
-            buyIsRange: ctx.search.buyIsRange,
-            sellIsRange: ctx.search.sellIsRange,
-          }}
-          className="text-24 font-weight-500 mb-16 flex items-center"
-        >
-          <BackButton className="mr-16" />
+        <div className="text-24 font-weight-500 mb-16 flex items-center">
+          <Link
+            to="/simulate/recurring"
+            search={{
+              baseToken: ctx.search.baseToken,
+              quoteToken: ctx.search.quoteToken,
+              start: ctx.search.start,
+              end: ctx.search.end,
+              buyMin: ctx.search.buyMin,
+              buyMax: ctx.search.buyMax,
+              buyBudget: ctx.search.buyBudget,
+              sellMin: ctx.search.sellMin,
+              sellMax: ctx.search.sellMax,
+              sellBudget: ctx.search.sellBudget,
+              buyIsRange: ctx.search.buyIsRange,
+              sellIsRange: ctx.search.sellIsRange,
+            }}
+          >
+            <BackButton className="mr-16" />
+          </Link>
           Simulate Strategy
-        </Link>
+        </div>
       )}
       {simulationType === 'overlapping' && (
-        <Link
-          to="/simulate/overlapping"
-          search={{
-            baseToken: ctx.search.baseToken,
-            quoteToken: ctx.search.quoteToken,
-            start: ctx.search.start,
-            end: ctx.search.end,
-            buyMin: ctx.search.buyMin,
-            sellMax: ctx.search.sellMax,
-            spread: ctx.search.spread,
-          }}
-          className="text-24 font-weight-500 mb-16 flex items-center"
-        >
-          <div className="bg-background-900 hover:bg-background-800 mr-16 flex size-40 items-center justify-center rounded-full">
-            <IconChevronLeft className="size-16" />
-          </div>
+        <div className="text-24 font-weight-500 mb-16 flex items-center">
+          <Link
+            to="/simulate/overlapping"
+            search={{
+              baseToken: ctx.search.baseToken,
+              quoteToken: ctx.search.quoteToken,
+              start: ctx.search.start,
+              end: ctx.search.end,
+              buyMin: ctx.search.buyMin,
+              sellMax: ctx.search.sellMax,
+              spread: ctx.search.spread,
+            }}
+          >
+            <BackButton className="mr-16" />
+          </Link>
           Simulate Strategy
-        </Link>
+        </div>
       )}
 
       <div className="rounded-20 bg-background-900 p-20">
