efde2b834760b2e022e391d333f1fe66705c64e6
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2024-08-22 16:48:46 +0200

    rollback text change

--- src/components/trade/tradeWidget/NotEnoughLiquidity.tsx
+++ src/components/trade/tradeWidget/NotEnoughLiquidity.tsx
@@ -26,16 +26,8 @@ export const NotEnoughLiquidity = ({
         <div className="size-38 bg-error/10 mb-16 flex items-center justify-center rounded-full">
           <IconWarning className="fill-error/100 size-16" />
         </div>
-        <h2 className="font-weight-500 mb-8">No Liquidity Available</h2>
-        <p>
-          Looks like there isn't sufficient liquidity to execute your trade, but
-          don't worry, here's a pro tip:
-          <br />
-          Set your own price with a limit/range order and the built-in solver
-          system will search chain-wide liquidity to help efficiently fill you
-          order, all while you enjoy MEV sandwich attack resistance, zero
-          slippage, and zero fees!
-        </p>
+        <h2 className="font-weight-500 text-14 mb-8">No Liquidity Available</h2>
+        <p>No availble orders at this moment</p>
         <div>
           You can&nbsp;
           <span
