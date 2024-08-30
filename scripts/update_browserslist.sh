1d748710618da2e6be90aaa9ec66413d8609b312
Tiago Silva <tfn.da.silva@gmail.com>
2024-08-30 14:28:06 +0100

    Update browserslist workflow

--- scripts/update_browserslist.sh
+++ scripts/update_browserslist.sh
@@ -9,30 +9,18 @@ fi
 
 # Run the update and store the output
 OUTPUT=$(npx update-browserslist-db@latest)
+CLEAN_OUTPUT=$(echo "$OUTPUT" | sed 's/\x1b\[[0-9;]*m//g')
 {
   echo 'update_output<<EOF'
-  echo "$OUTPUT"
+  echo "$CLEAN_OUTPUT"
   echo EOF
 } >> "$GITHUB_OUTPUT"
 
 # Extract and save version information
 LATEST_VERSION=$(yarn info caniuse-lite version | sed -n '2p' | sed 's/\x1b\[[0-9;]*m//g')
-INSTALLED_VERSION=$(echo "$OUTPUT" | grep 'Installed version:' | awk '{print $3}' | sed 's/\x1b\[[0-9;]*m//g')
 echo "latest_version=$LATEST_VERSION" >> "$GITHUB_OUTPUT"
-echo "installed_version=$INSTALLED_VERSION" >> "$GITHUB_OUTPUT"
 
-echo "::group::Update result"
-echo "$OUTPUT"
-echo "Parsed installed Version: $INSTALLED_VERSION"
-echo "Parsed latest Version: $LATEST_VERSION"
+echo "::group::Update Result"
+echo "$CLEAN_OUTPUT"
+echo "Latest Version: $LATEST_VERSION"
 echo "::endgroup::"
-
-# Determine if an update should be performed
-if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]] || [[ ! "$OUTPUT" == *"caniuse-lite has been successfully updated"* ]]; then
-    echo "No updates were made to Browserslist DB."
-    SHOULD_UPDATE=false
-else
-    SHOULD_UPDATE=true
-fi
-
-echo "should_update=$SHOULD_UPDATE" >> "$GITHUB_OUTPUT"
