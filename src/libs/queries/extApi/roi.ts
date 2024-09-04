d4c5a5b7f5df7b3583844760a9e8fadbcf45f92f
Tiago Silva <tfn.da.silva@gmail.com>
2024-09-04 16:24:36 +0100

    Stop fetching ROI from Carbon API if showStrategyRoi is false

--- src/libs/queries/extApi/roi.ts
+++ src/libs/queries/extApi/roi.ts
@@ -2,11 +2,12 @@ import { useQuery } from '@tanstack/react-query';
 import { QueryKey } from 'libs/queries/queryKey';
 import { FIVE_MIN_IN_MS } from 'utils/time';
 import { carbonApi } from 'utils/carbonApi';
+import config from 'config';
 
 export const useGetRoi = () => {
   return useQuery({
     queryKey: QueryKey.roi(),
-    queryFn: async () => carbonApi.getRoi(),
+    queryFn: async () => !!config.showStrategyRoi && carbonApi.getRoi(),
     refetchInterval: FIVE_MIN_IN_MS,
     staleTime: FIVE_MIN_IN_MS,
   });
