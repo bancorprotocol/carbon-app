6039b62c17ea7eaffe21c5aa78081dfcb63647bf
Tiago Silva <tfn.da.silva@gmail.com>
2024-09-09 13:29:25 +0100

    Change what is displayed for duplicate symbol when the token is the native chain token

--- src/libs/modals/modals/ModalTokenList/ModalTokenListDuplicateWarning.tsx
+++ src/libs/modals/modals/ModalTokenList/ModalTokenListDuplicateWarning.tsx
@@ -13,25 +13,28 @@ type Props = {
 };
 
 export const ModalTokenListDuplicateWarning: FC<Props> = ({ token }) => {
-  const duplicatedTokenTooltipElement = (token: Token) => (
-    <>
-      It appears there might be multiple tokens with the same symbol. Please
-      ensure you select the correct address.
-      <br /> {token.symbol}:<br />
-      <div className="flex items-center">
-        {token.address}
-        {token.address !== NATIVE_TOKEN_ADDRESS && (
+  const isNativeToken = token.address === NATIVE_TOKEN_ADDRESS;
+
+  const duplicatedTokenTooltipElement = (token: Token) => {
+    if (isNativeToken) return 'This is the native token of the chain.';
+
+    return (
+      <>
+        It appears there might be multiple tokens with the same symbol. Please
+        ensure you select the correct address.
+        <br /> {token.symbol}:<br />
+        <div className="flex items-center">
           <NewTabLink to={getExplorerLink('token', token.address)}>
             <IconLink className="ml-6 w-14 text-white/80" />
           </NewTabLink>
-        )}
-      </div>
-    </>
-  );
+        </div>
+      </>
+    );
+  };
 
   return (
     <div className="text-12 flex max-w-full truncate py-2 text-white/60">
-      {shortenString(token.address)}
+      {isNativeToken ? 'Native Gas Token' : shortenString(token.address)}
       <Tooltip maxWidth={450} element={duplicatedTokenTooltipElement(token)}>
         <span>
           <IconWarning className="ml-5 size-14 text-white/60" />
