35c74c3f8ab110fdae7fb9b912d72279b81a597e
Tiago Silva <tfn.da.silva@gmail.com>
2024-09-12 12:12:44 +0100

    Add activityValidators to every validateActivityParams

--- src/libs/routing/routes/strategy.ts
+++ src/libs/routing/routes/strategy.ts
@@ -1,17 +1,17 @@
 import { StrategyPage } from 'pages/strategy';
 import { rootRoute } from './root';
 import { Route } from '@tanstack/react-router';
-import { validNumber, validateSearchParams } from '../utils';
+import { validNumber } from '../utils';
 import {
-  ActivitySearchParams,
   activityValidators,
+  validateActivityParams,
 } from 'components/activity/utils';
 
 export const strategyPage = new Route({
   getParentRoute: () => rootRoute,
   path: 'strategy/$id',
   component: StrategyPage,
-  validateSearch: validateSearchParams<ActivitySearchParams>({
+  validateSearch: validateActivityParams({
     ...activityValidators,
     priceStart: validNumber,
     priceEnd: validNumber,
