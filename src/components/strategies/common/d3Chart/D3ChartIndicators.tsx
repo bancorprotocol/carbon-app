ca2034e67313cc7217c4e71cfc92525c2d959a7f
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2024-09-16 13:54:41 +0200

    use week range

--- src/components/strategies/common/d3Chart/D3ChartIndicators.tsx
+++ src/components/strategies/common/d3Chart/D3ChartIndicators.tsx
@@ -7,6 +7,7 @@ import {
   FloatTooltipContent,
   FloatTooltipTrigger,
 } from 'components/common/tooltip/FloatTooltip';
+import { endOfWeek, isSameDay, isSameWeek, startOfWeek } from 'date-fns';
 import { ScaleBand, ScaleLinear } from 'libs/d3';
 import { Activity } from 'libs/queries/extApi/activity';
 import { SafeDecimal } from 'libs/safedecimal';
@@ -15,25 +16,36 @@ import { FC, ReactNode } from 'react';
 interface Indicator {
   x: number;
   y: number;
+  label: string;
   activities: Activity[];
 }
 
+const formatter = new Intl.DateTimeFormat();
+
+const displayDay = (date: Date) => formatter.format(date);
+const displayWeek = (date: Date) =>
+  formatter.formatRange(startOfWeek(date), endOfWeek(date));
+
 const groupByIndicators = (activities: Activity[], domain: string[]) => {
-  const points = domain.map((d) => Number(d));
+  const points = domain.map((d) => new Date(Number(d) * 1000));
   const operations: Record<number, Indicator> = {};
   const trades: Record<number, Indicator> = {};
+  const isSame = points.length > 60 ? isSameWeek : isSameDay;
+  const getLabel = points.length > 60 ? displayWeek : displayDay;
   for (const activity of activities) {
     const key = (() => {
-      const clostest = points.find((point) => point > activity.timestamp);
-      return clostest ?? points.at(-1)!;
+      const clostest = points.find((point) => isSame(point, activity.date));
+      return (clostest ?? points.at(-1)!).getTime() / 1000;
     })();
-    if (activity.action === 'buy' || activity.action === 'sell') {
-      trades[key] ||= { x: key, y: 0, activities: [] };
-      trades[key].activities.push(activity);
-    } else {
-      operations[key] ||= { x: key, y: 0, activities: [] };
-      operations[key].activities.push(activity);
-    }
+    const action = activity.action;
+    const list = action === 'buy' || action === 'sell' ? trades : operations;
+    list[key] ||= {
+      x: key,
+      y: 0,
+      label: getLabel(activity.date),
+      activities: [],
+    };
+    list[key].activities.push(activity);
   }
   for (const indicator of Object.values(trades)) {
     const sum = indicator.activities.reduce((acc, activity) => {
@@ -69,53 +81,59 @@ export const D3ChartIndicators = (props: D3ChartIndicatorsProps) => {
   const { operations, trades } = groupByIndicators(activities, xScale.domain());
   return (
     <>
-      {operations.map(({ x, activities }, i) => (
-        <IndicatorTooltip key={i} timestamp={x} activities={activities}>
-          <g transform={`translate(${xScale(x.toString())},${height - 10})`}>
-            <rect
-              className="transformBox-fill origin-center rotate-45"
-              x={xScale.bandwidth() / 2 - 4}
-              y={xScale.bandwidth() / 2 - 4}
-              width={8}
-              height={8}
-              fill="white"
-              stroke="black"
-            />
-          </g>
-        </IndicatorTooltip>
-      ))}
-      {trades.map(({ x, y, activities }, i) => (
-        <IndicatorTooltip key={i} timestamp={x} activities={activities}>
-          <g transform={`translate(${xScale(x.toString())},${yScale(y)})`}>
-            <circle
-              cx={xScale.bandwidth() / 2 - 5}
-              cy={xScale.bandwidth() / 2 - 5}
-              r={5}
-              fill="white"
-              stroke="black"
-            />
-          </g>
-        </IndicatorTooltip>
-      ))}
+      {operations.map((indicator, i) => {
+        const { x } = indicator;
+        return (
+          <IndicatorTooltip key={i} indicator={indicator}>
+            <g transform={`translate(${xScale(x.toString())},${height - 10})`}>
+              <rect
+                className="transformBox-fill origin-center rotate-45"
+                x={xScale.bandwidth() / 2 - 4}
+                y={xScale.bandwidth() / 2 - 4}
+                width={8}
+                height={8}
+                fill="white"
+                stroke="black"
+              />
+            </g>
+          </IndicatorTooltip>
+        );
+      })}
+      {trades.map((indicator, i) => {
+        const { x, y } = indicator;
+        return (
+          <IndicatorTooltip key={i} indicator={indicator}>
+            <g transform={`translate(${xScale(x.toString())},${yScale(y)})`}>
+              <circle
+                cx={xScale.bandwidth() / 2 - 5}
+                cy={xScale.bandwidth() / 2 - 5}
+                r={5}
+                fill="white"
+                stroke="black"
+              />
+            </g>
+          </IndicatorTooltip>
+        );
+      })}
     </>
   );
 };
 
 interface TooltipProps {
-  timestamp: number;
-  activities: Activity[];
+  indicator: Indicator;
   children: ReactNode;
 }
 const IndicatorTooltip: FC<TooltipProps> = (props) => {
-  const { activities, timestamp, children } = props;
-  const date = new Date(timestamp * 1000).toLocaleDateString();
+  const { indicator, children } = props;
   return (
     <FloatTooltip>
       <FloatTooltipTrigger>{children}</FloatTooltipTrigger>
       <FloatTooltipContent className="bg-background-800 max-h-[300px] max-w-[250px] overflow-auto rounded p-16">
-        <h4 className="text-12 font-weight-500 mb-10">Date: {date}</h4>
+        <h4 className="text-12 font-weight-500 mb-10">
+          Date: {indicator.label}
+        </h4>
         <ul className="flex list-disc flex-col gap-10 ps-16">
-          {activities.map((activity, j) => (
+          {indicator.activities.map((activity, j) => (
             <li key={j} className="text-12 text-white/80">
               <b>{activityActionName[activity.action]}</b>:&nbsp;
               {activityDescription(activity)}
