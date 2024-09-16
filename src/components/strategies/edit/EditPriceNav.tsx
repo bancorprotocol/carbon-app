2ff24459f2a51f7a835c83bcf12351a220b745c3
Tiago Silva <tfn.da.silva@gmail.com>
2024-09-16 14:48:01 +0100

    Update edit price nav to match trade nav

--- src/components/strategies/edit/EditPriceNav.tsx
+++ src/components/strategies/edit/EditPriceNav.tsx
@@ -1,64 +1,96 @@
-import { useEditStrategyCtx } from './EditStrategyContext';
 import { Link, useParams } from '@tanstack/react-router';
+import { ReactComponent as IconDisposable } from 'assets/icons/disposable.svg';
+import { ReactComponent as IconRecurring } from 'assets/icons/recurring.svg';
+import { ReactComponent as IconOverlapping } from 'assets/icons/overlapping.svg';
+import { Tooltip } from 'components/common/tooltip/Tooltip';
+import { useEditStrategyCtx } from 'components/strategies/edit/EditStrategyContext';
+import { EditDisposableStrategySearch } from 'pages/strategies/edit/prices/disposable';
+import { EditRecurringStrategySearch } from 'pages/strategies/edit/prices/recurring';
+import { EditOverlappingStrategySearch } from 'pages/strategies/edit/prices/overlapping';
+import { Strategy } from 'libs/queries';
 import {
   EditTypes,
   toDisposablePricesSearch,
   toOverlappingPricesSearch,
   toRecurringPricesSearch,
 } from 'libs/routing/routes/strategyEdit';
-import { ReactComponent as IconDisposable } from 'assets/icons/disposable.svg';
-import { ReactComponent as IconRecurring } from 'assets/icons/recurring.svg';
-import { ReactComponent as IconOverlapping } from 'assets/icons/overlapping.svg';
-import { FC } from 'react';
 
-interface Props {
-  editType: EditTypes;
-}
-export const EditPriceNav: FC<Props> = ({ editType }) => {
+const links = [
+  {
+    label: 'Limit / Range',
+    svg: <IconDisposable className="hidden size-14 md:inline" />,
+    to: '/strategies/edit/$strategyId/prices/disposable',
+    text: 'Buy or sell at a specific price, or gradually scale in or out of a position.',
+    id: 'disposable',
+  },
+  {
+    label: 'Recurring',
+    svg: <IconRecurring className="hidden size-14 md:inline" />,
+    to: '/strategies/edit/$strategyId/prices/recurring',
+    text: 'Create an automated trading cycle of buy low/sell high with two separate orders.',
+    id: 'recurring',
+  },
+  {
+    label: 'Concentrated',
+    svg: <IconOverlapping className="hidden size-14 md:inline" />,
+    to: '/strategies/edit/$strategyId/prices/overlapping',
+    text: 'Buy and sell within custom parameters with custom fee tier and auto-compounding fees.',
+    id: 'overlapping',
+  },
+] as const;
+
+export const toPricesSearch = (
+  strategy: Strategy,
+  editType: 'editPrices' | 'renew',
+  strategyType: 'disposable' | 'recurring' | 'overlapping'
+):
+  | EditDisposableStrategySearch
+  | EditRecurringStrategySearch
+  | EditOverlappingStrategySearch => {
+  switch (strategyType) {
+    case 'disposable':
+      return toDisposablePricesSearch(strategy, editType);
+    case 'recurring':
+      return toRecurringPricesSearch(strategy, editType);
+    case 'overlapping':
+      return toOverlappingPricesSearch(strategy, editType);
+  }
+};
+
+export const EditPriceNav = ({ editType }: { editType: EditTypes }) => {
   const params = useParams({ from: '/strategies/edit/$strategyId' });
-  const isDisposable = window.location.pathname.includes('disposable');
-  const isRecurring = window.location.pathname.includes('recurring');
-  const isOverlapping = window.location.pathname.includes('overlapping');
+  const pathName = window.location.pathname;
+
   const { strategy } = useEditStrategyCtx();
   if (editType !== 'editPrices' && editType !== 'renew') return;
+
   return (
-    <nav
-      className="border-background-900 text-14 flex gap-4 rounded-full border-2 p-6"
-      aria-label="Switch between type of strategy"
-    >
-      <Link
-        className="aria-current-page:bg-white/10 aria-current-page:text-white flex flex-1 items-center justify-center gap-8 rounded-full bg-transparent py-4 text-center text-white/60 hover:text-white/80"
-        to="/strategies/edit/$strategyId/prices/disposable"
-        search={toDisposablePricesSearch(strategy, editType)}
-        params={params}
-        replace={true}
-        aria-current={isDisposable ? 'page' : 'false'}
+    <article className="bg-background-900 grid gap-20 rounded p-20">
+      <h2 id="edit-strategy-nav" className="text-18">
+        Trading Strategy
+      </h2>
+      <nav
+        aria-labelledby="edit-strategy-nav"
+        className="text-14 grid grid-cols-2 gap-8"
+        aria-label="Switch between type of strategy"
       >
-        <IconDisposable className="hidden size-14 md:inline" />
-        Limit / Range
-      </Link>
-      <Link
-        className="aria-current-page:bg-white/10 aria-current-page:text-white flex flex-1 items-center justify-center gap-8 rounded-full bg-transparent py-4 text-center text-white/60 hover:text-white/80"
-        to="/strategies/edit/$strategyId/prices/recurring"
-        search={toRecurringPricesSearch(strategy, editType)}
-        params={params}
-        replace={true}
-        aria-current={isRecurring ? 'page' : 'false'}
-      >
-        <IconRecurring className="hidden size-14 md:inline" />
-        Recurring
-      </Link>
-      <Link
-        className="aria-current-page:bg-white/10 aria-current-page:text-white flex flex-1 items-center justify-center gap-8 rounded-full bg-transparent py-4 text-center text-white/60 hover:text-white/80"
-        to="/strategies/edit/$strategyId/prices/overlapping"
-        search={toOverlappingPricesSearch(strategy, editType)}
-        params={params}
-        replace={true}
-        aria-current={isOverlapping ? 'page' : 'false'}
-      >
-        <IconOverlapping className="hidden size-14 md:inline" />
-        Concentrated
-      </Link>
-    </nav>
+        {links.map((link) => (
+          <Link
+            key={link.id}
+            to={link.to}
+            search={toPricesSearch(strategy, editType, link.id)}
+            params={params}
+            replace={true}
+            aria-current={pathName.includes(link.id) ? 'page' : 'false'}
+            data-testid={`edit-${link.id}`}
+            className="rounded-8 hover:border-background-400 flex items-center justify-center gap-8 border border-transparent bg-black p-8 text-white/60 aria-[current=page]:border-white aria-[current=page]:text-white"
+          >
+            {link.svg}
+            {link.label}
+            <Tooltip element={link.text} iconClassName="size-14" />
+          </Link>
+        ))}
+      </nav>
+    </article>
   );
 };
