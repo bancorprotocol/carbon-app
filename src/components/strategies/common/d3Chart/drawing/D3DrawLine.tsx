7c74a75399bebb5e257c9b4212299e52f825a532
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2024-10-15 15:22:37 +0200

    move drawing into D3Candlestick

--- src/components/strategies/common/d3Chart/drawing/D3DrawLine.tsx
+++ src/components/strategies/common/d3Chart/drawing/D3DrawLine.tsx
@@ -1,7 +1,37 @@
-export const D3DrawLine = () => {
+import { D3ChartSettings } from 'libs/d3';
+
+import { FC, MouseEvent, useState } from 'react';
+
+interface Props {
+  dms: D3ChartSettings;
+}
+
+export const D3DrawLine: FC<Props> = ({ dms }) => {
+  const [drawing, setDrawing] = useState(false);
+  const [points, setPoints] = useState<{ x: number; y: number }[]>([]);
+
+  const start = (event: MouseEvent) => {
+    const svg = document.getElementById('interactive-chart');
+    const root = svg!.getBoundingClientRect();
+    const x = event.clientX - root.x;
+    const y = event.clientY - root.y;
+    setPoints((points) => [...points, { x, y }]);
+  };
+
   return (
     <>
-      <rect />
+      <rect
+        className="chart-drawing-canvas"
+        x="0"
+        y="0"
+        width={dms.boundedWidth}
+        height={dms.boundedHeight}
+        fillOpacity="0"
+        onClick={start}
+      />
+      {points.map(({ x, y }) => (
+        <circle key={x + '-' + y} cx={x} cy={y} r="5" fill="white" />
+      ))}
     </>
   );
 };
