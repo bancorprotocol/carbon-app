4e27c94cdfde21945dd1f8e9c1cb26508a429130
Grandschtroumpf <francois.guezengar@hotmail.fr>
2024-10-28 17:17:03 +0100

    remove date picker

--- src/components/strategies/common/StrategyChartSection.tsx
+++ src/components/strategies/common/StrategyChartSection.tsx
@@ -1,39 +1,10 @@
 import { FC, ReactNode } from 'react';
-import {
-  DateRangePicker,
-  datePickerPresets,
-} from 'components/common/datePicker/DateRangePicker';
-import {
-  defaultEndDate,
-  defaultStartDate,
-} from 'components/strategies/common/utils';
-import { fromUnixUTC, toUnixUTC } from 'components/simulator/utils';
-import { datePickerDisabledDays } from 'components/simulator/result/SimResultChartHeader';
-import { useNavigate, useSearch } from '@tanstack/react-router';
-import { TradeSearch } from 'libs/routing';
-import config from 'config';
 
 interface Props {
   children: ReactNode;
 }
 
 export const StrategyChartSection: FC<Props> = ({ children }) => {
-  const navigate = useNavigate({ from: '/trade' });
-  const { priceStart, priceEnd } = useSearch({ strict: false }) as TradeSearch;
-  const onDatePickerConfirm = (props: { start?: Date; end?: Date }) => {
-    const { start, end } = props;
-    if (!start || !end) return;
-    navigate({
-      search: (previous) => ({
-        ...previous,
-        priceStart: toUnixUTC(start),
-        priceEnd: toUnixUTC(end),
-      }),
-      resetScroll: false,
-      replace: true,
-    });
-  };
-  const isNativeChart = config.ui.priceChart === 'native';
   return (
     <section
       aria-labelledby="price-chart-title"
@@ -43,20 +14,6 @@ export const StrategyChartSection: FC<Props> = ({ children }) => {
         <h2 id="price-chart-title" className="text-18">
           Price Chart
         </h2>
-        {isNativeChart && (
-          <DateRangePicker
-            defaultStart={defaultStartDate()}
-            defaultEnd={defaultEndDate()}
-            start={fromUnixUTC(priceStart)}
-            end={fromUnixUTC(priceEnd)}
-            onConfirm={onDatePickerConfirm}
-            presets={datePickerPresets}
-            options={{
-              disabled: datePickerDisabledDays,
-            }}
-            required
-          />
-        )}
       </header>
       {children}
     </section>
