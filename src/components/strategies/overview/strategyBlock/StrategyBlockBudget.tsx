b572e1be7fa55fe265ee9eb072cc3ee4bb9b9a97
Grandschtroumpf <francois.guezengar@hotmail.fr>
2024-11-04 16:55:59 +0100

    remove/rename tooltips

--- src/components/strategies/overview/strategyBlock/StrategyBlockBudget.tsx
+++ src/components/strategies/overview/strategyBlock/StrategyBlockBudget.tsx
@@ -1,6 +1,4 @@
 import { FC } from 'react';
-import { Tooltip } from 'components/common/tooltip/Tooltip';
-import { ReactComponent as IconTooltip } from 'assets/icons/tooltip.svg';
 import { cn, prettifyNumber } from 'utils/helpers';
 import { useFiatCurrency } from 'hooks/useFiatCurrency';
 import { StrategyWithFiat } from 'libs/queries';
@@ -21,16 +19,13 @@ export const StrategyBlockBudget: FC<Props> = ({ strategy }) => {
   return (
     <article
       className={cn(
-        'rounded-8 border-background-800 flex w-2/3 flex-col border-2 p-16',
+        'rounded-8 border-background-800 flex w-3/5 flex-col border-2 p-16',
         strategy.status === 'active' ? '' : 'opacity-50'
       )}
     >
-      <Tooltip element={<TooltipContent />}>
-        <h4 className="text-12 flex items-center gap-4 text-white/60">
-          Total Budget
-          <IconTooltip className="size-10" />
-        </h4>
-      </Tooltip>
+      <h4 className="text-12 flex items-center gap-4 text-white/60">
+        Total Budget
+      </h4>
       <p
         className={cn(
           'text-18 font-weight-500',
@@ -42,10 +37,4 @@ export const StrategyBlockBudget: FC<Props> = ({ strategy }) => {
       </p>
     </article>
   );
-};
-
-const TooltipContent: FC = () => {
-  const currency = useFiatCurrency();
-  const fiatSymbol = currency.selectedFiatCurrency;
-  return <p>Sum of the {fiatSymbol} value of the token budgets.</p>;
 };
--- src/components/strategies/overview/strategyBlock/StrategyBlockTradeCount.tsx
+++ src/components/strategies/overview/strategyBlock/StrategyBlockTradeCount.tsx
@@ -1,6 +1,4 @@
 import { FC } from 'react';
-import { Tooltip } from 'components/common/tooltip/Tooltip';
-import { ReactComponent as IconTooltip } from 'assets/icons/tooltip.svg';
 import { cn, prettifyNumber } from 'utils/helpers';
 import { StrategyWithFiat } from 'libs/queries';
 
@@ -16,17 +14,14 @@ export const StrategyBlockTradeCount: FC<Props> = ({ strategy }) => {
   return (
     <article
       className={cn(
-        'rounded-8 border-background-800 flex w-1/3 flex-col border-2 p-16',
+        'rounded-8 border-background-800 flex w-2/5 flex-col border-2 p-16',
         strategy.status === 'active' ? '' : 'opacity-50'
       )}
     >
-      <Tooltip element="Trade count indicates how often this strategy has been used. More trades suggest it was relevant and actively engaged in trading.">
-        <h4 className="text-12 flex items-center gap-4 text-white/60">
-          Trades
-          <IconTooltip className="size-10" />
-        </h4>
-      </Tooltip>
-      <p className="text-18 font-weight-500">{count}</p>
+      <h4 className="text-12 flex items-center gap-4 text-white/60">
+        Trade Count
+      </h4>
+      <p className="text-18 font-weight-500 truncate">{count}</p>
     </article>
   );
 };
