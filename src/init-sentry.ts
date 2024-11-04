65da3988243da5cba48214e43a97260796ac9e55
Doron Zavelevsky <zavelevsky@gmail.com>
2024-11-04 10:07:08 +0000

    unmask sentry replays

--- src/init-sentry.ts
+++ src/init-sentry.ts
@@ -7,7 +7,11 @@ if (config.sentryDSN) {
     dsn: config.sentryDSN,
     integrations: [
       Sentry.tanstackRouterBrowserTracingIntegration(router),
-      Sentry.replayIntegration(),
+      Sentry.replayIntegration({
+        // NOTE: This will disable built-in masking. Only use this if your site has no sensitive data, or if you've already set up other options for masking or blocking relevant data, such as 'ignore', 'block', 'mask' and 'maskFn'.
+        maskAllText: false,
+        blockAllMedia: false,
+      }),
       Sentry.captureConsoleIntegration({ levels: ['error'] }),
     ],
     // Performance Monitoring
