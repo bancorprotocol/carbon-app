5fad4a4664872dfe83b965e1d768c082387e7cb6
Grandschtroumpf <francois.guezengar@hotmail.fr>
2024-12-17 10:12:53 +0100

    export activity limit

--- src/components/activity/ActivityExport.tsx
+++ src/components/activity/ActivityExport.tsx
@@ -6,7 +6,7 @@ import { useActivity } from './ActivityProvider';
 import { carbonApi } from 'utils/carbonApi';
 import { useTokens } from 'hooks/useTokens';
 import { toActivities } from './useActivityQuery';
-import { useState } from 'react';
+import { MouseEvent, useRef, useState } from 'react';
 
 export const getActivityCSV = (activities: Activity[]) => {
   const header = [
@@ -66,14 +66,27 @@ export const getActivityCSV = (activities: Activity[]) => {
   return encodeURI(csvContent);
 };
 
+const limit = 10_000;
 export const ActivityExport = () => {
+  const ref = useRef<HTMLDialogElement>(null);
   const [loading, setLoading] = useState(false);
-  const { queryParams } = useActivity();
+  const { queryParams, size } = useActivity();
   const { tokensMap } = useTokens();
+
+  const close = (e: MouseEvent<HTMLDialogElement>) => {
+    if (e.target !== e.currentTarget) return;
+    e.currentTarget.close();
+    setLoading(false);
+  };
 
   const download = async () => {
     setLoading(true);
-    const data = await carbonApi.getActivity(queryParams);
+    if (size && size > limit) ref.current?.showModal();
+    const data = await carbonApi.getActivity({
+      ...queryParams,
+      offset: 0,
+      limit,
+    });
     const activities = toActivities(data, tokensMap);
     const anchor = document.createElement('a');
     anchor.href = getActivityCSV(activities);
@@ -82,14 +95,31 @@ export const ActivityExport = () => {
     setLoading(false);
   };
   return (
-    <button
-      type="button"
-      onClick={download}
-      disabled={loading}
-      className="border-background-800 text-12 hover:border-background-700 hover:bg-background-800 flex items-center gap-8 rounded-full border-2 px-12 py-8 disabled:pointer-events-none disabled:opacity-60"
-    >
-      <IconDownloadFile className="text-primary size-14" />
-      <span>{loading ? 'Exporting' : 'Export'}</span>
-    </button>
+    <>
+      <button
+        type="button"
+        onClick={download}
+        disabled={loading}
+        className="border-background-800 text-12 hover:border-background-700 hover:bg-background-800 flex items-center gap-8 rounded-full border-2 px-12 py-8 disabled:pointer-events-none disabled:opacity-60"
+      >
+        <IconDownloadFile className="text-primary size-14" />
+        <span>{loading ? 'Exporting' : 'Export'}</span>
+      </button>
+      {!!size && size > limit && (
+        <dialog className="modal" ref={ref} onClick={close}>
+          <form method="dialog" className="grid gap-16">
+            <p>
+              The Export is limited to 10.000 transactions.
+              <br />
+              Consider changing the filters
+            </p>
+            <hr className="border-1 border-t-white/60" />
+            <button type="submit" value="">
+              Ok
+            </button>
+          </form>
+        </dialog>
+      )}
+    </>
   );
 };
