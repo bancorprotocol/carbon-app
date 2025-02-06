7ca14204f8c0b6957a6ae7c336724815124420fa
Grandschtroumpf <francois.guezengar@hotmail.fr>
2025-02-06 09:58:47 +0100

    update debug mode display

--- src/components/core/menu/mainMenu/MainMenuRight.tsx
+++ src/components/core/menu/mainMenu/MainMenuRight.tsx
@@ -1,22 +1,39 @@
-import { FC } from 'react';
-import { IS_TENDERLY_FORK } from 'libs/wagmi';
+import { FC, useEffect, useState } from 'react';
 import { useBreakpoints } from 'hooks/useBreakpoints';
 import { MainMenuRightWallet } from 'components/core/menu/mainMenu/MainMenuRightWallet';
 import { MainMenuRightNotifications } from 'components/core/menu/mainMenu/MainMenuRightNotifications';
-import { Button } from 'components/common/button';
 import { MainMenuRightBurger } from './MainMenuRightBurger';
 import { useBurgerMenuItems } from './MainMenuRightBurger/useBurgerMenuItems';
 import { MainMenuRightChainSelector } from './MainMenuRightChainSelector';
 import { networks } from 'config';
 import { MainMenuCart } from './MainMenuCart';
+import { lsService } from 'services/localeStorage';
+import { Link } from '@tanstack/react-router';
 import config from 'config';
 
 const TenderlyForkAlert = () => {
-  return IS_TENDERLY_FORK ? (
-    <Button variant="error" size="sm">
-      Fork
-    </Button>
-  ) : null;
+  const [isDebugMode, setIsDebugMode] = useState(false);
+  useEffect(() => {
+    const checkDebugMode = () => {
+      if (!!lsService.getItem('tenderlyRpc')) return setIsDebugMode(true);
+      if (!!lsService.getItem('configOverride')) return setIsDebugMode(true);
+      const api = lsService.getItem('carbonApi');
+      if (api && api !== config.carbonApi) return setIsDebugMode(true);
+      setIsDebugMode(false);
+    };
+    checkDebugMode();
+    window.addEventListener('storage', checkDebugMode);
+    return () => window.removeEventListener('storage', checkDebugMode);
+  }, []);
+  if (!isDebugMode) return;
+  return (
+    <Link
+      to="/debug"
+      className="bg-warning text-14 rounded-full px-16 py-8 text-black"
+    >
+      Debug Mode
+    </Link>
+  );
 };
 
 export const MainMenuRight: FC = () => {
