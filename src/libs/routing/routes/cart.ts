e3a1097772fe91678de4af4c5af7b686611e46f6
Grandschtroumpf <francois.guezengar@hotmail.fr>
2025-02-06 09:29:08 +0100

    redirect if show cart is false

--- src/libs/routing/routes/cart.ts
+++ src/libs/routing/routes/cart.ts
@@ -1,9 +1,15 @@
-import { createRoute } from '@tanstack/react-router';
+import { createRoute, redirect } from '@tanstack/react-router';
 import { rootRoute } from 'libs/routing/routes/root';
 import { CartPage } from 'pages/cart';
+import config from 'config';
 
 export const cartPage = createRoute({
   getParentRoute: () => rootRoute,
   path: '/cart',
   component: CartPage,
+  loader: () => {
+    if (!config.ui.showCart) {
+      throw redirect({ to: '/' });
+    }
+  },
 });
