4be1edbadeb786b20426b037bdf1f977c16c0834
Grandschtroumpf <francois.guezengar@hotmail.fr>
2025-06-12 12:26:25 +0200

    add dialog

--- src/components/core/menu/mainMenu/MainMenuRightWalkthrough.tsx
+++ src/components/core/menu/mainMenu/MainMenuRightWalkthrough.tsx
@@ -1,18 +1,20 @@
-import { FC, useEffect } from 'react';
+import { FC, useEffect, useRef } from 'react';
 import { ReactComponent as IconInfo } from 'assets/icons/tooltip.svg';
 import { Button } from 'components/common/button';
 import { Tooltip } from 'components/common/tooltip/Tooltip';
 import { lsService } from 'services/localeStorage';
 
 export const MainMenuRightWalkthrough: FC = () => {
+  const dialog = useRef<HTMLDialogElement>(null);
   useEffect(() => {
     const hasWalkthrough = lsService.getItem('hasWalkthrough');
     if (!hasWalkthrough) {
-      start();
+      dialog.current?.showModal();
     }
   }, []);
 
   const start = () => {
+    dialog.current?.close();
     (globalThis as any).Storylane.Play({
       type: 'popup',
       demo_type: 'image',
@@ -25,15 +27,37 @@ export const MainMenuRightWalkthrough: FC = () => {
     lsService.setItem('hasWalkthrough', true);
   };
 
+  const lightDismiss = { closedby: 'any' };
+
   return (
-    <Tooltip element="Start Walkthrough">
-      <Button
-        variant="secondary"
-        className="size-40 hidden p-0 md:grid"
-        onClick={start}
+    <>
+      <Tooltip element="Start Walkthrough">
+        <Button
+          variant="secondary"
+          className="size-40 hidden p-0 md:grid"
+          onClick={start}
+        >
+          <IconInfo className="place-self-center size-20" />
+        </Button>
+      </Tooltip>
+      <dialog
+        ref={dialog}
+        className="bg-background-800 rounded w-[500px] h-[300px]"
+        {...lightDismiss}
       >
-        <IconInfo className="place-self-center size-20" />
-      </Button>
-    </Tooltip>
+        <div className="grid gap-16 place-items-center p-16">
+          <h2>Do you want to start a Walkthrough ?</h2>
+          <button
+            className="bg-background-700 rounded px-16 py-8"
+            onClick={start}
+          >
+            Yes I would love that ! Thanks you Bancor
+          </button>
+          <button onClick={() => dialog.current?.close()}>
+            No, your app is so easy to use, but thank you anyway
+          </button>
+        </div>
+      </dialog>
+    </>
   );
 };
