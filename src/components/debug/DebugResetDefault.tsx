1080d1a3fc5b36a721dd6f774ef64b93887314d1
Grandschtroumpf <francois.guezengar@hotmail.fr>
2025-06-24 14:12:42 +0200

    deactivate debug mode for all networks

--- src/components/debug/DebugResetDefault.tsx
+++ src/components/debug/DebugResetDefault.tsx
@@ -1,18 +1,22 @@
-import { useWagmi } from 'libs/wagmi';
 import { Button } from 'components/common/button';
+import { lsService } from 'services/localeStorage';
+import config from 'config';
 
 export const DebugResetDefault = () => {
-  const { handleTenderlyRPC, setImposterAccount } = useWagmi();
-
-  const handleOnClick = () => {
-    handleTenderlyRPC();
-    setImposterAccount();
+  const deactivateOne = () => {
+    lsService.removeItem('imposterAccount');
+    lsService.removeItem('tenderlyRpc');
+    lsService.removeItem('configOverride');
+    lsService.removeItem('carbonApi');
+    location.href = location.href;
   };
 
   return (
-    <div className="rounded-18 bg-background-900 flex flex-col items-center space-y-20 p-20">
-      <h2>Reset to defaults</h2>
-      <Button onClick={handleOnClick}>RESET</Button>
-    </div>
+    <article className="rounded-18 bg-background-900 flex flex-col items-center space-y-20 p-20">
+      <h2>Deactivate Debug Mode</h2>
+      <Button variant="success" onClick={deactivateOne}>
+        Deactive for {config.network.name}
+      </Button>
+    </article>
   );
 };
