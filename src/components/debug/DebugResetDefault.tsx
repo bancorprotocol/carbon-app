e5c4c1354706845d1cfe0c3668b2ed3b85d73ffc
Grandschtroumpf <francois.guezengar@hotmail.fr>
2025-06-24 14:22:20 +0200

    improve text

--- src/components/debug/DebugResetDefault.tsx
+++ src/components/debug/DebugResetDefault.tsx
@@ -12,11 +12,16 @@ export const DebugResetDefault = () => {
   };
 
   return (
-    <article className="rounded-18 bg-background-900 flex flex-col items-center space-y-20 p-20">
+    <article className="rounded-18 bg-background-900 grid place-items-center gap-40 p-20">
       <h2>Deactivate Debug Mode</h2>
       <Button variant="success" onClick={deactivateOne}>
         Deactive for {config.network.name}
       </Button>
+      <p className="text-white/60 text-14 text-center">
+        This will only deactivate debug mode
+        <br /> for the current network:{' '}
+        <b className="text-white">{config.network.name}</b>
+      </p>
     </article>
   );
 };
