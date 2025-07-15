8d3fcff617894d6d036388b3adbdc681bc9c2122
Doron Zavelevsky <zavelevsky@gmail.com>
2025-07-15 15:22:05 +0300

    moved rpc to common conf

--- src/config/tac/production.ts
+++ src/config/tac/production.ts
@@ -4,12 +4,6 @@ import { commonConfig } from './common';
 const config: AppConfig = {
   ...commonConfig,
   mode: 'production',
-  network: {
-    ...commonConfig.network,
-    rpc: {
-      url: 'https://rpc.ankr.com/tac/fa8610ec591a5bec6d5a4412d00b3892c774b8f3cfba3dae4be715aaaf64bdc4',
-    },
-  },
   sentryDSN:
     'https://1aaa1b99875949cdb08089743dcc1ec5@o1249488.ingest.us.sentry.io/4505074572197888',
 };
