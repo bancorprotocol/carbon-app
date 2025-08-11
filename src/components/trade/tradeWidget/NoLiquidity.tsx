14c0fafe547ed3263788ab8777fd9a703190416e
Grandschtroumpf <francois.guezengar@hotmail.fr>
2025-08-11 11:22:06 +0200

    update text

--- src/components/trade/tradeWidget/NoLiquidity.tsx
+++ src/components/trade/tradeWidget/NoLiquidity.tsx
@@ -2,11 +2,11 @@ export const NoLiquidity = () => {
   return (
     <div className="grid gap-8 border-gradient rounded-8 p-16">
       <h3 className="text-gradient text-16">
-        Liquidity to spot trade this pair isn't available — yet.
+        Swapping at market price is not available - yet!
       </h3>
       <p className="text-14">
-        Create a different strategy - the built-in solver system fills your
-        trades usings chain-wide liquidity.
+        Create a limit/range strategy and let our solver find the best time to
+        fill it using chain-wide liquidity
       </p>
     </div>
   );
