ff0cd081045061be946a7a6ed6e83e9ecc74fc47
Grandschtroumpf <francois.guezengar@hotmail.fr>
2025-09-07 14:44:29 +0200

    hide header on scroll

--- src/components/core/menu/mainMenu/MainMenu.tsx
+++ src/components/core/menu/mainMenu/MainMenu.tsx
@@ -1,14 +1,32 @@
-import { FC } from 'react';
+import { FC, useEffect, useRef } from 'react';
 import { MainMenuLeft } from 'components/core/menu/mainMenu/MainMenuLeft';
 import { MainMenuRight } from 'components/core/menu/mainMenu/MainMenuRight';
 
 export const MainMenu: FC = () => {
+  const ref = useRef<HTMLHeadElement>(null);
+  useEffect(() => {
+    let top = true;
+    const handler = () => {
+      if (top !== window.scrollY < 80) {
+        top = window.scrollY < 80;
+        if (window.scrollY > 80) {
+          ref.current!.style.backgroundColor = 'black';
+        } else {
+          ref.current!.style.backgroundColor = 'transparent';
+        }
+      }
+    };
+    document.addEventListener('scroll', handler);
+    return () => document.removeEventListener('scroll', handler);
+  }, []);
   return (
-    <header className="sticky top-0 z-40 w-full" data-testid="main-menu">
-      <div className="main-menu">
-        <MainMenuLeft />
-        <MainMenuRight />
-      </div>
+    <header
+      ref={ref}
+      className="sticky top-0 z-40 w-full h-[80px] px-content font-medium flex items-center justify-between text-white/50"
+      data-testid="main-menu"
+    >
+      <MainMenuLeft />
+      <MainMenuRight />
     </header>
   );
 };
