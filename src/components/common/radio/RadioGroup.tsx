6ab633fe29fb94f7d862e8b68885d5da9ca8d222
Grandschtroumpf <francois.guezengar@hotmail.fr>
2025-09-26 10:57:10 +0200

    animate radio

--- src/components/common/radio/RadioGroup.tsx
+++ src/components/common/radio/RadioGroup.tsx
@@ -15,6 +15,7 @@ export const RadioGroup: FC<RadioGroupProps> = ({ children, ...props }) => {
       className={cn(
         'text-14 relative flex items-center rounded-full bg-black-gradient px-6 py-4',
         props.className,
+        style.radioGroup,
       )}
     >
       {children}
