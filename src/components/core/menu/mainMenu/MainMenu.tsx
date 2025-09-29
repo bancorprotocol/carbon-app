3ab7bab90906c61c0e725989b007be7ff11fa6c8
Grandschtroumpf <francois.guezengar@hotmail.fr>
2025-09-29 10:56:31 +0200

    improve main menu on scroll

--- src/components/core/menu/mainMenu/MainMenu.tsx
+++ src/components/core/menu/mainMenu/MainMenu.tsx
@@ -1,34 +1,11 @@
-import { FC, useEffect, useRef } from 'react';
+import { FC } from 'react';
 import { MainMenuLeft } from 'components/core/menu/mainMenu/MainMenuLeft';
 import { MainMenuRight } from 'components/core/menu/mainMenu/MainMenuRight';
+import style from './MainMenu.module.css';
 
 export const MainMenu: FC = () => {
-  const ref = useRef<HTMLHeadElement>(null);
-  useEffect(() => {
-    let top = true;
-    const handler = () => {
-      if (window.scrollY > 80) {
-        if (!top) return;
-        ref.current!.style.backgroundColor =
-          'oklch(20% var(--chroma) var(--hue) / 0.5)';
-        ref.current!.style.backdropFilter = 'blur(20px) saturate(150%)';
-        top = false;
-      } else {
-        if (top) return;
-        ref.current!.style.backgroundColor = 'transparent';
-        ref.current!.style.backdropFilter = 'none';
-        top = true;
-      }
-    };
-    document.addEventListener('scroll', handler);
-    return () => document.removeEventListener('scroll', handler);
-  }, []);
   return (
-    <header
-      ref={ref}
-      className="sticky top-0 z-40 w-full h-[80px] px-content font-medium flex items-center justify-between text-white/50 backdrop-mix"
-      data-testid="main-menu"
-    >
+    <header className={style.mainMenu} data-testid="main-menu">
       <MainMenuLeft />
       <MainMenuRight />
     </header>
