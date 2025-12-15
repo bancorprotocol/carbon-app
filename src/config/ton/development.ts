d8f0fcfa16360f0f157cc28cd8a31b5fd8532375
Grandschtroumpf <francois.guezengar@hotmail.fr>
2025-12-15 17:09:04 +0100

    fix build

--- src/config/ton/development.ts
+++ src/config/ton/development.ts
@@ -28,6 +28,7 @@ const config: AppConfig = {
       carbonController: '0xe4816658ad10bF215053C533cceAe3f59e1f1087',
       voucher: '0xA4682A2A5Fe02feFF8Bd200240A41AD0E6EaF8d5',
       batcher: '0x5E994Ac7d65d81f51a76e0bB5a236C6fDA8dBF9A',
+      vault: '',
     },
     tac: {
       proxy: '0xfa60E12747D719baDBe3B67dEd3f128B20EDd058',
